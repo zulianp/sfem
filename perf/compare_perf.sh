3ed7d6f6ba786f0a88ac8383a8c3a9d1a3a7977b
Patrick Zulian <patrick.zulian@gmail.com>
2022-12-16 13:09:43 +0100

    ...

--- perf/compare_perf.sh
+++ perf/compare_perf.sh
@@ -10,12 +10,6 @@
 set -e
 # set -x
 
-module purge
-module load petsc/3.13.5_gcc-10.1.0
-module load git
-module load cmake
-module load boost
-
 #if ((1 != $#))
 #then
 #	printf "usage: $0 <path/to/mesh>\n" 1>&2
@@ -23,12 +17,18 @@ module load boost
 #fi
 
 #case_folder=$1
+
+module purge
+module load petsc/3.13.5_gcc-10.1.0
+module load git
+module load cmake
+module load boost
+
 case_folder=/scratch/zulian/xdns/fe_hydros/sfem/tests/compare/mesh-multi-outlet-better
 
 # Libraries
 laplsoldir=/scratch/diegor/zu/laplsol/
 sfemdir=/scratch/zulian/xdns/fe_hydros/sfem/
-#utopiadir=/home/zulian/utopia/utopia/build/
 utopiadir=./
 
 # Add executables to path
@@ -36,9 +36,15 @@ PATH=$sfemdir:$PATH
 PATH=$laplsoldir:$PATH
 PATH=$utopiadir:$PATH
 
-if [ ! -d "./out/" ]; then
-	mkdir out
-fi
+
+patdir=`mktemp -d`
+patdircond=$patdir/condensed
+mkdir $patdircond
+pat32dir=$patdir/fp32
+mkdir $pat32dir
+
+diegodir=`mkdir -d`
+cp -R $case_folder $diegodir
 
 ##############
 # SFEM
@@ -46,17 +52,23 @@ fi
 echo "software: SFEM"
 
 # Assemble operator
-mpirun -np 1 assemble $case_folder ./out
+mpirun -np 1 assemble $case_folder $patdir
 
 # Remove dirichlet nodes
-mpirun -np 1 condense_matrix ./out 	   $case_folder/zd.raw ./condensed
-mpirun -np 1 condense_vector ./out/rhs.raw $case_folder/zd.raw condensed/rhs.raw
+mpirun -np 1 condense_matrix $patdir 	      $case_folder/zd.raw $patdircond
+mpirun -np 1 condense_vector $patdir/rhs.raw  $case_folder/zd.raw $patdircond/rhs.raw
 
 # Parallel linear solve
-mpirun utopia_exec -app ls_solve -A ./condensed/rowptr.raw -b ./condensed/rhs.raw -use_amg false --use_ksp -pc_type hypre -ksp_type cg -atol 1e-18 -rtol 0 -stol 1e-19 -out ./condensed/out.raw --verbose
+mpirun utopia_exec -app ls_solve -A $patdircond/rowptr.raw -b $patdircond/rhs.raw -use_amg false --use_ksp -pc_type hypre -ksp_type cg -atol 1e-18 -rtol 0 -stol 1e-19 -out ./condensed/out.raw --verbose
 
 # Post processing of vector
-mpirun -np 1 remap_vector ./condensed/out.raw $case_folder/zd.raw ./out.raw
+mpirun -np 1 remap_vector $patdircond/out.raw $case_folder/zd.raw $patdir/out.raw
+
+# Convert output from fp64 to fp32
+fp_convert.py $patdircond/rhs.raw 	  $pat32dir/rhs.fp32.raw 		float64 float32
+fp_convert.py $patdircond/values.raw  $pat32dir/values.fp32.raw  	float64 float32
+fp_convert.py $patdircond/sol.raw     $pat32dir/sol.fp32.raw 		float64 float32
+fp_convert.py $patdir/sol.raw     	  $pat32dir/full_sol.fp32.raw 	float64 float32
 
 ##############
 # LAPLSOL
@@ -64,13 +76,34 @@ mpirun -np 1 remap_vector ./condensed/out.raw $case_folder/zd.raw ./out.raw
 echo "software: LAPLSOL"
 
 # Assemble operator
-laplsol-bc $case_folder
-laplsol-asm $case_folder
+laplsol-bc $diegodir
+laplsol-asm $diegodir
 
 # Parallel linear solve
-mpirun laplsol-solve $case_folder $case_folder/rhs.raw $case_folder/sol.raw
+mpirun laplsol-solve $diegodir $diegodir/rhs.raw $diegodir/sol.raw
 
 # Post processing of solution vector
-laplsol-post $case_folder $case_folder/sol.raw
+laplsol-post $diegodir $diegodir/sol.raw
 
-cd $HERE
+##############
+# Compare
+#############
+
+# Int
+fdiff.py $patdircond/rowptr.raw $diegodir/lhs.rowindex.raw 	int32 int32 1 ./rowptr_pat_vs_diego_rhs.png
+fdiff.py $patdircond/colidx.raw $diegodir/lhs.colindex.raw 	int32 int32 1 ./colidx_pat_vs_diego_rhs.png
+
+# FP
+fdiff.py $pat32dir/values.fp32.raw 	$diegodir/lhs.value.raw float32 float32 1 ./lhs_pat_vs_diego_rhs.png
+fdiff.py $pat32dir/rhs.fp32.raw 	$diegodir/rhs.raw 		float32 float32 1 ./rhs_pat_vs_diego_rhs.png
+fdiff.py $pat32dir/sol.fp32.raw 	$diegodir/sol.raw  		float32 float32 1 ./sol_pat_vs_diego_sol.png
+
+diffsol.py $diegodir/sol.raw $patdir/sol.fp32.raw ./diff.fp32.raw
+
+
+# Remove temporaries
+rm -rf $patdir
+rm -rf $patdircond
+rm -rf $pat32dir
+rm -rf $diegodir
+
