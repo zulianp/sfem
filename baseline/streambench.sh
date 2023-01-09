875753413f813404b323af80530879c86679ed5a
Patrick Zulian <patrick.zulian@gmail.com>
2023-01-09 14:23:09 +0100

    ...

--- baseline/streambench.sh
+++ baseline/streambench.sh
@@ -4,4 +4,10 @@ git clone https://github.com/jeffhammond/STREAM.git stream
 
 set -e
 
-cd stream && make
+cd stream
+gcc -mcmodel=large -DSTREAM_TYPE=double -mavx2 -DSTREAM_ARRAY_SIZE=560000000 -DNTIMES=10 -ffp-contract=fast -fopenmp -o stream_c.exe stream.c
+
+export OMP_NUM_THREADS=20
+export OMP_PROC_BIND=close
+
+./stream_c.exe 
