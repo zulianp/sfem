4dbffe158bb9ee98c12eea005a11da821b267750
zulianp <patrick.zulian@gmail.com>
2023-04-01 10:54:00 +0200

    better test script

--- gpu_test/test.sh
+++ gpu_test/test.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
 set -e
+
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+PATH=$SCRIPTPATH:$PATH
+PATH=$SCRIPTPATH/..:$PATH
+PATH=$SCRIPTPATH/../python:$PATH
+PATH=$SCRIPTPATH/../python/mesh:$PATH
+LAUNCH=srun
+
+
 set -x
 
-CASE=../../fetest/meshes/case4
-CASE=../../fetest/meshes/case5
-CASE=../data/mesh-multi-outlet-better/
-# CASE=../sorted_mesh
-# CASE=../data/half_tera_cube 
+CASE=$1
+
+mkdir -p test_actual
+mkdir -p test_oracle
 
 export SFEM_HANDLE_DIRICHLET=0
 export OMP_NUM_THREADS=4 
 export OMP_PROC_BIND=true 
 
 # CUDA + OpenMP
-../assemble $CASE test_actual;  
+$LAUNCH assemble $CASE test_actual;  
 
 # Serial
-../assemble_oracle $CASE test_oracle 
+$LAUNCH assemble_oracle $CASE test_oracle 
 
-../python/fdiff.py test_actual/values.raw test_oracle/values.raw
+fdiff.py test_actual/values.raw test_oracle/values.raw
 
 
 # MATRIXIO_DENSE_OUTPUT=1 ../../matrix.io/print_crs test_actual/rowptr.raw test_actual/colidx.raw test_actual/values.raw int int double
