8e6670f18a0aed0b16e0fae219fdc86dd37b8012
Patrick Zulian <patrick.zulian@gmail.com>
2023-04-06 10:11:16 +0200

    p1 to p2 (untested)

--- mesh/mesh_p1_to_p2.c
+++ mesh/mesh_p1_to_p2.c
@@ -14,6 +14,8 @@
 #include "sfem_base.h"
 #include "sfem_mesh_write.h"
 
+#include "sortreduce.h"
+
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
@@ -90,17 +92,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (!rank) {
-        printf("----------------------------------------\n");
-        printf("mesh_p1_to_p2.c: nodes #p1 %ld, #p2 %ld\n", p1_mesh.nnodes, p2_nodes);
-    }
-
     ///////////////////////////////////////////////////////////////////////////////
     // Create P2 mesh
     ///////////////////////////////////////////////////////////////////////////////
 
     const int n_p2_node_x_element = (p1_mesh.element_type == 4 ? 6 : 3);
 
+    p2_mesh.comm = comm;
     p2_mesh.mem_space = p1_mesh.mem_space;
     p2_mesh.nelements = p1_mesh.nelements;
     p2_mesh.nnodes = p1_mesh.nnodes + p2_nodes;
@@ -116,45 +114,105 @@ int main(int argc, char *argv[]) {
 
     p2_mesh.element_type = p1_mesh.element_type + n_p2_node_x_element;
     p2_mesh.elements = (idx_t **)malloc(p2_mesh.element_type * sizeof(idx_t *));
-    p2_mesh.points = (geom_t **)malloc(p2_mesh.spatial_dim * sizeof(geom_t));
+    p2_mesh.points = (geom_t **)malloc(p2_mesh.spatial_dim * sizeof(geom_t *));
 
-    for(int d = 0; d < p1_mesh.element_type; d++) {
+    for (int d = 0; d < p1_mesh.element_type; d++) {
         p2_mesh.elements[d] = p1_mesh.elements[d];
     }
 
     // Allocate space for p2 nodes
-    for(int d = p1_mesh.element_type; d < p2_mesh.element_type; d++) {
-        p2_mesh.elements[d] = (idx_t*)malloc(p2_mesh.nelements * sizeof(idx_t));
+    for (int d = p1_mesh.element_type; d < p2_mesh.element_type; d++) {
+        p2_mesh.elements[d] = (idx_t *)malloc(p2_mesh.nelements * sizeof(idx_t));
     }
 
-    for(int d = 0; d < p2_mesh.spatial_dim; d++) {
-        p2_mesh.points[d] = (geom_t*)malloc(p2_mesh.nnodes * sizeof(geom_t));
+    for (int d = 0; d < p2_mesh.spatial_dim; d++) {
+        p2_mesh.points[d] = (geom_t *)malloc(p2_mesh.nnodes * sizeof(geom_t));
 
         // Copy p1 portion
-        memcpy(p2_mesh.points[d], p1_mesh.points[d], p2_mesh.nnodes * sizeof(geom_t));
+        memcpy(p2_mesh.points[d], p1_mesh.points[d], p1_mesh.nnodes * sizeof(geom_t));
     }
 
-    // TODO generate p2 Nodes
+    if (p1_mesh.element_type == 4) {
+        // TODO fill p2 node indices in elements
+        for (ptrdiff_t e = 0; e < p2_mesh.nelements; e++) {
+            // Ordering of edges compliant to exodusII spec
+            idx_t row[6];
+            row[0] = MIN(p2_mesh.elements[0][e], p2_mesh.elements[1][e]);
+            row[1] = MIN(p2_mesh.elements[1][e], p2_mesh.elements[2][e]);
+            row[2] = MIN(p2_mesh.elements[2][e], p2_mesh.elements[0][e]);
+            row[3] = MIN(p2_mesh.elements[0][e], p2_mesh.elements[3][e]);
+            row[4] = MIN(p2_mesh.elements[1][e], p2_mesh.elements[3][e]);
+            row[5] = MIN(p2_mesh.elements[2][e], p2_mesh.elements[3][e]);
 
+            idx_t key[6];
+            key[0] = MAX(p2_mesh.elements[0][e], p2_mesh.elements[1][e]);
+            key[1] = MAX(p2_mesh.elements[1][e], p2_mesh.elements[2][e]);
+            key[2] = MAX(p2_mesh.elements[2][e], p2_mesh.elements[0][e]);
+            key[3] = MAX(p2_mesh.elements[0][e], p2_mesh.elements[3][e]);
+            key[4] = MAX(p2_mesh.elements[1][e], p2_mesh.elements[3][e]);
+            key[5] = MAX(p2_mesh.elements[2][e], p2_mesh.elements[3][e]);
+
+            for (int l = 0; l < 6; l++) {
+                const count_t row_begin = rowptr[row[l]];
+                const count_t len_row = rowptr[row[l] + 1] - row_begin;
+                const idx_t *cols = &colidx[row_begin];
+                const idx_t k = find_idx_binary_search(key[l], cols, len_row);
+                p2_mesh.elements[l + p1_mesh.element_type][e] = p2idx[k];
+            }
+        }
+
+    } else {
+        fprintf(stderr, "Implement for element_type %d\n!", p1_mesh.element_type);
+        return EXIT_FAILURE;
+    }
+
+    // Generate p2 coordinates
+    for (ptrdiff_t i = 0; i < p1_mesh.nnodes; i++) {
+        const count_t begin = rowptr[i];
+        const count_t end = rowptr[i + 1];
+
+        for (count_t k = begin; k < end; k++) {
+            const idx_t j = colidx[k];
+
+            if (i < j) {
+                idx_t nidx = p2idx[k];
+
+                for (int d = 0; d < p2_mesh.spatial_dim; d++) {
+                    geom_t xi = p2_mesh.points[d][i];
+                    geom_t xj = p2_mesh.points[d][j];
+
+                    // Midpoint
+                    p2_mesh.points[d][nidx] = (xi + xj) / 2;
+                }
+            }
+        }
+    }
 
     ///////////////////////////////////////////////////////////////////////////////
     // Write P2 mesh to disk
     ///////////////////////////////////////////////////////////////////////////////
 
-    // mesh_write(output_folder, &p2_mesh);
+    mesh_write(output_folder, &p2_mesh);
 
     // Make sure we do not delete the same array twice
-    for(int d = 0; d < p1_mesh.element_type; d++) {
+    for (int d = 0; d < p1_mesh.element_type; d++) {
         p1_mesh.elements[d] = 0;
     }
 
-    mesh_destroy(&p1_mesh);
-    // mesh_destroy(&p2_mesh);
-
-    double tock = MPI_Wtime();
-
     if (!rank) {
         printf("----------------------------------------\n");
+        printf("mesh_p1_to_p2.c: #elements %ld, nodes #p1 %ld, #p2 %ld\n",
+               (long)p1_mesh.nelements,
+               (long)p1_mesh.nnodes,
+               (long)p2_mesh.nnodes);
+        printf("----------------------------------------\n");
+    }
+
+    mesh_destroy(&p1_mesh);
+    mesh_destroy(&p2_mesh);
+
+    double tock = MPI_Wtime();
+    if (!rank) {
         printf("TTS:\t\t\t%g seconds\n", tock - tick);
     }
 
