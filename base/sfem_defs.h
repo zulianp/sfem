b31fbec60e92c7d3077616d7f155d9f78a6514f7
Patrick Zulian <patrick.zulian@gmail.com>
2023-04-11 18:22:21 +0200

    sketching skin for p_k

--- base/sfem_defs.h
+++ base/sfem_defs.h
@@ -7,17 +7,56 @@
 
 enum ElemType { TRI3 = 3, TET4 = 4, TRI6 = 6, HEX8 = 8, TET10 = 10, INVALID = -1 };
 
-SFEM_INLINE static int side_type(const enum ElemType type) {
+SFEM_INLINE static ElemType side_type(const enum ElemType type) {
     switch (type) {
         case TET4:
             return TRI3;
         case TET10:
             return TRI6;
         default: {
-        	assert(false);
+            assert(false);
             return INVALID;
         }
     }
 }
+
+SFEM_INLINE static ElemType elem_num_nodes(const enum ElemType type) {
+    switch (type) {
+        case TRI3:
+            return 3;
+        case TET4:
+            return 4;
+        case TRI6:
+            return 6;
+        case HEX8:
+            return 8;
+        case TET10:
+            return 10;
+        default: {
+            assert(false);
+            return INVALID;
+        }
+    }
+}
+
+SFEM_INLINE static ElemType elem_num_sides(const enum ElemType type) {
+    switch (type) {
+        case TRI3:
+            return 3;
+        case TET4:
+            return 4;
+        case TRI6:
+            return 3;
+        case HEX8:
+            return 6;
+        case TET10:
+            return 4;
+        default: {
+            assert(false);
+            return INVALID;
+        }
+    }
+}
+
 
 #endif  // SFEM_DEFS_H
--- mesh/extract_surface_graph.cpp
+++ mesh/extract_surface_graph.cpp
@@ -2,9 +2,119 @@
 #include "sfem_base.h"
 
 #include <algorithm>
-#include <vector>
 #include <cassert>
 #include <cstddef>
+#include <vector>
+
+#if 0
+
+#include "crs_graph.h"
+
+extern "C" void extract_surface_connectivity(const int element_type,
+                                             const ptrdiff_t n_elements,
+                                             const ptrdiff_t n_nodes,
+                                             idx_t** const SFEM_RESTRICT elems,
+                                             ptrdiff_t* n_surf_elements,
+                                             idx_t** surf_elems,
+                                             idx_t** parent_element) {
+    count_t* n2eptr = 0;
+    idx_t* elindex = 0;
+
+    const int el_n_nodes = elem_num_nodes(element_type);
+    const int el_n_sides = elem_num_sides(element_type);
+
+    ElemType st = side_type(element_type);
+    const int side_num_nodes = elem_num_nodes(st);
+
+    if (element_type == TET10) {
+        // Use edge nodes?
+        build_n2e(n_elements, n_nodes, 6, elems + 4, &n2eptr, &elindex);
+
+        // Or use corners?
+        // build_n2e(n_elements, n_nodes, 4, elems, &n2eptr, &elindex);
+    } else if (element_type == TRI6) {
+        // This will give us an element adjaciency graph directly
+        build_n2e(n_elements, n_nodes, 3, elems + 3, &n2eptr, &elindex);
+    } else {
+        // Generic case (more work?)
+        build_n2e(n_elements, n_nodes, el_n_nodes, elems, &n2eptr, &elindex);
+    }
+
+    int max_n2e = 0;
+
+    // Use N2E connectivity to find adjacient elements
+    for (ptrdiff_t node = 0; node < n_nodes; ++node) {
+        const count_t begin = n2eptr[node];
+        const count_t end = n2eptr[node + 1];
+        max_n2e = MAX(max_n2e, end - begin);
+    }
+
+    idx_t* etable = (idx_t*)malloc(el_n_nodes * max_n2e);
+
+    for (ptrdiff_t node = 0; node < n_nodes; ++node) {
+        const count_t begin = n2eptr[node];
+        const count_t end = n2eptr[node + 1];
+        const count_t range = end - begin;
+
+        // Fill table
+        for (count_t k = 0; k < range; k++) {
+            idx_t e = elindex[begin + k];
+            idx_t * trow = &etable[k*el_n_nodes];
+
+            for(int i = 0; i < el_n_nodes; i++) {
+                trow[i] = elems[i][e];
+            }
+
+            // Sort for comparison
+            std::sort(trow, trow + el_n_nodes);
+        }
+
+        // find adjacient elements
+        for (count_t k1 = 0; k1 < range; k1++) {
+            idx_t e1 = elindex[begin + k1];
+            idx_t * trow1 = &etable[k1*el_n_nodes];
+
+            for (count_t k2 = k1+1; k2 < range; k2++) {
+                idx_t e2 = elindex[begin + k2];
+                idx_t * trow2 = &etable[k2*el_n_nodes];
+
+                // See if common nodes are exactly equal to side_num_nodes
+
+                int count_common = 0;
+                for(int i1 = 0, i2 = 0; i1 < el_n_nodes && i2 < el_n_nodes;) {
+                    if(trow1[i1] < trow2[i2]) {
+                        i1++;
+                        continue;
+                    }
+
+                    if(trow1[i1] > trow2[i2]) {
+                        i2++;
+                        continue;
+                    }
+
+                    count_common++;
+                    i1++;
+                    i2++;
+                }
+
+                if(count_common == side_num_nodes) {
+                    // We have a pair of adj-elements
+                    // Face is not boundary
+                    continue;
+                }
+
+            }
+        }
+
+    }
+
+
+    free(etable);
+    free(n2eptr);
+    free(elindex);
+}
+
+#else
 
 #define PARENT_ID(sideidx) (sideidx) / (4)
 
@@ -130,5 +240,5 @@ extern "C" void extract_surface_connectivity(const ptrdiff_t n_elements,
     *n_surf_elements = n_surface;
 }
 
-
 #undef PARENT_ID
+#endif
