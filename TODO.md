952d1d5f8f252cdd30eca6f8258c435ca513f68d
Patrick Zulian <patrick.zulian@gmail.com>
2023-04-13 17:32:31 +0200

    sketch of weighted fe

--- TODO.md
+++ TODO.md
@@ -6,9 +6,9 @@ The following driver require adaptation for the the pressure projection
 
 - surface_outflux + surface_projection_p0_to_p1 -> (combined driver and p2) [UNTESTED]
 - cgrad + projection_p0_to_p1 -> (combined driver and p2)
-- divergence (p2)
-- lumped_mass_inv (p2)
-- volumes (check that p2 works by piggy-backing)
+- divergence (p2), implement dispatch
+- lumped_mass_inv (p2)  [Needs transformed and dual fe]
+- volumes (check that p2 works by piggy-backing) [LOOKS good]
 - integrate_divergence (this is used for checks, maybe p2)
 
 ## P2 solver performance
--- python/codegen/tet10.py
+++ python/codegen/tet10.py
@@ -96,3 +96,77 @@ class Tet10(FE):
 	def measure(self, q):
 		return det3(A) / 6
 
+#######################################
+# Dual basis trafo and weights
+#######################################
+
+ls_tet10_trafo_ = [1, 0, 0, 0, 0.2, 0,   0.2, 0.2, 0,   0,   0, 1, 0, 0, 0.2, 0.2, 0, 0,   0.2, 0,
+                      0, 0, 1, 0, 0,   0.2, 0.2, 0,   0,   0.2, 0, 0, 0, 1, 0,   0,   0, 0.2, 0.2, 0.2,
+                      0, 0, 0, 0, 0.6, 0,   0,   0,   0,   0,   0, 0, 0, 0, 0,   0.6, 0, 0,   0,   0,
+                      0, 0, 0, 0, 0,   0,   0.6, 0,   0,   0,   0, 0, 0, 0, 0,   0,   0, 0.6, 0,   0,
+                      0, 0, 0, 0, 0,   0,   0,   0,   0.6, 0,   0, 0, 0, 0, 0,   0,   0, 0,   0,   0.6 ]
+
+
+ls_tet10_weights_ = [7.0,    0.7,    0.7,    0.7,   0.175,  0.7,    0.175,  0.175,  0.7,    0.7,    0.7,  7.0,    0.7,
+                0.7,    0.175,  0.175,  0.7,   0.7,    0.175,  0.7,    0.7,    0.7,    7.0,    0.7,  0.7,    0.175,
+                0.175,  0.7,    0.7,    0.175, 0.7,    0.7,    0.7,    7.0,    0.7,    0.7,    0.7,  0.175,  0.175,
+                0.175,  -3.9,   -3.9,   1.2,   1.2,    5.55,   -1.875, -1.875, -1.875, -1.875, 1.2,  1.2,    -3.9,
+                -3.9,   1.2,    -1.875, 5.55,  -1.875, 1.2,    -1.875, -1.875, -3.9,   1.2,    -3.9, 1.2,    -1.875,
+                -1.875, 5.55,   -1.875, 1.2,   -1.875, -3.9,   1.2,    1.2,    -3.9,   -1.875, 1.2,  -1.875, 5.55,
+                -1.875, -1.875, 1.2,    -3.9,  1.2,    -3.9,   -1.875, -1.875, 1.2,    -1.875, 5.55, -1.875, 1.2,
+                1.2,    -3.9,   -3.9,   1.2,   -1.875, -1.875, -1.875, -1.875, 5.55 ]
+
+tet10_trafo = sp.Matrix(10, 10, ls_tet10_trafo_)
+tet10_weights = sp.Matrix(10, 10, ls_tet10_weights_)
+
+def WeightedFE(FE):
+	def __init__(self, fe, weights):
+		self.fe = fe
+		self.weights = weights
+
+	def fun(self, p):
+		fe = self.fe
+		weights = self.weights
+
+		nn = fe.n_nodes()
+		f = fe.fun(p)
+		ret = [0] * nn
+
+		for i in range(0, nn):
+			for j in range(0, nn):
+				ret[i] += f[j] * weights[i, j]
+		return ret
+
+	def name(self):
+		return f'WeightedFE({self.fe.name()})'
+				
+	def n_nodes(self):
+		return self.fe.n_nodes()
+
+	def manifold_dim(self):
+		return self.fe.manifold_dim()
+
+	def spatial_dim(self):
+		return self.fe.spatial_dim()
+
+	def integrate(self, q, expr):
+		return self.fe.integrate(q, expr)
+	
+	def jacobian(self, q):
+		return self.fe.jacobian(q)
+
+	def jacobian_inverse(self, q):
+		return self.fe.jacobian_inverse(q)
+
+	def jacobian_determinant(self, q):
+		return self.fe.jacobian_determinant(q)
+
+	def measure(self, q):
+		return self.fe.measure(q)
+
+# Factory functions
+def TransformedTet10():
+	return WeightedFE(Tet10(), tet10_trafo)
+
+def DualTet10():
+	return WeightedFE(Tet10(), tet10_weights)
