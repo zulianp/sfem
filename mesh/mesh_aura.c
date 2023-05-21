11a29bcd09ef081675052de2d596a898dad3bf5e
Patrick Zulian <patrick.zulian@gmail.com>
2023-05-21 15:39:59 +0200

    ...

--- mesh/mesh_aura.c
+++ mesh/mesh_aura.c
@@ -360,91 +360,95 @@ void mesh_aura(const mesh_t *mesh, mesh_t *aura) {
                                       comm));
     }
 
-    { // Exchange node_mapping and xyz
+    {  // Exchange node_mapping and xyz
         int max_send_elems = 0;
-        for(int r = 0; r < size; r++) {
+        for (int r = 0; r < size; r++) {
             max_send_elems = MAX(max_send_elems, send_count[r]);
         }
 
         idx_t *node_send_lists = (idx_t *)malloc(max_send_elems * nnxe * sizeof(idx_t));
 
-        int * node_send_count = (int *)malloc(size * sizeof(int));
-        int * node_send_displs = (int *)malloc((size+1) * sizeof(int));
-        
+        int *node_send_count = (int *)malloc(size * sizeof(int));
+        int *node_send_displs = (int *)malloc((size + 1) * sizeof(int));
+
         memset(node_send_count, 0, (size) * sizeof(int));
         node_send_displs[0] = 0;
 
         // Count number of nodes to send
-        for(int r = 0; r < size; r++) {
+        for (int r = 0; r < size; r++) {
             const int begin = send_displs[r];
             const int extent = send_count[r];
-            const idx_t * const elems = &element_send_lists[begin];
+            const idx_t *const elems = &element_send_lists[begin];
 
-            for(int d = 0; d < nnxe; d++) {
-                idx_t * nodes = &node_send_lists[d*extent];
-
-                for(int i = 0; i < extent; i++) {
+            int nnn = 0;
+            for (int d = 0; d < nnxe; d++) {
+                for (int i = 0; i < extent; i++) {
                     const idx_t node = mesh->elements[d][elems[i]];
                     // Send only nodes we own
-                    if(mesh->node_owner[node] == rank) {
-                        nodes[i] = node;
+                    if (mesh->node_owner[node] == rank) {
+                        node_send_lists[nnn++] = node;   
                     }
                 }
             }
 
-            const idx_t n_send_nodes = sortreduce(node_send_lists, extent * nnxe);
-            node_send_displs[r+1] = n_send_nodes + node_send_displs[r];
+            const idx_t n_send_nodes = sortreduce(node_send_lists, nnn);
+            node_send_displs[r + 1] = n_send_nodes + node_send_displs[r];
         }
 
-        idx_t * node_send_buff = (idx_t*)malloc(node_send_displs[size] * sizeof(idx_t));
-        geom_t * point_send_buff = (geom_t*)malloc(node_send_displs[size] * sizeof(geom_t));
+        idx_t *node_send_buff = (idx_t *)malloc(node_send_displs[size] * sizeof(idx_t));
+        geom_t *point_send_buff = (geom_t *)malloc(node_send_displs[size] * sizeof(geom_t));
 
         // Pack nodes to send
-        for(int r = 0; r < size; r++) {
+        int offset = 0;
+        for (int r = 0; r < size; r++) {
             const int begin = send_displs[r];
             const int extent = send_count[r];
-            const idx_t * const elems = &element_send_lists[begin];
+            const idx_t *const elems = &element_send_lists[begin];
 
-            for(int d = 0; d < nnxe; d++) {
-                idx_t * nodes = &node_send_lists[d*extent];
-
-                for(int i = 0; i < extent; i++) {
+            int nnn = 0;
+            for (int d = 0; d < nnxe; d++) {
+                for (int i = 0; i < extent; i++) {
                     const idx_t node = mesh->elements[d][elems[i]];
                     // Send only nodes we own
-                    if(mesh->node_owner[node] == rank) {
-                        nodes[i] = node;
+                    if (mesh->node_owner[node] == rank) {
+                        assert(node >= 0);
+                        node_send_lists[nnn++] = node;
                     }
                 }
             }
 
-            const int n_send_nodes = sortreduce(node_send_lists, extent * nnxe);
-            for(int i = 0; i < n_send_nodes; i++) {
+            const int n_send_nodes = sortreduce(node_send_lists, nnn);
+
+            for (int i = 0; i < n_send_nodes; i++) {
+                assert(node_send_lists[i] >= 0);
+                assert(node_send_displs[r] + node_send_count[r] < node_send_displs[size]);
                 node_send_buff[node_send_displs[r] + node_send_count[r]++] = node_send_lists[i];
             }
         }
 
         // exchange
-        int * node_recv_count = (int *)malloc(size * sizeof(int));
-        int * node_recv_displs = (int *)malloc((size+1) * sizeof(int));
+        int *node_recv_count = (int *)malloc(size * sizeof(int));
+        int *node_recv_displs = (int *)malloc((size + 1) * sizeof(int));
 
         CATCH_MPI_ERROR(MPI_Alltoall(node_send_count, 1, MPI_INT, node_recv_count, 1, MPI_INT, comm));
 
         node_recv_displs[0] = 0;
 
-        for(int r = 0; r < size; r++) {
-            node_recv_displs[r+1] = node_recv_displs[r] + node_recv_count[r];
+        for (int r = 0; r < size; r++) {
+            node_recv_displs[r + 1] = node_recv_displs[r] + node_recv_count[r];
         }
 
-        idx_t * aura_nodes = (idx_t*)malloc(node_recv_displs[size] * sizeof(idx_t));
-        geom_t **aura_points = (geom_t**)malloc(mesh->spatial_dim * sizeof(geom_t *));
+        idx_t *aura_nodes = (idx_t *)malloc(node_recv_displs[size] * sizeof(idx_t));
+        geom_t **aura_points = (geom_t **)malloc(mesh->spatial_dim * sizeof(geom_t *));
 
-
-        for(int d = 0; d < mesh->spatial_dim; d++) {
-            for(int i = 0; i < node_send_displs[size]; i++) {
+        for (int d = 0; d < mesh->spatial_dim; d++) {
+            for (int i = 0; i < node_send_displs[size]; i++) {
+                assert(node_send_buff[i] >= 0);
+                assert(node_send_buff[i] < mesh->nnodes);
                 point_send_buff[i] = mesh->points[d][node_send_buff[i]];
             }
 
-            aura_points[d] = (geom_t*)malloc(node_recv_displs[size] * sizeof(geom_t));
+            aura_points[d] = (geom_t *)malloc(node_recv_displs[size] * sizeof(geom_t));
 
             CATCH_MPI_ERROR(MPI_Alltoallv(point_send_buff,
                                           node_send_count,
@@ -457,7 +461,7 @@ void mesh_aura(const mesh_t *mesh, mesh_t *aura) {
                                           comm));
         }
 
-        for(int i = 0; i < node_send_displs[size]; i++) {
+        for (int i = 0; i < node_send_displs[size]; i++) {
             node_send_buff[i] = mesh->node_mapping[node_send_buff[i]];
         }
 
@@ -471,17 +475,17 @@ void mesh_aura(const mesh_t *mesh, mesh_t *aura) {
                                       SFEM_MPI_IDX_T,
                                       comm));
 
-        aura->points = aura_points; 
+        aura->points = aura_points;
         aura->node_mapping = aura_nodes;
-        aura->node_owner = (idx_t*)malloc(node_recv_displs[size] * sizeof(idx_t));
+        aura->node_owner = (idx_t *)malloc(node_recv_displs[size] * sizeof(idx_t));
 
-        for(int r = 0; r < size; r++) {
+        for (int r = 0; r < size; r++) {
             int begin = node_recv_displs[r];
             int extent = node_recv_count[r];
 
-            idx_t * no = &aura->node_owner[begin];
+            idx_t *no = &aura->node_owner[begin];
 
-            for(int k = 0; k < extent; k++) {
+            for (int k = 0; k < extent; k++) {
                 no[k] = r;
             }
         }
@@ -506,180 +510,3 @@ void mesh_aura(const mesh_t *mesh, mesh_t *aura) {
         free(recv_displs);
     }
 }
-
-// void mesh_aura(const mesh_t *mesh, mesh_t *aura) {
-//     MPI_Comm comm = mesh->comm;
-
-//     int rank, size;
-//     MPI_Comm_rank(comm, &rank);
-//     MPI_Comm_size(comm, &size);
-
-//     const ptrdiff_t n_owned_elements = mesh->n_owned_elements;
-//     const ptrdiff_t n_shared_elements = mesh->n_shared_elements;
-
-//     const int nnxe = elem_num_nodes(mesh->element_type);
-//     idx_t **shared_elements = (idx_t **)malloc(nnxe * sizeof(idx_t *));
-
-//     int *send_displs = (int *)malloc((size + 1) * sizeof(int));
-//     memset(send_displs, 0, (size + 1) * sizeof(int));
-
-//     idx_t *owner = malloc(nnxe * sizeof(idx_t));
-
-//     ptrdiff_t n_send_elements = 0;
-//     for (ptrdiff_t es = 0; es < n_shared_elements; es++) {
-//         for (int d = 0; d < nnxe; d++) {
-//             owner[d] = mesh->node_owner[mesh->elements[d][n_owned_elements + es]];
-//         }
-
-//         int n_owners = sortreduce(owner, nnxe);
-//         for (int i = 0; i < n_owners; i++) {
-//             send_displs[owner[i] + 1]++;
-//             n_send_elements++;
-//         }
-//     }
-
-//     for (int r = 0; r < size; r++) {
-//         send_displs[r + 1] += send_displs[r];
-//     }
-
-//     for (int d = 0; d < nnxe; d++) {
-//         shared_elements[d] = (idx_t *)malloc(n_send_elements * sizeof(idx_t));
-//     }
-
-//     int *send_count = (int *)malloc((size + 1) * sizeof(int));
-//     memset(send_count, 0, (size + 1) * sizeof(int));
-
-//     for (ptrdiff_t es = 0; es < n_shared_elements; es++) {
-//         for (int d = 0; d < nnxe; d++) {
-//             owner[d] = mesh->node_owner[mesh->elements[d][n_owned_elements + es]];
-//         }
-
-//         int n_owners = sortreduce(owner, nnxe);
-
-//         for (int i = 0; i < n_owners; i++) {
-//             int owner_rank = owner[i];
-//             for (int d = 0; d < nnxe; d++) {
-//                 // Retrieve original global identifier
-//                 const idx_t nidx = mesh->node_mapping[mesh->elements[d][n_owned_elements + es]];
-//                 shared_elements[d][send_displs[owner_rank] + send_count[owner_rank]] = nidx;
-//             }
-
-//             send_count[owner_rank]++;
-//         }
-//     }
-
-//     int *recv_count = (int *)malloc((size + 1) * sizeof(int));
-//     memset(recv_count, 0, (size + 1) * sizeof(int));
-
-//     int *recv_displs = (int *)malloc((size + 1) * sizeof(int));
-//     memset(recv_displs, 0, (size + 1) * sizeof(int));
-
-//     CATCH_MPI_ERROR(MPI_Alltoall(send_count, 1, MPI_INT, recv_count, 1, MPI_INT, comm));
-
-//     recv_displs[0] = 0;
-//     for (int r = 0; r < size; r++) {
-//         recv_displs[r + 1] = recv_displs[r] + recv_count[r];
-//     }
-
-//     idx_t **aura_elements = (idx_t **)malloc(nnxe * sizeof(idx_t *));
-//     for (int d = 0; d < nnxe; d++) {
-//         aura_elements[d] = (idx_t *)malloc(recv_displs[size] * sizeof(idx_t));
-
-//         CATCH_MPI_ERROR(MPI_Alltoallv(shared_elements[d],
-//                                       send_count,
-//                                       send_displs,
-//                                       SFEM_MPI_IDX_T,
-//                                       recv_count,
-//                                       recv_displs,
-//                                       aura_elements[d],
-//                                       SFEM_MPI_IDX_T,
-//                                       comm));
-//     }
-
-//     const size_t n_unsorted = recv_displs[size] * nnxe;
-//     idx_t *aura_nodes = (idx_t *)malloc(n_unsorted * sizeof(idx_t));
-
-//     for (int d = 0; d < nnxe; d++) {
-//         memcpy(&aura_nodes[d * recv_displs[size]], aura_elements[d], recv_displs[size] * sizeof(idx_t));
-//     }
-
-//     const ptrdiff_t n_aura_nodes = sortreduce(aura_nodes, n_unsorted);
-
-//     idx_t *global_to_local_map = (idx_t *)malloc(n_aura_nodes * sizeof(idx_t));
-
-//     for (ptrdiff_t i = 0; i < n_aura_nodes; i++) {
-//         // FIXME check if not found!
-//         global_to_local_map[i] = find_idx_binary_search(aura_nodes[i], mesh->node_mapping, mesh->nnodes);
-//     }
-
-//     uint8_t *marked = (uint8_t *)malloc(mesh->nnodes * sizeof(uint8_t));
-//     memset(marked, 0, mesh->nnodes * sizeof(uint8_t));
-
-//     for (ptrdiff_t i = 0; i < n_aura_nodes; i++) {
-//         if (global_to_local_map[i] >= 0 && global_to_local_map[i] < mesh->nnodes) {
-//             marked[global_to_local_map[i]] = 1;
-//         }
-//     }
-
-//     ptrdiff_t n_send_elements_phase_2 = 0;
-
-//     // Identify owned elements
-//     for (ptrdiff_t i = 0; i < mesh->n_owned_elements; i++) {
-//         int must_add = 0;
-//         for (int d = 0; d < nnxe; d++) {
-//             must_add += marked[mesh->elements[d][i]];
-//         }
-
-//         if (must_add) {
-//             n_send_elements_phase_2++;
-//         }
-//     }
-
-//     idx_t **shared_elements_2 = (idx_t **)malloc(nnxe * sizeof(idx_t *));
-//     for (int d = 0; d < nnxe; d++) {
-//         shared_elements_2[d] = (idx_t *)malloc(n_send_elements_phase_2 * sizeof(idx_t));
-//     }
-
-//     {  // Pack elements for sending to
-//         ptrdiff_t offset = 0;
-//         for (ptrdiff_t i = 0; i < mesh->n_owned_elements; i++) {
-//             int must_add = 0;
-//             for (int d = 0; d < nnxe; d++) {
-//                 must_add += marked[mesh->elements[d][i]];
-//             }
-
-//             if (must_add) {
-//                 for (int d = 0; d < nnxe; d++) {
-//                     shared_elements_2[d][offset] = mesh->elements[d][i];
-//                 }
-
-//                 offset++;
-//             }
-//         }
-//     }
-
-//     // Store in output mesh
-//     aura->comm = comm;
-//     // aura->elements
-
-//     // Clean-up
-//     free(owner);
-//     free(send_count);
-//     free(send_displs);
-//     free(recv_count);
-//     free(recv_displs);
-//     free(aura_nodes);
-//     free(global_to_local_map);
-
-//     for (int d = 0; d < nnxe; d++) {
-//         free(aura_elements[d]);
-//     }
-
-//     free(aura_elements);
-
-//     for (int d = 0; d < nnxe; d++) {
-//         free(shared_elements_2[d]);
-//     }
-
-//     free(shared_elements_2);
-// }
