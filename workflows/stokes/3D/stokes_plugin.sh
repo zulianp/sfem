361970f68750e42870a248b41396829ea6f9eb28
Patrick Zulian <patrick.zulian@gmail.com>
2023-08-11 14:13:41 +0200

    ...

--- workflows/stokes/3D/stokes_plugin.sh
+++ workflows/stokes/3D/stokes_plugin.sh
@@ -12,8 +12,8 @@ PATH=$SCRIPTPATH/../../../data/benchmarks/meshes:$PATH
 PATH=$SCRIPTPATH/../../../../matrix.io:$PATH
 PATH=$SCRIPTPATH/../../convert_exodus_to_raw:$PATH
 
-export PATH=$CODE_DIR/utopia/utopia/build_debug:$PATH
-# export PATH=$CODE_DIR/utopia/utopia/build:$PATH
+# export PATH=$CODE_DIR/utopia/utopia/build_debug:$PATH
+export PATH=$CODE_DIR/utopia/utopia/build:$PATH
 
 export VAR_UX=0
 export VAR_UY=1
@@ -51,7 +51,7 @@ head -c 4 $SFEM_MESH_DIR/sidesets_aos/ARTIFICIAL_MEMBRANE.raw > $pdof
 
 
 export SFEM_DIRICHLET_NODESET="$sinlet,$sinlet,$sinlet,$swall,$swall,$swall,$smembrane,$smembrane,$smembrane,$soutlet"
-export SFEM_DIRICHLET_VALUE="0.1,0,0,0.1,0,0,0,0,0,0"
+export SFEM_DIRICHLET_VALUE="0.1,0,0,0,0,0,0,0,0,0"
 export SFEM_DIRICHLET_COMPONENT="$VAR_UX,$VAR_UY,$VAR_UZ,$VAR_UX,$VAR_UY,$VAR_UZ,$VAR_UX,$VAR_UY,$VAR_UZ,$VAR_P"
 
 ##########################################################################################
@@ -69,9 +69,9 @@ export SFEM_DIRICHLET_COMPONENT="$VAR_UX,$VAR_UY,$VAR_UZ,$VAR_UX,$VAR_UY,$VAR_UZ
 set -x
 
 # lldb --  
-# utopia_exec -app nlsolve -path $CODE_DIR/sfem/stokes_plugin.dylib -solver_type ConjugateGradient --verbose -max_it 10000 -matrix_free false -apply_gradient_descent_step true
+utopia_exec -app nlsolve -path $CODE_DIR/sfem/stokes_plugin.dylib -solver_type ConjugateGradient --verbose -max_it 10000 -matrix_free true -apply_gradient_descent_step true
 # lldb -- 
-utopia_exec --verbose  -app nlsolve -path $CODE_DIR/sfem/stokes_plugin.dylib -solver_type Newton -max_it 40
+# utopia_exec --verbose  -app nlsolve -path $CODE_DIR/sfem/stokes_plugin.dylib -solver_type Newton -max_it 40 -ksp_monitor -pc_monitor
 
 aos_to_soa $SFEM_OUTPUT_DIR/out.raw 8 $BLOCK_SIZE $SFEM_OUTPUT_DIR/x
 raw_to_db.py $SFEM_MESH_DIR $SFEM_OUTPUT_DIR/x.vtk -p "$SFEM_OUTPUT_DIR/x.*.raw"
