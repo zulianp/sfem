8add35951d43574c814636705aadcd2d9348b5eb
Patrick Zulian <patrick.zulian@gmail.com>
2023-10-11 16:23:12 +0200

    xdmf also considers origin and dx

--- python/grid/raw_to_xdmf.py
+++ python/grid/raw_to_xdmf.py
@@ -15,14 +15,16 @@ def main(path):
     xdmf_path = fname + '.xdmf'
     file_name = fname
 
+    ox = oy = oz = 0
     nx = ny = nz = 0
+    dx = dy = dz = 1
     block_size = 0
     time_steps = 0
     endianess = tp = precision = number_type = ''
     attribute_type = 'Vector'
     final_path = pdir + '/' + 'metadata_' + file_name + '.yml'
 
-    print(f'reading {final_path} ...')
+    # print(f'reading {final_path} ...')
     with open(final_path, 'r'
               ) as f:
         Lines = f.readlines()
@@ -33,6 +35,18 @@ def main(path):
                 ny = int(i[4:])
             elif i[:4] == 'nz: ':
                 nz = int(i[4:])
+            elif i[:4] == 'ox: ':
+                ox = float(i[4:])
+            elif i[:4] == 'oy: ':
+                oy = float(i[4:])
+            elif i[:4] == 'oz: ':
+                oz = float(i[4:])
+            elif i[:4] == 'dx: ':
+                dx = float(i[4:])
+            elif i[:4] == 'dy: ':
+                dy = float(i[4:])
+            elif i[:4] == 'dz: ':
+                dz = float(i[4:])
             elif i[:11] == 'endianess: ':
                 endianess = i[11:]
                 endianess = endianess.replace("\n","")
@@ -65,67 +79,68 @@ def main(path):
     # #################################TIME-VARIANT#########################################
     time_string = ''
     if int(time_steps) > 1:
+        assert False
 
-        n_grids = int(time_steps)
-        time_string_header = \
-            """<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
-<Xdmf 
-    xmlns:xi="http://www.w3.org/2001/XInclude" Version="2.0">
-    <Domain>
-        <Topology name="topo" TopologyType="3DCoRectMesh"
-            Dimensions="{dim}">
-        </Topology>
-        <Geometry name="geo" Type="ORIGIN_DXDYDZ">
-            <!-- Origin -->
-            <DataItem Format="XML" Dimensions="3">
-                0.0 0.0 0.0
-            </DataItem>
-            <!-- DxDyDz -->
-            <DataItem Format="XML" Dimensions="3">
-                1 1 1
-            </DataItem>
-        </Geometry>""".format(dim=''
-                 + str(nx) + ' ' + str(ny) + ' ' + str(nz) + '')
-        time_string_global = \
-            """
-        <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
-            <Time TimeType="HyperSlab">
-                <DataItem Format="XML" NumberType="{number_type}" Dimensions="3"> 
-                    <!-- start stride count-->
-                    0.0 1.0 {n_grids}
-                </DataItem>
-            </Time>""".format(n_grids=n_grids,
-                number_type=number_type)
-        time_string_local_final = ''
-        for i in range(0, int(time_steps)):
-            time_string_local_final = time_string_local_final \
-                + """
-            <Grid Name="{grid_name}" GridType="Uniform">
-                <Topology Reference="/Xdmf/Domain/Topology[1]"/>
-                <Geometry Reference="/Xdmf/Domain/Geometry[1]"/>
-                    <Attribute Name="{file_name}" Center="Node" AttributeType="{attribute_type}">
-                        <DataItem Format="Binary" Precision="{precision}" Endian="{endianess}"
-                            Dimensions="{dim} {block_size}" NumberType="{number_type}">
-                                {file_name_t}.raw
-                        </DataItem>
-                    </Attribute>
-            </Grid>""".format(
-                dim='' + str(nx) + ' ' + str(ny) + ' ' + str(nz) + '',
-                endianess=endianess,
-                file_name=file_name,
-                file_name_t=file_name + '_t' + str(i),
-                precision=precision,
-                number_type=number_type,
-                block_size=block_size,
-                attribute_type=attribute_type,
-                grid_name='T' + str(i),
-                )
-        time_footer = """
-        </Grid>
-    </Domain>
-</Xdmf>"""
-        time_string = time_string_header + time_string_global \
-            + time_string_local_final + time_footer
+#         n_grids = int(time_steps)
+#         time_string_header = \
+#             """<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+# <Xdmf 
+#     xmlns:xi="http://www.w3.org/2001/XInclude" Version="2.0">
+#     <Domain>
+#         <Topology name="topo" TopologyType="3DCoRectMesh"
+#             Dimensions="{dim}">
+#         </Topology>
+#         <Geometry name="geo" Type="ORIGIN_DXDYDZ">
+#             <!-- Origin -->
+#             <DataItem Format="XML" Dimensions="3">
+#                 0.0 0.0 0.0
+#             </DataItem>
+#             <!-- DxDyDz -->
+#             <DataItem Format="XML" Dimensions="3">
+#                 1 1 1
+#             </DataItem>
+#         </Geometry>""".format(dim=''
+#                  + str(nx) + ' ' + str(ny) + ' ' + str(nz) + '')
+#         time_string_global = \
+#             """
+#         <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+#             <Time TimeType="HyperSlab">
+#                 <DataItem Format="XML" NumberType="{number_type}" Dimensions="3"> 
+#                     <!-- start stride count-->
+#                     0.0 1.0 {n_grids}
+#                 </DataItem>
+#             </Time>""".format(n_grids=n_grids,
+#                 number_type=number_type)
+#         time_string_local_final = ''
+#         for i in range(0, int(time_steps)):
+#             time_string_local_final = time_string_local_final \
+#                 + """
+#             <Grid Name="{grid_name}" GridType="Uniform">
+#                 <Topology Reference="/Xdmf/Domain/Topology[1]"/>
+#                 <Geometry Reference="/Xdmf/Domain/Geometry[1]"/>
+#                     <Attribute Name="{file_name}" Center="Node" AttributeType="{attribute_type}">
+#                         <DataItem Format="Binary" Precision="{precision}" Endian="{endianess}"
+#                             Dimensions="{dim} {block_size}" NumberType="{number_type}">
+#                                 {file_name_t}.raw
+#                         </DataItem>
+#                     </Attribute>
+#             </Grid>""".format(
+#                 dim='' + str(nx) + ' ' + str(ny) + ' ' + str(nz) + '',
+#                 endianess=endianess,
+#                 file_name=file_name,
+#                 file_name_t=file_name + '_t' + str(i),
+#                 precision=precision,
+#                 number_type=number_type,
+#                 block_size=block_size,
+#                 attribute_type=attribute_type,
+#                 grid_name='T' + str(i),
+#                 )
+#         time_footer = """
+#         </Grid>
+#     </Domain>
+# </Xdmf>"""
+#         time_string = time_string_header + time_string_global \
+#             + time_string_local_final + time_footer
 
     # #################################NORMAL-VARIANT#########################################
 
@@ -140,11 +155,11 @@ def main(path):
         <Geometry name="geo" Type="ORIGIN_DXDYDZ">
             <!-- Origin -->
             <DataItem Format="XML" Dimensions="3">
-                0.0 0.0 0.0
+                {oz} {oy} {ox}
             </DataItem>
             <!-- DxDyDz -->
             <DataItem Format="XML" Dimensions="3">
-                1 1 1
+                {dz} {dy} {dx}
             </DataItem>
         </Geometry>
         <Grid Name="T1" GridType="Uniform">
@@ -159,6 +174,12 @@ def main(path):
     </Domain>
 </Xdmf>""".format(
         dim='' + str(nz) + ' ' + str(ny) + ' ' + str(nx) + '',
+        ox=ox,
+        oy=oy,
+        oz=oz,
+        dx=dx,
+        dy=dy,
+        dz=dz,
         endianess=endianess,
         file_name=file_name,
         block_size=block_size,
@@ -167,8 +188,8 @@ def main(path):
         attribute_type=attribute_type,
         )
     
-    print(f'Writing {xdmf_path} ...')
-    print(xdmf_string)
+    # print(f'Writing {xdmf_path} ...')
+    # print(xdmf_string)
 
     # if int(time_steps) == 1:
     textfile = open(xdmf_path, 'w')
--- python/sdf/mesh_to_sdf.py
+++ python/sdf/mesh_to_sdf.py
@@ -390,11 +390,17 @@ if __name__ == '__main__':
     header +=   f'nz: {dims[2]}\n'
     header +=   f'block_size: 1\n'
     header +=   f'type: float\n'
+    header +=   f'ox: {pmin[0]}\n'
+    header +=   f'oy: {pmin[1]}\n'
+    header +=   f'oz: {pmin[2]}\n'
+    header +=   f'dx: {(pmax[0] - pmin[0])/(dims[0] - 1)}\n'
+    header +=   f'dy: {(pmax[1] - pmin[1])/(dims[1] - 1)}\n'
+    header +=   f'dz: {(pmax[2] - pmin[2])/(dims[2] - 1)}\n'
 
     fname, fextension = os.path.splitext(output_path)
     pdir = os.path.dirname(fname)
     fname = os.path.basename(fname)
-    
+
     with open(f'{pdir}/metadata_{fname}.yml', 'w') as f:
         f.write(header)
 
