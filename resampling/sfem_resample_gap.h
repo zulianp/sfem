606b6fd1eb984013b73da4a337d10f1b35b178c0
Patrick Zulian <patrick.zulian@gmail.com>
2023-11-10 15:37:17 +0100

    Added extra function

--- resampling/sfem_resample_gap.h
+++ resampling/sfem_resample_gap.h
@@ -11,6 +11,25 @@
 extern "C" {
 #endif
 
+int resample_gap_local(
+    // Mesh
+    const enum ElemType element_type,
+    const ptrdiff_t nelements,
+    const ptrdiff_t nnodes,
+    idx_t** const SFEM_RESTRICT elems,
+    geom_t** const SFEM_RESTRICT xyz,
+    // SDF
+    const ptrdiff_t* const SFEM_RESTRICT n,
+    const ptrdiff_t* const SFEM_RESTRICT stride,
+    const geom_t* const SFEM_RESTRICT origin,
+    const geom_t* const SFEM_RESTRICT delta,
+    const geom_t* const SFEM_RESTRICT data,
+    // Output
+    real_t* const SFEM_RESTRICT wg,
+    real_t* const SFEM_RESTRICT xnormal,
+    real_t* const SFEM_RESTRICT ynormal,
+    real_t* const SFEM_RESTRICT znormal);
+
 int resample_gap(
                  // Mesh
                  const enum ElemType element_type,
