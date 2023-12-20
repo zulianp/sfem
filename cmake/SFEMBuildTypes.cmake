6ed4a946b018abd16012ffe0c7dc8dffcc933db8
Patrick Zulian <patrick.zulian@gmail.com>
2023-12-20 16:59:38 +0100

    ...

--- cmake/SFEMBuildTypes.cmake
+++ cmake/SFEMBuildTypes.cmake
@@ -3,5 +3,5 @@
 include(cmake/SFEMSanitizer.cmake)
 
 set(CMAKE_CXX_FLAGS_AVX2
-    "-Ofast -DNDEBUG  -mavx2 "
+    "-Ofast -march=core-avx2 -DNDEBUG -DSFEM_ENABLE_AVX2_SORT -DSFEM_ENABLE_EXPLICIT_VECTORIZATION -Iexternal"
     CACHE STRING "Flags for using fast operations and avx2" FORCE)
