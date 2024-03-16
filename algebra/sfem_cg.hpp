93a791ce25d93ac8e3cfef94f78638f1642270ca
Patrick Zulian <patrick.zulian@gmail.com>
2024-03-16 12:50:04 +0100

    sketch prototype cg

--- algebra/sfem_cg.hpp
+++ algebra/sfem_cg.hpp
@@ -5,54 +5,62 @@
 
 // https://en.wikipedia.org/wiki/Conjugate_gradient_method
 namespace sfem {
-	template<typename T>
-	class ConjugateGradient {
-	public:
-		std::function<void(const T*const, T*const)> apply;
-		std::function<T*(const std::size_t)> allocate;
-		std::function<void(const T*)> destroy;
-		std::function<T (const T*const, const T*const)> dot;
-		std::function<void(const T, const T*const, const T, T*const)> axpby;
-		T tol{1e-10};
+    template <typename T>
+    class ConjugateGradient {
+    public:
+        std::function<void(const T* const, T* const)> apply;
+        std::function<T*(const std::size_t)> allocate;
+        std::function<void(const T*)> destroy;
+        std::function<T(const T* const, const T* const)> dot;
+        std::function<void(const T, const T* const, const T, T* const)> axpby;
+        T tol{1e-10};
 
-		int apply(const size_t n, const T*const b, T*const x)
-		{
-			T * r = allocate(n);
-			T * p = allocate(n);
-			T * ptAp = allocate(n);
+        int apply(const size_t n, const T* const b, T* const x) {
+            T* r = allocate(n);
 
-			apply(x, r);
-			axpby(1, b, -1, r);
+            apply(x, r);
+            axpby(1, b, -1, r);
 
-			T rtr = dot(r, r);
-			
-			if(rtr < tol) {
-				return 0;
-			}
+            T rtr = dot(r, r);
 
-			for(int k = 0; k < max_it; k++) {
-				apply(p, ptAp);
+            if (rtr < tol) {
+                destroy(r);
+                return 0;
+            }
 
-				T ptAp = dot(p, Ap);
-				T alpha = rtr/ptAp;
+            T* p = allocate(n);
+            T* ptAp = allocate(n);
 
-				// Opt use 2 cuda streams?
-				axpby(alpha, p, 1, x);
-				axpby(-alpha, Ap, 1, r);
+            int info = -1;
+            for (int k = 0; k < max_it; k++) {
+                apply(p, ptAp);
 
-				T rtr_new = dot(r, r);
-				T beta = rtr_new/rtr;
-				rtr = rtr_new;
+                T ptAp = dot(p, Ap);
+                T alpha = rtr / ptAp;
 
-				axpby(1, r, beta, p);
-			}
+                // Opt use 2 cuda streams?
+                axpby(alpha, p, 1, x);
+                axpby(-alpha, Ap, 1, r);
 
-			// clean-up
-			destroy(r);
-			destroy(p);
-			destroy(ptAp);
-		}
-	};
-}
+                T rtr_new = dot(r, r);
+                T beta = rtr_new / rtr;
+                rtr = rtr_new;
+
+                if (rtr < tol) {
+                    info = 0;
+                    break;
+                }
+
+                axpby(1, r, beta, p);
+            }
+
+            // clean-up
+            destroy(r);
+            destroy(p);
+            destroy(ptAp);
+            return info;
+        }
+    };
+}  // namespace sfem
 
 #endif SFEM_CG_HPP
