c0f92de899be08eaaa890071b7b888f63fda4783
Simone <sriva@susePC>
2024-03-20 16:14:09 +0100

    first cuda test is working

---
+++ resampling/cuda/comp.sh
@@ -0,0 +1,9 @@
+
+# nvcc -O3  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
+nvcc -g -G  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
+
+nvcc --gpu-architecture=sm_75 -dlink -o sfem_resample_field_cuda.o temp.o 
+ar cru libsfem_resample_field_cuda.a sfem_resample_field_cuda.o temp.o
+rm temp.o
+ranlib libsfem_resample_field_cuda.a
+
