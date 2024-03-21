6de72d5af3d8db757413666b70f1bea13e04f0cb
Simone <sriva@susePC>
2024-03-21 09:11:47 +0100

    test cuda

--- resampling/cuda/comp.sh
+++ resampling/cuda/comp.sh
@@ -1,9 +1,11 @@
 
-# nvcc -O3  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
-nvcc -g -G  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
+nvcc -O3 -v -use_fast_math -Xptxas=-O3  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
+# nvcc -g -G  --gpu-architecture=sm_75 -rdc=true -c sfem_resample_field_cuda.cu -o temp.o
 
 nvcc --gpu-architecture=sm_75 -dlink -o sfem_resample_field_cuda.o temp.o 
 ar cru libsfem_resample_field_cuda.a sfem_resample_field_cuda.o temp.o
 rm temp.o
 ranlib libsfem_resample_field_cuda.a
+
+
 
