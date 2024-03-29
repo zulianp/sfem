8a52ed0c37caca689061e41860619fd6287807ab
Patrick Zulian <patrick.zulian@gmail.com>
2024-03-29 13:52:48 +0100

    box gen script

---
+++ data/benchmarks/meshes/create_box.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+PATH=$SCRIPTPATH:$PATH
+PATH=$SCRIPTPATH/../../..:$PATH
+PATH=$SCRIPTPATH/../../../python:$PATH
+PATH=$SCRIPTPATH/../../../python/sfem/mesh:$PATH
+
+if (($# != 8))
+then
+	printf "usage: $0 <cell_type> <nx> <ny> <nz> <width> <height> <depth> <output_dir>\n" 1>&2
+	exit -1
+fi
+
+cell_type=$1
+
+x=$2
+y=$3
+z=$4
+
+width=$5
+height=$6
+depth=$7
+
+output_dir=$8
+
+box_mesh.py $output_dir 		\
+	--cell_type=$cell_type 		\
+	-x $x -y $y -z $z  			\
+	--width=$width --height=$height --depth=$depth
+raw_to_db.py $output_dir $output_dir/mesh.vtk   
