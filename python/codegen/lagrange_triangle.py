79c0a6f3573664168ccdd2c80234a7101e889ee7
Patrick Zulian <patrick.zulian@gmail.com>
2024-05-05 17:40:56 +0200

    small fix

--- python/codegen/lagrange_triangle.py
+++ python/codegen/lagrange_triangle.py
@@ -4,6 +4,17 @@ from fe import FE
 import sympy as sp
 from sfem_codegen import *
 
+see_lagrs = False
+# see_lagrs = True
+
+def nsimplify(expr):
+	return sp.nsimplify(expr)
+	# return expr
+
+def simplify(expr):
+	# return sp.simplify(expr)
+	return expr
+
 def lagrange_triangle(m, x, y):
 	l = [1 - x - y, x, y]
 
@@ -25,51 +36,61 @@ def lagrange_triangle(m, x, y):
 			denom *= sp.Rational((k - i), m)
 		return num / denom
 
-	def f(i, j, k, x, y):
-		return sp.nsimplify(lagr(i, L0(x, y)) * lagr(j, L1(x, y)) * lagr(k, L2(x, y)))
-
 	def lf(i, j, k):
-		l0, l1, l2 = sp.symbols('l0 l1 l2', positive=True, real=True)
-		return sp.nsimplify(lagr(i, l0) * lagr(j, l1) * lagr(k, l2))
+
+		if see_lagrs:
+			l0, l1, l2 = sp.symbols('l0 l1 l2', positive=True, real=True)
+		else:
+			l0 = lagr(i, L0(x, y))
+			l1 = lagr(j, L1(x, y))
+			l2 = lagr(k, L2(x, y))
+		
+		return nsimplify(lagr(i, l0) * lagr(j, l1) * lagr(k, l2))
 
 	f = []
 
+	####################################
 	# Corners
-	f.append(sp.nsimplify(lf(m, 0, 0)))
-	f.append(sp.nsimplify(lf(0, m, 0)))
-	f.append(sp.nsimplify(lf(0, 0, m)))
+	####################################
+	f.append(simplify(lf(m, 0, 0)))
+	f.append(simplify(lf(0, m, 0)))
+	f.append(simplify(lf(0, 0, m)))
 	idx = 3
 
+	####################################
 	# Edges
-
+	####################################
 	for i in range(1, m):
-		f.append(sp.nsimplify(lf(m-i, i, 0)))
+		f.append(simplify(lf(m-i, i, 0)))
 	idx += (m-1)
 
 	for i in range(1, m):
-		f.append(sp.nsimplify(lf(0, m-i, i)))
+		f.append(simplify(lf(0, m-i, i)))
 	idx += (m-1)
 
 	for i in range(1, m):
-		f.append(sp.nsimplify(lf(i, 0, m-i)))
+		f.append(simplify(lf(i, 0, m-i)))
 	idx += (m-1)
 
+	####################################
 	# Face
-	for i in range(1, m-1):
-		for j in range(1, m-1):
-			for k in range(1, m-1):
+	####################################
+	for i in reversed(range(1, m-1)):
+		for j in reversed(range(1, m-1)):
+			for k in reversed(range(1, m-1)):
 				if i + j + k != m:
 					continue
-
-				f.append(sp.nsimplify(lf(i, j, k)))
-
+				f.append(simplify(lf(i, j, k)))
 	return f
 
 if __name__ == '__main__':
-	x, y = sp.symbols('x y', postive=True, real=True)
+	x, y = sp.symbols('x y', positive=True, real=True)
 	m = 3
 
 	f = lagrange_triangle(m, x, y)
 
 	for i in range(0, len(f)):
-		print(f'f[{i}] = {f[i]}')
+		print(f'f[{i}] = {simplify(f[i])}')
+
+
+	c_code(f)
