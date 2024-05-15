Trying to fix tet10 hessian

tests/test_linear_elasticity.py
@@ -45,16 +45,23 @@ def solve_linear_elasticity(options):
 	x = np.zeros(fs.n_dofs())
 	cg = sfem.ConjugateGradient()
 	cg.default_init()
-	cg.set_max_it(3000)
+	cg.set_max_it(1000)
 
 	lop = sfem.make_op(fun, x)
 	cg.set_op(lop)
 
-	d = np.zeros(fs.n_dofs())
-	sfem.hessian_diag(fun, x, d)
-
-	prec = sfem.diag(1./d)
-	cg.set_preconditioner_op(prec)
+	# use_prec = True
+	use_prec = False
+	if use_prec:
+		d = np.zeros(fs.n_dofs())
+		sfem.hessian_diag(fun, x, d)
+		prec = sfem.diag(1./d)
+		cg.set_preconditioner_op(prec)
+	
+		# print(np.max(d))
+		# print(np.min(d))
+		# print(np.max(np.abs(d)))
+		# print(np.min(np.abs(d)))
 	
 	g = np.zeros(fs.n_dofs())
 	sfem.apply_constraints(fun, x)