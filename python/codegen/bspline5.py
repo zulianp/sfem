8ec2f8f9af60099e2d766fa4ca92ca35b9402a4a
Patrick Zulian <patrick.zulian@gmail.com>
2024-06-14 09:47:14 +0200

    Added specialization of bspline5

--- python/codegen/bspline5.py
+++ python/codegen/bspline5.py
@@ -12,6 +12,15 @@ def bspline(i, order, t):
 	else:
 		return (t - i)/(i+order-1 - i) * bspline(i, order-1, t) + (i+order - t)/(i+order - (i+1)) * bspline(i+1, order-1, t) 
 
+def bspline5(i, order, t):
+	if order == 1:
+		if i == 2:
+			return 1
+		else:
+			return 0
+	else:
+		return (t - i)/(i+order-1 - i) * bspline5(i, order-1, t) + (i+order - t)/(i+order - (i+1)) * bspline5(i+1, order-1, t) 
+
 order = 5
 extras = 0
 nctrl = int((order + 1) + extras)
@@ -21,25 +30,32 @@ if True:
 	x = np.linspace(0, nctrl-1, nctrl)
 	y = np.sin(x * (2*3.14/nctrl)) + np.sin(x * (4.4*3.14/nctrl))
 
-	n = 100
-	t = np.linspace(0, nctrl-1, n)
+	n = 40
+	t = np.linspace(2, 3, n)
 	yt = np.zeros(t.shape)
+	ytref = np.zeros(t.shape)
 
 	b = [0] * nctrl
+	bref = [0] * nctrl
 
 	for j in range(0, nctrl):
 		b[j] = np.zeros(t.shape)
+		bref[j] = np.zeros(t.shape)
 
 	for i in range(0, n):
 		ti = t[i]
 
 		f = 0
+		fref = 0
 		for j in range(0, nctrl):
-			bj = bspline(j-(order+1)/2, order+1, ti)
-			b[j][i] = bj
-			f += y[j] * bj
+			b[j][i] = bspline5(j-(order+1)/2, order+1, ti)
+			f += y[j] * b[j][i] 
+
+			bref[j][i] = bspline(j-(order+1)/2, order+1, ti)
+			fref += y[j] * bref[j][i]
 
 		yt[i] = f
+		ytref[i] = fref
 
 
 	fig, axs = plt.subplots(2)
@@ -48,22 +64,26 @@ if True:
 	axs[0].axvspan(2, 3, color='green', alpha=0.2)
 
 	axs[0].plot(x, y, marker='8')
-	axs[0].plot(t, yt)
-	axs[0].set_title("Evalutation (valid interval is [2-3])")
+	axs[0].plot(t, yt, label='spline5')
+	axs[0].plot(t, ytref, label='spline ref')
+	axs[0].set_title("Evalutation (evaluation assumed in interval is [2-3])")
+	axs[0].legend()
 
-	for j in range(0, nctrl):
-		axs[1].plot(t, b[j])
+	# for j in range(0, nctrl):
+	for j in [2, 3]:
+		axs[1].plot(t, b[j],  label=f'b{j}')
+		axs[1].plot(t, bref[j],  marker='.', label=f'ref{j}')
 
 	axs[1].set_title("Basis functions")
+	axs[1].legend()
 	fig.tight_layout()
-	plt.savefig("bspline.pdf")
+	plt.savefig("bspline5.pdf")
 
 expr = []
 t = sp.symbols('t', positive=True)
 for j in range(0, nctrl):
-	s = bspline(j-(order+1)/2,order+1, t)
+	s = bspline5(j-(order+1)/2,order+1, t)
 	var = sp.symbols(f'w[{j}]') 
 	expr.append(ast.Assignment(var, s))
 
-# Generated code is garbage with this solution
 c_code(expr)
