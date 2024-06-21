9c5dc8a0180c126770cb10509dcc73b9cbf548ee
Patrick Zulian <patrick.zulian@gmail.com>
2024-06-21 16:14:55 +0200

    refactored laplacian tri3, tri6

--- operators/tri3/tri3_laplacian.h
+++ operators/tri3/tri3_laplacian.h
@@ -1,31 +1,48 @@
 #ifndef TRI3_LAPLACIAN_H
 #define TRI3_LAPLACIAN_H
 
-#include "sfem_base.h"
 #include <stddef.h>
+#include "sfem_base.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+// Code generated with laplace_op.py (mixed symbolic and numerical integration)
 
+int tri3_laplacian_assemble_value(const ptrdiff_t nelements,
+                                  const ptrdiff_t nnodes,
+                                  idx_t **const SFEM_RESTRICT elems,
+                                  geom_t **const SFEM_RESTRICT xyz,
+                                  const real_t *const SFEM_RESTRICT u,
+                                  real_t *const SFEM_RESTRICT value);
 
-void tri3_laplacian_assemble_hessian(const ptrdiff_t nelements,
-                                const ptrdiff_t nnodes,
-                                idx_t **const SFEM_RESTRICT elems,
-                                geom_t **const SFEM_RESTRICT xyz,
-                                const count_t *const SFEM_RESTRICT rowptr,
-                                const idx_t *const SFEM_RESTRICT colidx,
-                                real_t *const SFEM_RESTRICT values);
+int tri3_laplacian_apply(const ptrdiff_t nelements,
+                         const ptrdiff_t nnodes,
+                         idx_t **const SFEM_RESTRICT elems,
+                         geom_t **const SFEM_RESTRICT xyz,
+                         const real_t *const SFEM_RESTRICT u,
+                         real_t *const SFEM_RESTRICT values);
 
-void tri3_laplacian_apply(const ptrdiff_t nelements,
-                          const ptrdiff_t nnodes,
-                          idx_t **const SFEM_RESTRICT elems,
-                          geom_t **const SFEM_RESTRICT xyz,
-                          const real_t *const SFEM_RESTRICT u,
-                          real_t *const SFEM_RESTRICT values);
+int tri3_laplacian_assemble_hessian(const ptrdiff_t nelements,
+                                    const ptrdiff_t nnodes,
+                                    idx_t **const SFEM_RESTRICT elems,
+                                    geom_t **const SFEM_RESTRICT xyz,
+                                    const count_t *const SFEM_RESTRICT rowptr,
+                                    const idx_t *const SFEM_RESTRICT colidx,
+                                    real_t *const SFEM_RESTRICT values);
 
-#ifdef __cplusplus
-}
-#endif
+int tri3_laplacian_diag(const ptrdiff_t nelements,
+                        const ptrdiff_t nnodes,
+                        idx_t **const SFEM_RESTRICT elements,
+                        geom_t **const SFEM_RESTRICT points,
+                        real_t *const SFEM_RESTRICT diag);
 
-#endif //TRI3_LAPLACIAN_H
+int tri3_laplacian_apply_opt(const ptrdiff_t nelements,
+                             idx_t **const SFEM_RESTRICT elements,
+                             const jacobian_t *const SFEM_RESTRICT fff,
+                             const real_t *const SFEM_RESTRICT u,
+                             real_t *const SFEM_RESTRICT values);
+
+int tri3_laplacian_diag_opt(const ptrdiff_t nelements,
+                            idx_t **const SFEM_RESTRICT elements,
+                            const jacobian_t *const SFEM_RESTRICT fff,
+                            real_t *const SFEM_RESTRICT diag);
+
+#endif  // TRI3_LAPLACIAN_H
