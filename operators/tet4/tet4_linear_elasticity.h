a8c105bb4b6514962f09806ad609f13d38125f4b
Patrick Zulian <patrick.zulian@gmail.com>
2024-06-24 16:13:59 +0200

    clean-up

--- operators/tet4/tet4_linear_elasticity.h
+++ operators/tet4/tet4_linear_elasticity.h
@@ -35,6 +35,21 @@ int tet4_linear_elasticity_apply(const ptrdiff_t nelements,
                                  real_t *const outy,
                                  real_t *const outz);
 
+int tet4_linear_elasticity_apply_opt(const ptrdiff_t nelements,
+                                 idx_t **const SFEM_RESTRICT elements,
+                                 const jacobian_t *const SFEM_RESTRICT g_jacobian_adjugate,
+                                 const jacobian_t *const SFEM_RESTRICT g_jacobian_determinant,
+                                 const real_t mu,
+                                 const real_t lambda,
+                                 const ptrdiff_t u_stride,
+                                 const real_t *const ux,
+                                 const real_t *const uy,
+                                 const real_t *const uz,
+                                 const ptrdiff_t out_stride,
+                                 real_t *const outx,
+                                 real_t *const outy,
+                                 real_t *const outz);
+
 int tet4_linear_elasticity_diag(const ptrdiff_t nelements,
                                 const ptrdiff_t nnodes,
                                 idx_t **const SFEM_RESTRICT elements,
@@ -46,6 +61,17 @@ int tet4_linear_elasticity_diag(const ptrdiff_t nelements,
                                 real_t *const outy,
                                 real_t *const outz);
 
+int tet4_linear_elasticity_diag_opt(const ptrdiff_t nelements,
+                                    idx_t **const SFEM_RESTRICT elements,
+                                    const jacobian_t *const SFEM_RESTRICT g_jacobian_adjugate,
+                                    const jacobian_t *const SFEM_RESTRICT g_jacobian_determinant,
+                                    const real_t mu,
+                                    const real_t lambda,
+                                    const ptrdiff_t out_stride,
+                                    real_t *const outx,
+                                    real_t *const outy,
+                                    real_t *const outz);
+
 int tet4_linear_elasticity_hessian(const ptrdiff_t nelements,
                                    const ptrdiff_t nnodes,
                                    idx_t **const SFEM_RESTRICT elements,
@@ -55,6 +81,9 @@ int tet4_linear_elasticity_hessian(const ptrdiff_t nelements,
                                    const count_t *const SFEM_RESTRICT rowptr,
                                    const idx_t *const SFEM_RESTRICT colidx,
                                    real_t *const SFEM_RESTRICT values);
+
+
+
 
 #ifdef __cplusplus
 }
