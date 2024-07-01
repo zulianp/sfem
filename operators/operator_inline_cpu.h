66c43263b2bd7bf520688af8e85fd1e2c93e41f3
Patrick Zulian <patrick.zulian@gmail.com>
2024-07-01 21:54:15 +0200

    ...

--- operators/operator_inline_cpu.h
+++ operators/operator_inline_cpu.h
@@ -18,7 +18,11 @@ typedef double scalar_t;
 typedef real_t scalar_t;
 #endif
 
+// TODO play around with accumulator precision
 typedef real_t accumulator_t;
+
 typedef scalar_t vec_t
         __attribute__((vector_size(SFEM_VEC_SIZE * sizeof(scalar_t)), aligned(sizeof(scalar_t))));
-#endif
+
+
+#endif //OPERATOR_INLINE_CPU_H
