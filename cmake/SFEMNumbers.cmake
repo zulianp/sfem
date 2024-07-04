df8f72279aeb932174c299231cb0b1ae64e71ca6
Patrick Zulian <patrick.zulian@gmail.com>
2024-07-04 12:00:34 +0200

    Made sure that make files also works

--- cmake/SFEMNumbers.cmake
+++ cmake/SFEMNumbers.cmake
@@ -2,18 +2,32 @@
 # XSDK_PRECISION
 # ##############################################################################
 
-option(SFEM_ENABLE_CUSTOM_NUMBERS "Enable custom numbers support. If OFF uses default" ON)
+option(SFEM_ENABLE_CUSTOM_NUMBERS "Enable custom numbers support. (this option will be removed in the future)" ON)
 
 set(SFEM_REAL_TYPE          "float64"   CACHE STRING "Set SFEM real_t type. Used for solution vectors.")
 set(SFEM_SCALAR_TYPE        "float64"   CACHE STRING "Set SFEM scalar_t type. Used for local kernel computations")
 set(SFEM_GEOM_TYPE          "float32"   CACHE STRING "Set SFEM geom_t type")
 set(SFEM_JACOBIAN_CPU_TYPE  "float32"   CACHE STRING "Set SFEM jacobian_t type")
 set(SFEM_JACOBIAN_GPU_TYPE  "half"      CACHE STRING "Set SFEM cu_jacobian_t type")
+set(SFEM_ACCUMULATOR_TYPE   "float64"   CACHE STRING "Set SFEM accumulator_t type")
 
 set(SFEM_IDX_TYPE           "int32"     CACHE STRING "Set SFEM idx_t type")
 set(SFEM_COUNT_TYPE         "int64"     CACHE STRING "Set SFEM count_t type")
 set(SFEM_ELEMENT_IDX_TYPE   "int32"     CACHE STRING "Set SFEM element_idx_t type")
 set(SFEM_LOCAL_IDX_TYPE     "int16"     CACHE STRING "Set SFEM local_idx_t type")
+
+function(sfem_simd_vector_size type vec_size)
+    if("${type}" STREQUAL "float32")
+        set(${vec_size} 8 PARENT_SCOPE)
+    elseif("${type}" STREQUAL "float64")
+        set(${vec_size} 4 PARENT_SCOPE)
+    else()
+        message(FATAL_ERROR "Not simd vector size type for `${type}`!")
+    endif()
+endfunction()
+
+sfem_simd_vector_size(${SFEM_SCALAR_TYPE} SFEM_VEC_SIZE_DEFAUT)
+set(SFEM_VEC_SIZE  ${SFEM_VEC_SIZE_DEFAUT}   CACHE STRING "Set number of simd lanes for vec_t type")
 
 function(sfem_c_type type c_type)
     if("${type}" STREQUAL "int16")
@@ -76,7 +90,7 @@ function(sfem_print_type type print_type)
     elseif("${type}" STREQUAL "int32")
         set(${print_type} "d" PARENT_SCOPE)
     elseif("${type}" STREQUAL "int64")
-        set(${print_type} "l" PARENT_SCOPE)
+        set(${print_type} "ld" PARENT_SCOPE)
     # elseif("${type}" STREQUAL "float16")
     #     set(${print_type} "NO direct MPI Support" PARENT_SCOPE)
     elseif("${type}" STREQUAL "float32")
@@ -93,6 +107,7 @@ sfem_c_type(${SFEM_REAL_TYPE} SFEM_REAL_C_TYPE)
 sfem_c_type(${SFEM_SCALAR_TYPE} SFEM_SCALAR_C_TYPE)
 sfem_c_type(${SFEM_GEOM_TYPE} SFEM_GEOM_C_TYPE)
 sfem_c_type(${SFEM_JACOBIAN_CPU_TYPE} SFEM_JACOBIAN_CPU_C_TYPE)
+sfem_c_type(${SFEM_ACCUMULATOR_TYPE} SFEM_ACCUMULATOR_C_TYPE)
 sfem_c_type(${SFEM_IDX_TYPE} SFEM_IDX_C_TYPE)
 sfem_c_type(${SFEM_COUNT_TYPE} SFEM_COUNT_C_TYPE)
 sfem_c_type(${SFEM_ELEMENT_IDX_TYPE} SFEM_ELEMENT_IDX_C_TYPE)
@@ -128,7 +143,6 @@ sfem_print_type(${SFEM_COUNT_TYPE} SFEM_COUNT_PRINT_TYPE)
 sfem_print_type(${SFEM_ELEMENT_IDX_TYPE} SFEM_ELEMENT_IDX_PRINT_TYPE)
 sfem_print_type(${SFEM_LOCAL_IDX_TYPE} SFEM_LOCAL_IDX_PRINT_TYPE)
 
-
 message(STATUS 
     "\nSFEM Numbers\n"
     "--------------------------------------------------------------------------------------\n"
@@ -144,6 +158,9 @@ message(STATUS
     "element_idx_t\t${SFEM_ELEMENT_IDX_C_TYPE}\t${SFEM_ELEMENT_IDX_TYPE}\t${SFEM_ELEMENT_IDX_MPI_TYPE}\t${SFEM_ELEMENT_IDX_PRINT_TYPE}\t(SFEM_ELEMENT_IDX_TYPE)\n"
     "local_idx_t\t${SFEM_LOCAL_IDX_C_TYPE}\t${SFEM_LOCAL_IDX_TYPE}\t${SFEM_LOCAL_IDX_MPI_TYPE}\t${SFEM_LOCAL_IDX_PRINT_TYPE}\t(SFEM_LOCAL_IDX_TYPE)\n"
     "--------------------------------------------------------------------------------------\n"
+    "SFEM_VEC_SIZE=${SFEM_VEC_SIZE}\n"
+    "--------------------------------------------------------------------------------------\n"
+
 )
 
 # if(NOT XSDK_PRECISION OR USE_XSDK_DEFAULTS)
--- operators/operator_inline_cpu.h
+++ operators/operator_inline_cpu.h
@@ -4,25 +4,4 @@
 #include "sfem_base.h"
 #include "sfem_vec.h"
 
-#ifdef SFEM_ENABLE_FP32_KERNELS
-// Single precision indepent from real_t
-#define SFEM_VEC_SIZE 8
-typedef float scalar_t;
-#elif SFEM_ENABLE_FP64_KERNELS
-// Double precision indepent from real_t
-#define SFEM_VEC_SIZE 4
-typedef double scalar_t;
-#else
-// Same precision as for real_t
-#define SFEM_VEC_SIZE SFEM_VREAL_SIZE
-typedef real_t scalar_t;
-#endif
-
-// TODO play around with accumulator precision
-typedef real_t accumulator_t;
-
-typedef scalar_t vec_t
-        __attribute__((vector_size(SFEM_VEC_SIZE * sizeof(scalar_t)), aligned(SFEM_VEC_SIZE * sizeof(scalar_t))));
-
-
 #endif //OPERATOR_INLINE_CPU_H
