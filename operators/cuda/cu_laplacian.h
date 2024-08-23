c7a9754e9f17595b00e5465ce47a7865d4d52a98
Patrick Zulian <patrick.zulian@gmail.com>
2024-08-23 09:59:24 +0200

    CRS assembly for laplacian, missing BC

--- operators/cuda/cu_laplacian.h
+++ operators/cuda/cu_laplacian.h
@@ -12,7 +12,7 @@ extern "C" {
 
 int cu_laplacian_apply(const enum ElemType element_type,
                        const ptrdiff_t nelements,
-                       const ptrdiff_t stride, // Stride for elements and fff
+                       const ptrdiff_t stride,  // Stride for elements and fff
                        const idx_t *const SFEM_RESTRICT elements,
                        const void *const SFEM_RESTRICT fff,
                        const enum RealType real_type_xy,
@@ -22,12 +22,23 @@ int cu_laplacian_apply(const enum ElemType element_type,
 
 int cu_laplacian_diag(const enum ElemType element_type,
                       const ptrdiff_t nelements,
-                      const ptrdiff_t stride, // Stride for elements and fff
+                      const ptrdiff_t stride,  // Stride for elements and fff
                       const idx_t *const SFEM_RESTRICT elements,
                       const void *const SFEM_RESTRICT fff,
                       const enum RealType real_type_xy,
                       void *const SFEM_RESTRICT diag,
                       void *stream);
+
+int cu_laplacian_crs(const enum ElemType element_type,
+                     const ptrdiff_t nelements,
+                     const ptrdiff_t stride,  // Stride for elements and fff
+                     const idx_t *const SFEM_RESTRICT elements,
+                     const void *const SFEM_RESTRICT fff,
+                     const count_t *const SFEM_RESTRICT rowptr,
+                     const idx_t *const SFEM_RESTRICT colidx,
+                     const enum RealType real_type,
+                     void *const SFEM_RESTRICT values,
+                     void *stream);
 
 #ifdef __cplusplus
 }
