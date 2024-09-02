4bf44f8d27e7147daf066f742496f48ecfa74471
Patrick Zulian <patrick.zulian@gmail.com>
2024-09-02 16:56:30 +0200

    ...

--- tests/hex8_laplacian/hex8_laplacian.sh
+++ tests/hex8_laplacian/hex8_laplacian.sh
@@ -4,9 +4,7 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-
 export PATH=$SCRIPTPATH/../../:$PATH
-export PATH=$SCRIPTPATH/../../bin/:$PATH
 export PATH=$SCRIPTPATH/../../../matrix.io:$PATH
 
 export PATH=$SCRIPTPATH:$PATH
@@ -31,15 +29,23 @@ fi
 export OMP_NUM_THREADS=8
 export OMP_PROC_BIND=true 
 
-
 if [[ -d "hex8_mesh" ]]
 then
 	echo "Reusing mesh"
 else
-	N=100
+	N=150
 	box_mesh.py hex8_mesh -c hex8 -x $N -y $N -z $N --height=1 --width=1 --depth=1
 	box_mesh.py tet4_mesh -c tet4 -x $N -y $N -z $N --height=1 --width=1 --depth=1
+
+	N_coarse=$(( N/2 + 1 ))
+	box_mesh.py tet4_mesh_coarse -c tet4 -x $N_coarse -y $N_coarse -z $N_coarse --height=1 --width=1 --depth=1
+	mesh_p1_to_p2 tet4_mesh_coarse macro_tet4_mesh
 fi
 
 laplacian_apply hex8_mesh gen:ones hex8_AxU.raw
 laplacian_apply tet4_mesh gen:ones tet4_AxU.raw
+
+SFEM_USE_MACRO=0 laplacian_apply macro_tet4_mesh gen:ones tet10_AxU.raw
+SFEM_USE_MACRO=1 laplacian_apply macro_tet4_mesh gen:ones macro_tet4_AxU.raw
+
+
