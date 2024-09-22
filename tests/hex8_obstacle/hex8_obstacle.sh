fb227a853b1ab022150920eacfc095d4d521fad2
Patrick Zulian <patrick.zulian@gmail.com>
2024-09-22 16:22:44 +0200

    ...

--- tests/hex8_obstacle/hex8_obstacle.sh
+++ tests/hex8_obstacle/hex8_obstacle.sh
@@ -29,7 +29,7 @@ export OMP_PROC_BIND=true
 export CUDA_LAUNCH_BLOCKING=0
 
 export SFEM_ELEMENT_TYPE=PROTEUS_HEX8 
-export SFEM_ELEMENT_REFINE_LEVEL=6
+export SFEM_ELEMENT_REFINE_LEVEL=8
 
 mesh=mesh
 
@@ -38,28 +38,41 @@ then
 	echo "Reusing mesh"
 else
 	# Cyclic mesh
-	# create_cyclic_ss_mesh.sh 1 $SFEM_ELEMENT_REFINE_LEVEL
+	create_cyclic_ss_mesh.sh 1 $SFEM_ELEMENT_REFINE_LEVEL
 
 	# BOX mesh for testing
-	create_box_ss_mesh.sh 1 $SFEM_ELEMENT_REFINE_LEVEL
+	# create_box_ss_mesh.sh 8 $SFEM_ELEMENT_REFINE_LEVEL
 fi
 
 # Cyclic mesh
-# sinlet=$mesh/surface/sidesets_aos/inlet.raw
-# soutlet=$mesh/surface/sidesets_aos/outlet.raw
+sinlet=$mesh/surface/sidesets_aos/inlet.raw
+soutlet=$mesh/surface/sidesets_aos/outlet.raw
+sobstacle=$mesh/surface/sidesets_aos/wall1.raw
+./compute_distance.py mesh/viz/ $sobstacle ub.raw
 
 # Box mesh for testing
-sinlet=$mesh/surface/sidesets_aos/left.raw 
-soutlet=$mesh/surface/sidesets_aos/right.raw 
+# sinlet=$mesh/surface/sidesets_aos/left.raw 
+# soutlet=$mesh/surface/sidesets_aos/right.raw 
 
 SQP=1
 export SFEM_USE_ELASTICITY=1
 
 if [[ $SFEM_USE_ELASTICITY -eq 1 ]]
 then
-	export SFEM_DIRICHLET_NODESET="$sinlet,$sinlet,$sinlet"
-	export SFEM_DIRICHLET_VALUE="0,1,0"
-	export SFEM_DIRICHLET_COMPONENT="0,1,2"
+	if [[ $SQP -eq 1 ]]
+	then
+		export SFEM_DIRICHLET_NODESET="$sinlet,$sinlet,$sinlet,$soutlet,$soutlet,$soutlet"
+		export SFEM_DIRICHLET_VALUE="0,0.2,0,0,0.2,0"
+		export SFEM_DIRICHLET_COMPONENT="0,1,2,0,1,2"
+
+		export SFEM_CONTACT_NODESET="$sobstacle"
+		export SFEM_CONTACT_VALUE="path:ub.raw"
+		export SFEM_CONTACT_COMPONENT="1"
+	else
+		export SFEM_DIRICHLET_NODESET="$sinlet,$sinlet,$sinlet,$soutlet,$soutlet,$soutlet"
+		export SFEM_DIRICHLET_VALUE="0,0.1,0,0,-0.1,0"
+		export SFEM_DIRICHLET_COMPONENT="0,1,2,0,1,2"
+	fi
 else
 	if [[ $SQP -eq 1 ]]
 	then
@@ -100,8 +113,8 @@ then
 		mv output/soa/$name".2.raw" output/soa/"$var".2."$ts".raw
 	done
 
-	raw_to_db.py $mesh/viz $mesh/viz/hex8.vtk  --point_data="output/soa/*.raw"
+	raw_to_db.py $mesh/viz output/hex8.vtk  --point_data="output/soa/*.raw"
 
 else
-	raw_to_db.py $mesh/viz $mesh/viz/hex8.vtk --point_data=output/u.raw,output/rhs.raw,output/upper_bound.raw --point_data_type="$SFEM_REAL_T,$SFEM_REAL_T,$SFEM_REAL_T"
+	raw_to_db.py $mesh/viz output/hex8.vtk --point_data=output/u.raw,output/rhs.raw,output/upper_bound.raw --point_data_type="$SFEM_REAL_T,$SFEM_REAL_T,$SFEM_REAL_T"
 fi
