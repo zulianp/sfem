ddcac69023def11c0659a003384db942c09e2fd0
Patrick Zulian <patrick.zulian@gmail.com>
2024-10-10 16:22:52 +0200

    cu_proteus_affine_hex8_laplacian_apply_kernel needs to be heavily optimized!

--- tests/steady_state/test_mg.sh
+++ tests/steady_state/test_mg.sh
@@ -20,7 +20,7 @@ source $SFEM_DIR/workflows/sfem_config.sh
 export OMP_NUM_THREADS=8
 export OMP_PROC_BIND=true 
 export CUDA_LAUNCH_BLOCKING=0
-export SFEM_ELEMENT_REFINE_LEVEL=8
+export SFEM_ELEMENT_REFINE_LEVEL=2
 
 mesh=mesh
 
@@ -29,14 +29,14 @@ then
 	echo "Reusing mesh"
 else
 	export SFEM_BOX_SIZE=1
-	create_box_ss_mesh.sh 40 $SFEM_ELEMENT_REFINE_LEVEL
+	create_box_ss_mesh.sh 2 $SFEM_ELEMENT_REFINE_LEVEL
 fi
 
-export SFEM_BLOCK_SIZE=3
-export SFEM_OPERATOR="LinearElasticity"
+# export SFEM_BLOCK_SIZE=3
+# export SFEM_OPERATOR="LinearElasticity"
 
-# export SFEM_BLOCK_SIZE=1
-# export SFEM_OPERATOR="Laplacian"
+export SFEM_BLOCK_SIZE=1
+export SFEM_OPERATOR="Laplacian"
 
 
 # export SFEM_SHEAR_MODULUS=1
