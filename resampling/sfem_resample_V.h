bb320cf23ecdf69bff997c57b7734d4c6edd059e
Simone <sriva@susePC>
2024-11-06 15:24:27 +0100

    refactor resample field for CPU

--- resampling/sfem_resample_V.h
+++ resampling/sfem_resample_V.h
@@ -21,6 +21,17 @@ int tet4_resample_field_local_v2(
         // Output
         real_t* const SFEM_RESTRICT weighted_field);
 
+int tet4_resample_field_local_V(
+        // Mesh
+        const ptrdiff_t nelements, const ptrdiff_t nnodes, idx_t** const SFEM_RESTRICT elems,
+        geom_t** const SFEM_RESTRICT xyz,
+        // SDF
+        const ptrdiff_t* const SFEM_RESTRICT n, const ptrdiff_t* const SFEM_RESTRICT stride,
+        const geom_t* const SFEM_RESTRICT origin, const geom_t* const SFEM_RESTRICT delta,
+        const real_t* const SFEM_RESTRICT data,
+        // Output
+        real_t* const SFEM_RESTRICT weighted_field);
+
 int tet4_resample_field_local_V8(
         // Mesh
         const ptrdiff_t nelements, const ptrdiff_t nnodes, idx_t** const SFEM_RESTRICT elems,
