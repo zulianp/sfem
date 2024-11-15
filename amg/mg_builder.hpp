4d444be2bdf2ec6510f083ee976aa3a61c8fc91a
Austen Nelson <ajn6@pdx.edu>
2024-11-15 05:45:56 -0800

    broken prototype

--- amg/mg_builder.hpp
+++ amg/mg_builder.hpp
@@ -1,10 +1,11 @@
 #ifndef MG_BUILDER_H
 #define MG_BUILDER_H
 
+#include "sfem_CooSym.hpp"
 #include "sfem_Multigrid.hpp"
-#include "sparse.h"
 
-std::shared_ptr<sfem::Multigrid<real_t>> builder(const real_t coarsening_factor,
-                                                 const mask_t *bdy_dofs,
-                                                 SymmCOOMatrix *fine_mat);
+std::shared_ptr<sfem::Multigrid<real_t>> builder(
+        const real_t coarsening_factor,
+        const mask_t *bdy_dofs,
+        std::shared_ptr<sfem::CooSymSpMV<idx_t, real_t>> &fine_mat);
 #endif  // MG_BUILDER_H
