699a4b1de051f5413105e07b5c2ec9c8051bca01
Austen Nelson <ajn6@pdx.edu>
2024-11-15 06:06:05 -0800

    fixed lpsmoothers...

--- amg/smoother.h
+++ amg/smoother.h
@@ -3,19 +3,10 @@
 
 #include "sparse.h"
 
-ptrdiff_t dim;  // Number of rows / columns
-
-count_t offdiag_nnz;         // Length of 3 below arrays, or half the actualy number of
-                             // off diagonal non-zero entries
-idx_t *offdiag_row_indices;  // Row indices of off diagonal non-zero entries
-idx_t *offdiag_col_indices;  // Column indices of off diagonal non-zero entries
-real_t *offdiag_values;      // Off diagonal non-zero values
-
-real_t *diag;  // Diagonal values
-
 int l2_smoother(const ptrdiff_t dim,
                 const count_t offdiag_nnz,
                 const real_t *const diag,
+                const real_t *const offdiag_values,
                 const idx_t *const offdiag_row_indices,
                 const idx_t *const offdiag_col_indices,
                 real_t *smoother);
@@ -23,6 +14,7 @@ int l2_smoother(const ptrdiff_t dim,
 int l1_smoother(const ptrdiff_t dim,
                 const count_t offdiag_nnz,
                 const real_t *const diag,
+                const real_t *const offdiag_values,
                 const idx_t *const offdiag_row_indices,
                 const idx_t *const offdiag_col_indices,
                 real_t *smoother);
