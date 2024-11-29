76e555e0f122f2dff72f308a0f8fc80ba34997e6
Patrick Zulian <patrick.zulian@gmail.com>
2024-11-29 18:35:26 +0100

    added vector BC

--- tests/sdf_obstacle/sdf_obstacle.py
+++ tests/sdf_obstacle/sdf_obstacle.py
@@ -16,6 +16,23 @@ except ImportError:
 
 # -----------------------
 
+# TODO move to pysfem
+def create_dirichlet_conditions(fs, config):
+	dirichlet_conditions = sfem.DirichletConditions(fs)
+	for c in config:
+		name = c['name']
+		nodeset = c['nodeset']
+		component = c['component']
+		value = c['value']
+
+		idx = np.unique(np.fromfile(nodeset, dtype=idx_t))
+		if isinstance(component, list):
+			for k in range(0, len(component)):
+				sfem.add_condition(dirichlet_conditions, idx, component[k], value[k])
+		else:
+			sfem.add_condition(dirichlet_conditions, idx, component, value)
+	return dirichlet_conditions
+
 def run(case):
 	m = sfem.Mesh()
 	m.read(case['mesh'])
@@ -24,21 +41,12 @@ def run(case):
 	block_size = case['block_size']
 
 	fs = sfem.FunctionSpace(m, block_size)
-	cc = sfem.contact_conditions_from_file(fs, str(config['obstacle']))
 
 	fun = sfem.Function(fs)
 	op = sfem.create_op(fs, case["operator"])
 	fun.add_operator(op)
 
-	dirichlet_conditions = sfem.DirichletConditions(fs)
-	for c in case['dirichlet_conditions']:
-		name = c['name']
-		nodeset = c['nodeset']
-		component = c['component']
-		value = c['value']
-
-		idx = np.unique(np.fromfile(nodeset, dtype=idx_t))
-		sfem.add_condition(dirichlet_conditions, idx, component, value);
+	dirichlet_conditions = create_dirichlet_conditions(fs, case['dirichlet_conditions'])
 
 	fun.add_dirichlet_conditions(dirichlet_conditions)
 	fun.set_output_dir(config['output'])
@@ -56,11 +64,16 @@ def run(case):
 
 	sfem.apply_constraints(fun, x)
 	sfem.gradient(fun, x, g)
+
+	print(f'Solving system with {fs.n_dofs()} dofs')
 	sfem.apply(cg, g, c)
 
 	x -= c
 
 	sfem.report_solution(fun, x)
+
+	# TODO
+	# cc = sfem.contact_conditions_from_file(fs, str(config['obstacle']))
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
