abb8c29cf06921dab22cd4c8a2384c377570c417
Simone <sriva@susePC>
2024-12-10 18:04:31 +0100

    Fix variable name typo and improve comments in CUDA resampling functions; update output path in sdf_test.py

--- python/sfem/sdf/sdf_test.py
+++ python/sfem/sdf/sdf_test.py
@@ -7,9 +7,10 @@ import time
 
 
 output_path = os.path.join(os.environ['HOME'], 'git/sfem/workflows/resample/sdf.float32.raw')
-output_path = os.path.join(os.environ['SCRATCH'], 'prj/sfem/workflows/resample/sdf.float32.raw')
+# output_path = os.path.join(os.environ['SCRATCH'], 'prj/sfem/workflows/resample/sdf.float32.raw')
 
 print("sdf_test.py: ==========================================")
+print(f'sdf_test.py: Writing field to {output_path}')
 
 sdf_t = np.float32
 
