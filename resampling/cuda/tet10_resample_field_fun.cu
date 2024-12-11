36a4d733171af48e7e35759f6bc47d5ec709a1f5
Simone <sriva@susePC>
2024-12-11 16:49:16 +0100

    Add managed memory functions for TET10 elements; improve comments and structure in CUDA resampling files

--- resampling/cuda/tet10_resample_field_fun.cu
+++ resampling/cuda/tet10_resample_field_fun.cu
@@ -52,13 +52,99 @@ void free_xyz_tet10_device(xyz_tet10_device* xyz) {
 // end free_xyz_tet10_device
 
 //////////////////////////////////////////////////////////
+// make_elems_tet10_device
+//////////////////////////////////////////////////////////
+elems_tet10_device                                     //
+make_elems_tet10_managed(const ptrdiff_t nelements) {  //
+    //
+    elems_tet10_device elems;
+
+    cudaError_t err0 = cudaMallocManaged(&elems.elems_v0, nelements * sizeof(int));
+    cudaError_t err1 = cudaMallocManaged(&elems.elems_v1, nelements * sizeof(int));
+    cudaError_t err2 = cudaMallocManaged(&elems.elems_v2, nelements * sizeof(int));
+    cudaError_t err3 = cudaMallocManaged(&elems.elems_v3, nelements * sizeof(int));
+    cudaError_t err4 = cudaMallocManaged(&elems.elems_v4, nelements * sizeof(int));
+    cudaError_t err5 = cudaMallocManaged(&elems.elems_v5, nelements * sizeof(int));
+    cudaError_t err6 = cudaMallocManaged(&elems.elems_v6, nelements * sizeof(int));
+    cudaError_t err7 = cudaMallocManaged(&elems.elems_v7, nelements * sizeof(int));
+    cudaError_t err8 = cudaMallocManaged(&elems.elems_v8, nelements * sizeof(int));
+    cudaError_t err9 = cudaMallocManaged(&elems.elems_v9, nelements * sizeof(int));
+
+    if (err0 != cudaSuccess || err1 != cudaSuccess || err2 != cudaSuccess || err3 != cudaSuccess || err4 != cudaSuccess ||
+        err5 != cudaSuccess || err6 != cudaSuccess || err7 != cudaSuccess || err8 != cudaSuccess || err9 != cudaSuccess) {
+        printf("ERROR: allocating memory for elems_tet10_device managed at %s:%d\n", __FILE__, __LINE__);
+        // Handle error
+    }
+
+    return elems;
+}
+
+cudaError_t                                              //
+copy_elems_tet10_managed(const ptrdiff_t     nelements,  //
+                         elems_tet10_device* elems,      //
+                         const idx_t**       elems_host) {     //
+                                                         //
+    cudaError_t err0 = cudaMemcpy(elems->elems_v0, elems_host[0], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err1 = cudaMemcpy(elems->elems_v1, elems_host[1], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err2 = cudaMemcpy(elems->elems_v2, elems_host[2], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err3 = cudaMemcpy(elems->elems_v3, elems_host[3], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err4 = cudaMemcpy(elems->elems_v4, elems_host[4], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err5 = cudaMemcpy(elems->elems_v5, elems_host[5], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err6 = cudaMemcpy(elems->elems_v6, elems_host[6], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err7 = cudaMemcpy(elems->elems_v7, elems_host[7], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err8 = cudaMemcpy(elems->elems_v8, elems_host[8], nelements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaError_t err9 = cudaMemcpy(elems->elems_v9, elems_host[9], nelements * sizeof(int), cudaMemcpyHostToDevice);
+
+    if (err0 != cudaSuccess || err1 != cudaSuccess || err2 != cudaSuccess || err3 != cudaSuccess || err4 != cudaSuccess ||
+        err5 != cudaSuccess || err6 != cudaSuccess || err7 != cudaSuccess || err8 != cudaSuccess || err9 != cudaSuccess) {
+        printf("ERROR: copying elements to device: %s\n", cudaGetErrorString(cudaGetLastError()));
+        return cudaGetLastError();
+    }
+}
+
+void                                                   //
+free_elems_tet10_managed(elems_tet10_device* elems) {  //
+
+    cudaError_t err0 = cudaFree(elems->elems_v0);
+    cudaError_t err1 = cudaFree(elems->elems_v1);
+    cudaError_t err2 = cudaFree(elems->elems_v2);
+    cudaError_t err3 = cudaFree(elems->elems_v3);
+    cudaError_t err4 = cudaFree(elems->elems_v4);
+    cudaError_t err5 = cudaFree(elems->elems_v5);
+    cudaError_t err6 = cudaFree(elems->elems_v6);
+    cudaError_t err7 = cudaFree(elems->elems_v7);
+    cudaError_t err8 = cudaFree(elems->elems_v8);
+    cudaError_t err9 = cudaFree(elems->elems_v9);
+
+    if (err0 != cudaSuccess || err1 != cudaSuccess || err2 != cudaSuccess || err3 != cudaSuccess || err4 != cudaSuccess ||
+        err5 != cudaSuccess || err6 != cudaSuccess || err7 != cudaSuccess || err8 != cudaSuccess || err9 != cudaSuccess) {
+        printf("ERROR: freeing device memory for elems: %s\n", cudaGetErrorString(cudaGetLastError()));
+    }
+
+    elems->elems_v0 = NULL;
+    elems->elems_v1 = NULL;
+    elems->elems_v2 = NULL;
+    elems->elems_v3 = NULL;
+    elems->elems_v4 = NULL;
+    elems->elems_v5 = NULL;
+    elems->elems_v6 = NULL;
+    elems->elems_v7 = NULL;
+    elems->elems_v8 = NULL;
+    elems->elems_v9 = NULL;
+}
+
+//////////////////////////////////////////////////////////
 // make_xyz_tet10_device_unified
 //////////////////////////////////////////////////////////
-xyz_tet10_device make_xyz_tet10_device_unified(const ptrdiff_t nnodes) {
+xyz_tet10_device                                         //
+make_xyz_tet10_device_unified(const ptrdiff_t nnodes) {  //
+                                                         //
     xyz_tet10_device xyz;
+
     xyz.x = NULL;
     xyz.y = NULL;
     xyz.z = NULL;
+
     return xyz;
 }
 // end make_xyz_tet10_device_unified
@@ -66,9 +152,11 @@ xyz_tet10_device make_xyz_tet10_device_unified(const ptrdiff_t nnodes) {
 //////////////////////////////////////////////////////////
 // copy_xyz_tet10_device_unified
 //////////////////////////////////////////////////////////
-void copy_xyz_tet10_device_unified(const ptrdiff_t   nnodes,  //
-                                   xyz_tet10_device* xyz,     //
-                                   const float**     xyz_host) {
+void                                                     //
+copy_xyz_tet10_device_unified(const ptrdiff_t   nnodes,  //
+                              xyz_tet10_device* xyz,     //
+                              const float**     xyz_host) {  //
+                                                         //
     xyz->x = (float*)xyz_host[0];
     xyz->y = (float*)xyz_host[1];
     xyz->z = (float*)xyz_host[2];
@@ -80,7 +168,7 @@ void copy_xyz_tet10_device_unified(const ptrdiff_t   nnodes,  //
 //////////////////////////////////////////////////////////
 void                                                                                   //
 memory_hint_xyz_tet10_device_unified(const ptrdiff_t nnodes, xyz_tet10_device* xyz) {  //
-
+                                                                                       //
     cudaError_t err0 = cudaMemAdvise(xyz->x, nnodes * sizeof(float), cudaMemAdviseSetReadMostly, 0);
     cudaError_t err1 = cudaMemAdvise(xyz->y, nnodes * sizeof(float), cudaMemAdviseSetReadMostly, 0);
     cudaError_t err2 = cudaMemAdvise(xyz->z, nnodes * sizeof(float), cudaMemAdviseSetReadMostly, 0);
@@ -104,7 +192,9 @@ memory_hint_xyz_tet10_device_unified(const ptrdiff_t nnodes, xyz_tet10_device* x
 //////////////////////////////////////////////////////////
 // free_xyz_tet10_device_unified
 //////////////////////////////////////////////////////////
-void free_xyz_tet10_device_unified(xyz_tet10_device* xyz) {
+void                                                    //
+free_xyz_tet10_device_unified(xyz_tet10_device* xyz) {  //
+                                                        //
     xyz->x = NULL;
     xyz->y = NULL;
     xyz->z = NULL;
@@ -112,10 +202,73 @@ void free_xyz_tet10_device_unified(xyz_tet10_device* xyz) {
 // end free_xyz_tet10_device_unified
 
 //////////////////////////////////////////////////////////
+// make_xyz_tet10_managed
+//////////////////////////////////////////////////////////
+xyz_tet10_device make_xyz_tet10_managed(const ptrdiff_t nnodes) {
+    xyz_tet10_device xyz;
+    cudaMallocManaged(&xyz.x, nnodes * sizeof(float));
+    cudaMallocManaged(&xyz.y, nnodes * sizeof(float));
+    cudaMallocManaged(&xyz.z, nnodes * sizeof(float));
+    return xyz;
+}
+// end make_xyz_tet10_managed
+
+//////////////////////////////////////////////////////////
+// copy_xyz_tet10_managed
+//////////////////////////////////////////////////////////
+void                                                     //
+copy_xyz_tet10_managed(const ptrdiff_t   nnodes,  //
+                              xyz_tet10_device* xyz,     //
+                              const float**     xyz_host) {  //
+                                                         //
+    cudaError_t err0 = cudaMemcpy(xyz->x, xyz_host[0], nnodes * sizeof(float), cudaMemcpyHostToDevice);
+    cudaError_t err1 = cudaMemcpy(xyz->y, xyz_host[1], nnodes * sizeof(float), cudaMemcpyHostToDevice);
+    cudaError_t err2 = cudaMemcpy(xyz->z, xyz_host[2], nnodes * sizeof(float), cudaMemcpyHostToDevice);
+
+    if (err0 != cudaSuccess || err1 != cudaSuccess || err2 != cudaSuccess) {
+        printf("Error copying xyz_tet10_device to managed memory: %s\n", cudaGetErrorString(cudaGetLastError()));
+        // Handle the error or exit the program
+    }
+}  // end copy_xyz_tet10_managed
+
+//////////////////////////////////////////////////////////
+// free_xyz_tet10_managed
+//////////////////////////////////////////////////////////
+void                                                    //
+free_xyz_tet10_managed(xyz_tet10_device* xyz) {  //
+                                                        //
+    cudaError_t err0 = cudaFree(xyz->x);
+    cudaError_t err1 = cudaFree(xyz->y);
+    cudaError_t err2 = cudaFree(xyz->z);
+
+    if (err0 != cudaSuccess || err1 != cudaSuccess || err2 != cudaSuccess) {
+        printf("Error freeing managed memory for xyz: %s\n", cudaGetErrorString(cudaGetLastError()));
+    }
+
+    xyz->x = NULL;
+    xyz->y = NULL;
+    xyz->z = NULL;
+}  // end free_xyz_tet10_managed
+
+//////////////////////////////////////////////////////////
+// memory_hint_xyz_tet10_managed
+//////////////////////////////////////////////////////////
+void                                                            //
+memory_hint_xyz_tet10_managed(const ptrdiff_t   nnodes,  //
+                                     xyz_tet10_device* xyz) {   //
+                                                                //
+    cudaMemAdvise(xyz->x, nnodes * sizeof(float), cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId);
+    cudaMemAdvise(xyz->y, nnodes * sizeof(float), cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId);
+    cudaMemAdvise(xyz->z, nnodes * sizeof(float), cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId);
+}  // end memory_hint_xyz_tet10_managed
+
+//////////////////////////////////////////////////////////
 // make_elems_tet10_device
 //////////////////////////////////////////////////////////
-elems_tet10_device make_elems_tet10_device(const ptrdiff_t nelements) {
-    elems_tet10_device elems;
+elems_tet10_device                                    //
+make_elems_tet10_device(const ptrdiff_t nelements) {  //
+                                                      //
+    elems_tet10_device elems;                         //
 
     cudaError_t err0 = cudaMalloc(&elems.elems_v0, nelements * sizeof(int));
     cudaError_t err1 = cudaMalloc(&elems.elems_v1, nelements * sizeof(int));
--- resampling/sfem_resample_field.h
+++ resampling/sfem_resample_field.h
@@ -6,6 +6,7 @@
 
 #include "sfem_base.h"
 #include "sfem_defs.h"
+#include "sfem_mesh.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,66 +26,113 @@ typedef struct {
 
 int resample_field_local(
         // Mesh
-        const enum ElemType element_type, const ptrdiff_t nelements, const ptrdiff_t nnodes,
-        idx_t** const SFEM_RESTRICT elems, geom_t** const SFEM_RESTRICT xyz,
+        const enum ElemType          element_type,  //
+        const ptrdiff_t              nelements,     //
+        const ptrdiff_t              nnodes,        //
+        idx_t** const SFEM_RESTRICT  elems,         //
+        geom_t** const SFEM_RESTRICT xyz,           //
         // SDF
-        const ptrdiff_t* const SFEM_RESTRICT n, const ptrdiff_t* const SFEM_RESTRICT stride,
-        const geom_t* const SFEM_RESTRICT origin, const geom_t* const SFEM_RESTRICT delta,
-        const real_t* const SFEM_RESTRICT data,
+        const ptrdiff_t* const SFEM_RESTRICT n,       //
+        const ptrdiff_t* const SFEM_RESTRICT stride,  //
+        const geom_t* const SFEM_RESTRICT    origin,  //
+        const geom_t* const SFEM_RESTRICT    delta,   //
+        const real_t* const SFEM_RESTRICT    data,    //
         // Output
-        real_t* const SFEM_RESTRICT wg, sfem_resample_field_info* info);
+        real_t* const SFEM_RESTRICT wg,   //
+        sfem_resample_field_info*   info);  //
 
-int resample_field(
-        // Mesh
-        const enum ElemType element_type, const ptrdiff_t nelements, const ptrdiff_t nnodes,
-        idx_t** const SFEM_RESTRICT elems, geom_t** const SFEM_RESTRICT xyz,
-        // SDF
-        const ptrdiff_t* const SFEM_RESTRICT n, const ptrdiff_t* const SFEM_RESTRICT stride,
-        const geom_t* const SFEM_RESTRICT origin, const geom_t* const SFEM_RESTRICT delta,
-        const real_t* const SFEM_RESTRICT data,
-        // Output
-        real_t* const SFEM_RESTRICT field, sfem_resample_field_info* info);
+/**
+ * @brief
+ *
+ * @param element_type
+ * @param nelements
+ * @param nnodes
+ * @param elems
+ * @param xyz
+ * @param n
+ * @param stride
+ * @param origin
+ * @param delta
+ * @param data
+ * @param field
+ * @param info
+ * @return int
+ */
+int                                                                //
+resample_field(const enum ElemType                  element_type,  // // Mesh
+               const ptrdiff_t                      nelements,     //
+               const ptrdiff_t                      nnodes,        //
+               idx_t** const SFEM_RESTRICT          elems,         //
+               geom_t** const SFEM_RESTRICT         xyz,           //
+               const ptrdiff_t* const SFEM_RESTRICT n,             // // SDF
+               const ptrdiff_t* const SFEM_RESTRICT stride,        //
+               const geom_t* const SFEM_RESTRICT    origin,        //
+               const geom_t* const SFEM_RESTRICT    delta,         //
+               const real_t* const SFEM_RESTRICT    data,          //
+               real_t* const SFEM_RESTRICT          field,         // // Output
+               sfem_resample_field_info*            info);                    //
+
+/**
+ * @brief
+ *
+ * @param mpi_size
+ * @param mpi_rank
+ * @param mesh
+ * @param n
+ * @param stride
+ * @param origin
+ * @param delta
+ * @param data
+ * @param field
+ * @param g
+ * @param info
+ * @return int
+ */
+int                                                                 //
+resample_field_mesh(const int                            mpi_size,  // MPI size
+                    const int                            mpi_rank,  // MPI rank
+                    const mesh_t* const SFEM_RESTRICT    mesh,      // Mesh
+                    const ptrdiff_t* const SFEM_RESTRICT n,         // // SDF
+                    const ptrdiff_t* const SFEM_RESTRICT stride,    //
+                    const geom_t* const SFEM_RESTRICT    origin,    //
+                    const geom_t* const SFEM_RESTRICT    delta,     //
+                    const real_t* const SFEM_RESTRICT    data,      //
+                    const real_t* const SFEM_RESTRICT    field,     // // Output
+                    real_t* const SFEM_RESTRICT          g,         //
+                    sfem_resample_field_info*            info);                //
 
 int interpolate_field(const ptrdiff_t nnodes, geom_t** const SFEM_RESTRICT xyz,
                       // SDF
-                      const ptrdiff_t* const SFEM_RESTRICT n,
-                      const ptrdiff_t* const SFEM_RESTRICT stride,
-                      const geom_t* const SFEM_RESTRICT origin,
-                      const geom_t* const SFEM_RESTRICT delta,
+                      const ptrdiff_t* const SFEM_RESTRICT n, const ptrdiff_t* const SFEM_RESTRICT stride,
+                      const geom_t* const SFEM_RESTRICT origin, const geom_t* const SFEM_RESTRICT delta,
                       const real_t* const SFEM_RESTRICT data,
                       // Output
                       real_t* const SFEM_RESTRICT field);
 
-int field_view(MPI_Comm comm, const ptrdiff_t nnodes, const geom_t* SFEM_RESTRICT z_coordinate,
-               const ptrdiff_t* const nlocal, const ptrdiff_t* const SFEM_RESTRICT nglobal,
-               const ptrdiff_t* const SFEM_RESTRICT stride, const geom_t* const origin,
-               const geom_t* const SFEM_RESTRICT delta, const real_t* const field,
-               real_t** field_out, ptrdiff_t* z_nlocal_out,
-               geom_t* const SFEM_RESTRICT z_origin_out);
+int field_view(MPI_Comm comm, const ptrdiff_t nnodes, const geom_t* SFEM_RESTRICT z_coordinate, const ptrdiff_t* const nlocal,
+               const ptrdiff_t* const SFEM_RESTRICT nglobal, const ptrdiff_t* const SFEM_RESTRICT stride,
+               const geom_t* const origin, const geom_t* const SFEM_RESTRICT delta, const real_t* const field, real_t** field_out,
+               ptrdiff_t* z_nlocal_out, geom_t* const SFEM_RESTRICT z_origin_out);
 
-int field_view_ensure_margin(MPI_Comm comm, const ptrdiff_t nnodes,
-                             const geom_t* SFEM_RESTRICT z_coordinate,
-                             const ptrdiff_t* const nlocal,
-                             const ptrdiff_t* const SFEM_RESTRICT nglobal,
-                             const ptrdiff_t* const SFEM_RESTRICT stride,
-                             const geom_t* const origin, const geom_t* const SFEM_RESTRICT delta,
-                             const real_t* const field, const ptrdiff_t z_margin,
-                             real_t** field_out, ptrdiff_t* z_nlocal_out,
-                             geom_t* const SFEM_RESTRICT z_origin_out);
+int field_view_ensure_margin(MPI_Comm comm, const ptrdiff_t nnodes, const geom_t* SFEM_RESTRICT z_coordinate,
+                             const ptrdiff_t* const nlocal, const ptrdiff_t* const SFEM_RESTRICT nglobal,
+                             const ptrdiff_t* const SFEM_RESTRICT stride, const geom_t* const origin,
+                             const geom_t* const SFEM_RESTRICT delta, const real_t* const field, const ptrdiff_t z_margin,
+                             real_t** field_out, ptrdiff_t* z_nlocal_out, geom_t* const SFEM_RESTRICT z_origin_out);
 
 int tet4_resample_field_local_v2(
         // Mesh
-        const ptrdiff_t start_element,      //
-        const ptrdiff_t end_element,        //
-        const ptrdiff_t nnodes,             //
-        idx_t** const SFEM_RESTRICT elems,  //
-        geom_t** const SFEM_RESTRICT xyz,   //
+        const ptrdiff_t              start_element,  //
+        const ptrdiff_t              end_element,    //
+        const ptrdiff_t              nnodes,         //
+        idx_t** const SFEM_RESTRICT  elems,          //
+        geom_t** const SFEM_RESTRICT xyz,            //
         // SDF
         const ptrdiff_t* const SFEM_RESTRICT n,       //
         const ptrdiff_t* const SFEM_RESTRICT stride,  //
-        const geom_t* const SFEM_RESTRICT origin,     //
-        const geom_t* const SFEM_RESTRICT delta,      //
-        const real_t* const SFEM_RESTRICT data,       //
+        const geom_t* const SFEM_RESTRICT    origin,  //
+        const geom_t* const SFEM_RESTRICT    delta,   //
+        const real_t* const SFEM_RESTRICT    data,    //
         // Output
         real_t* const SFEM_RESTRICT weighted_field);  //
 
