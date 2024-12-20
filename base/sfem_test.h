b918d4122614446eba901997fe52f79c5f2375ae
Patrick Zulian <patrick.zulian@gmail.com>
2024-12-20 18:45:46 +0100

    ...

--- base/sfem_test.h
+++ base/sfem_test.h
@@ -8,6 +8,8 @@
 extern "C" {
 #endif
 
+#define SFEM_TEST_SUCCESS 0
+#define SFEM_TEST_FAILURE 1
 #define SFEM_UNIT_TEST_INIT() int err = 0;
 #define SFEM_RUN_TEST(test_)                                                                          \
     do {                                                                                              \
@@ -19,14 +21,14 @@ static inline int sfem_test_assert(const int expr, const char *expr_string, cons
     if (!expr) {
         fprintf(stderr, "\nAssertion failure: %s\nAt %s:%d\n\n", expr_string, file, line);
         assert(0);
-        return 1;
+        return SFEM_TEST_FAILURE;
     }
 
     return 0;
 }
-#define SFEM_TEST_ASSERT(expr)                               \
-    if (sfem_test_assert(expr, #expr, __FILE__, __LINE__)) { \
-        return 1;                                            \
+#define SFEM_TEST_ASSERT(expr)                                                    \
+    if (sfem_test_assert(expr, #expr, __FILE__, __LINE__) == SFEM_TEST_FAILURE) { \
+        return SFEM_TEST_FAILURE;                                                 \
     }
 
 #ifdef __cplusplus
--- operators/ssquad4/tests/ssquad4_interpolate_test.cpp
+++ operators/ssquad4/tests/ssquad4_interpolate_test.cpp
@@ -25,7 +25,7 @@ int test_incidence_count() {
         SFEM_TEST_ASSERT(count->data()[i + 6] == 2);
     }
 
-    return 0;
+    return SFEM_TEST_SUCCESS;
 }
 
 int test_level1_to_level2() {
@@ -36,30 +36,30 @@ int test_level1_to_level2() {
     // the correct acceess is handled through strides. Specific ordering is expected!
     auto elements = sfem::h_buffer<idx_t>(9, 1);
 
-    elements->data()[0][0] = 0;
-    elements->data()[1][0] = 4;
-    elements->data()[2][0] = 1;
+    auto e  = elements->data();
+    e[0][0] = 0;
+    e[1][0] = 4;
+    e[2][0] = 1;
 
-    elements->data()[3][0] = 7;
-    elements->data()[4][0] = 8;
-    elements->data()[5][0] = 5;
+    e[3][0] = 7;
+    e[4][0] = 8;
+    e[5][0] = 5;
 
-    elements->data()[6][0] = 3;
-    elements->data()[7][0] = 6;
-    elements->data()[8][0] = 2;
+    e[6][0] = 3;
+    e[7][0] = 6;
+    e[8][0] = 2;
 
-    SFEM_TEST_ASSERT(ssquad4_prolongate(1,                 // nelements,
-                                        1,                 // rom_level
-                                        2,                 // from_level_stride
-                                        elements->data(),  // from_elements
-                                        2,                 // to_level
-                                        1,                 // to_level_stride
-                                        elements->data(),  // to_elements
-                                        1,                 // vec_size
+    SFEM_TEST_ASSERT(ssquad4_prolongate(1,  // nelements,
+                                        1,  // rom_level
+                                        2,  // from_level_stride
+                                        e,  // from_elements
+                                        2,  // to_level
+                                        1,  // to_level_stride
+                                        e,  // to_elements
+                                        1,  // vec_size
                                         from,
                                         to) == SFEM_SUCCESS);
 
-    auto e = elements->data();
     for (int xi = 0; xi < 3; xi++) {
         for (int yi = 0; yi < 3; yi++) {
             const idx_t idx = e[yi * 3 + xi][0];
@@ -81,7 +81,7 @@ int test_level1_to_level2() {
         SFEM_TEST_ASSERT(count_from->data()[i] == 1);
     }
 
-    return 0;
+    return SFEM_TEST_SUCCESS;
 }
 
 int test_level2_to_level4() {
@@ -136,13 +136,100 @@ int test_level2_to_level4() {
         }
     }
 
-    return 0;
+    return SFEM_TEST_SUCCESS;
+}
+
+int test_level1_to_level4() {
+    // 3 	14 	6 	13 	2
+    // 15 	23	18	22	12
+    // 7  	24	8   21	5
+    // 16	19	17	20	11
+    // 0 	9 	4 	10 	1
+
+    real_t from[4] = {0, 4, 4, 0};
+    auto   to      = sfem::h_buffer<real_t>(25);
+
+    // In this example we use the same element indices array for both discretization
+    // the correct acceess is handled through strides. Specific ordering is expected!
+    auto elements = sfem::h_buffer<idx_t>(25, 1);
+    auto e        = elements->data();
+
+    e[0][0] = 0;
+    e[1][0] = 9;
+    e[2][0] = 4;
+    e[3][0] = 10;
+    e[4][0] = 1;
+
+    e[5][0] = 16;
+    e[6][0] = 19;
+    e[7][0] = 17;
+    e[8][0] = 20;
+    e[9][0] = 11;
+
+    e[10][0] = 7;
+    e[11][0] = 24;
+    e[12][0] = 8;
+    e[13][0] = 21;
+    e[14][0] = 5;
+
+    e[15][0] = 15;
+    e[16][0] = 23;
+    e[17][0] = 18;
+    e[18][0] = 22;
+    e[19][0] = 12;
+
+    e[20][0] = 3;
+    e[21][0] = 14;
+    e[22][0] = 6;
+    e[23][0] = 13;
+    e[24][0] = 2;
+
+    SFEM_TEST_ASSERT(ssquad4_prolongate(1,  // nelements
+                                        1,  // from_level
+                                        4,  // from_level_stride
+                                        e,  // from_elements
+                                        4,  // to_level
+                                        1,  // to_level_stride
+                                        e,  // to_elements
+                                        1,  // vec_size
+                                        from,
+                                        to->data()) == SFEM_SUCCESS);
+
+    for (int yi = 0; yi < 5; yi++) {
+        for (int xi = 0; xi < 5; xi++) {
+            const idx_t idx = e[yi * 5 + xi][0];
+            SFEM_TEST_ASSERT(fabs(to->data()[idx] - xi) < 1e-12);
+        }
+    }
+
+    // Strided access different discretizations same supporting data-structure
+    SFEM_TEST_ASSERT(ssquad4_prolongate(1,  // nelements
+                                        1,  // from_level
+                                        4,  // from_level_stride
+                                        e,  // from_elements
+                                        2,  // to_level
+                                        2,  // to_level_stride
+                                        e,  // to_elements
+                                        1,  // vec_size
+                                        from,
+                                        to->data()) == SFEM_SUCCESS);
+
+    for (int yi = 0; yi < 3; yi++) {
+        for (int xi = 0; xi < 3; xi++) {
+            const idx_t idx = e[yi * 2 * 5 + xi * 2][0];
+            SFEM_TEST_ASSERT(fabs(to->data()[idx] - xi * 2) < 1e-12);
+        }
+    }
+
+    return SFEM_TEST_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
     SFEM_UNIT_TEST_INIT();
-    SFEM_RUN_TEST(test_incidence_count);
+    SFEM_RUN_TEST(test_level1_to_level4);
     SFEM_RUN_TEST(test_level2_to_level4);
     SFEM_RUN_TEST(test_level1_to_level2);
+    SFEM_RUN_TEST(test_incidence_count);
+
     return SFEM_UNIT_TEST_FINALIZE();
 }
