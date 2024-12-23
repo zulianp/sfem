ad95ac15d0660f26d72d895c4d4089f92c1404df
Patrick Zulian <patrick.zulian@gmail.com>
2024-12-23 12:32:20 +0100

    Wrote failing test for skeleton of sshex8_prolongate

--- operators/proteus_hex8/tests/sshex8_interpolate_test.cpp
+++ operators/proteus_hex8/tests/sshex8_interpolate_test.cpp
@@ -6,7 +6,6 @@
 
 #include "sfem_test.h"
 
-
 static int test_incidence_count() {
     auto elements = sfem::h_buffer<idx_t>(27, 2);
 
@@ -27,7 +26,107 @@ static int test_incidence_count() {
         SFEM_TEST_ASSERT(count->data()[i + 18] == 2);
     }
 
-    count->print(std::cout);
+    return SFEM_TEST_SUCCESS;
+}
+
+static int test_level1_to_level2() {
+    auto from = sfem::h_buffer<real_t>(8);
+    auto to   = sfem::h_buffer<real_t>(27);
+
+    // In this example we use the same element indices array for both discretization
+    // the correct acceess is handled through strides. Specific ordering is expected!
+    auto elements = sfem::h_buffer<idx_t>(27, 1);
+
+    auto e = elements->data();
+
+    // (0,0,0)-(1,0,0)
+    e[0][0] = 0;
+    e[1][0] = 8;
+    e[2][0] = 1;
+
+    // (0,0.5,0)-(1,0.5,0)
+    e[3][0] = 19;
+    e[4][0] = 10;
+    e[5][0] = 11;
+
+    // (0,1,0)-(1,1,0)
+    e[6][0] = 2;
+    e[7][0] = 12;
+    e[8][0] = 3;
+
+    // (0,0,0.5)-(1,0,0.5)
+    e[9][0]  = 13;
+    e[10][0] = 14;
+    e[11][0] = 15;
+
+    // (0,0.5,0.5)-(1,0.5,0.5)
+    e[12][0] = 16;
+    e[13][0] = 17;
+    e[14][0] = 18;
+
+    // (0,1,0.5)-(1,1,0.5)
+    e[15][0] = 19;
+    e[16][0] = 20;
+    e[17][0] = 21;
+
+    // (0,1,1)-(1,1,1)
+    e[18][0] = 4;
+    e[19][0] = 22;
+    e[20][0] = 5;
+
+    // (0,1,1)-(1,1,1)
+    e[21][0] = 23;
+    e[22][0] = 24;
+    e[23][0] = 25;
+
+    // (0,1,1)-(1,1,1)
+    e[24][0] = 6;
+    e[25][0] = 26;
+    e[26][0] = 7;
+
+    for (int zi = 0; zi < 2; zi++) {
+        for (int yi = 0; yi < 2; yi++) {
+            for (int xi = 0; xi < 2; xi++) {
+                const idx_t idx = e[2 * zi * 9 + 2 * yi * 3 + 2 * xi][0];
+                SFEM_TEST_ASSERT(idx < 8);
+                from->data()[idx] = xi * 2;
+            }
+        }
+    }
+    
+    SFEM_TEST_ASSERT(sshex8_prolongate(1,  // nelements,
+                                       1,  // rom_level
+                                       2,  // from_level_stride
+                                       e,  // from_elements
+                                       2,  // to_level
+                                       1,  // to_level_stride
+                                       e,  // to_elements
+                                       1,  // vec_size
+                                       from->data(),
+                                       to->data()) == SFEM_SUCCESS);
+
+    for (int zi = 0; zi < 3; zi++) {
+        for (int yi = 0; yi < 3; yi++) {
+            for (int xi = 0; xi < 3; xi++) {
+                const idx_t idx = e[zi * 9 + yi * 3 + xi][0];
+                SFEM_TEST_ASSERT(fabs(to->data()[idx] - xi) < 1e-12);
+            }
+        }
+    }
+
+    auto count_to = sfem::h_buffer<uint16_t>(9);
+    sshex8_element_node_incidence_count(2, 1, 1, elements->data(), count_to->data());
+
+    for (ptrdiff_t i = 0; i < 9; i++) {
+        SFEM_TEST_ASSERT(count_to->data()[i] == 1);
+    }
+
+    auto count_from = sfem::h_buffer<uint16_t>(4);
+    sshex8_element_node_incidence_count(1, 2, 1, elements->data(), count_from->data());
+
+    for (ptrdiff_t i = 0; i < 4; i++) {
+        SFEM_TEST_ASSERT(count_from->data()[i] == 1);
+    }
 
     return SFEM_TEST_SUCCESS;
 }
@@ -35,5 +134,6 @@ static int test_incidence_count() {
 int main(int argc, char *argv[]) {
     SFEM_UNIT_TEST_INIT();
     SFEM_RUN_TEST(test_incidence_count);
+    SFEM_RUN_TEST(test_level1_to_level2);
     return SFEM_UNIT_TEST_FINALIZE();
 }
