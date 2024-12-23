0d32ff0f0a11250ea27ff3862305ca0cbddf08e7
Patrick Zulian <patrick.zulian@gmail.com>
2024-12-23 11:51:32 +0100

    SSQUAD4 Restriction

--- operators/ssquad4/ssquad4_interpolate.c
+++ operators/ssquad4/ssquad4_interpolate.c
@@ -64,9 +64,9 @@ int ssquad4_restrict(const ptrdiff_t                     nelements,
 
 #pragma omp parallel
     {
-        const int from_nxe     = ssquad4_nxe(from_level);
-        const int to_nxe       = ssquad4_nxe(to_level);
-        const int to_from_step = from_level / to_level;
+        const int from_nxe    = ssquad4_nxe(from_level);
+        const int to_nxe      = ssquad4_nxe(to_level);
+        const int step_factor = from_level / to_level;
 
         scalar_t **from_coeffs = malloc(vec_size * sizeof(scalar_t *));
         for (int d = 0; d < vec_size; d++) {
@@ -75,7 +75,91 @@ int ssquad4_restrict(const ptrdiff_t                     nelements,
 
 #pragma omp for
         for (ptrdiff_t e = 0; e < nelements; e++) {
-            //
+            {  // Gather elemental coefficients
+                for (int yi = 0; yi <= from_level; yi++) {
+                    for (int xi = 0; xi <= from_level; xi++) {
+                        const int v = ssquad4_lidx(from_level, xi, yi);
+                        const int strided_v =
+                                ssquad4_lidx(from_level * from_level_stride, xi * from_level_stride, yi * from_level_stride);
+                        const ptrdiff_t gid = from_elements[strided_v][e];
+
+                        for (int d = 0; d < vec_size; d++) {
+                            from_coeffs[d][v] = from[gid * vec_size + d] / from_element_to_node_incidence_count[gid];
+                        }
+                    }
+                }
+            }
+
+            const scalar_t h = to_level * 1. / from_level;
+            for (int d = 0; d < vec_size; d++) {
+                scalar_t *c = from_coeffs[d];
+
+                // Restict the coefficients along the x-axis (edges)
+                for (int yi = 0; yi <= to_level; yi++) {
+                    for (int xi = 0; xi < to_level; xi++) {
+                        for (int between_xi = 1; between_xi < step_factor; between_xi++) {
+                            const scalar_t fl      = (1 - between_xi * h);
+                            const scalar_t fr      = (between_xi * h);
+                            const int      to_lidx = ssquad4_lidx(from_level, xi * step_factor + between_xi, yi * step_factor);
+                            c[ssquad4_lidx(from_level, xi * step_factor, yi * step_factor)] += fl * c[to_lidx];
+                            c[ssquad4_lidx(from_level, (xi + 1) * step_factor, yi * step_factor)] += fr * c[to_lidx];
+                        }
+                    }
+                }
+
+                // Restrict the coefficients along the x-axis (center)
+                for (int yi = 0; yi < to_level; yi++) {
+                    for (int xi = 0; xi < to_level; xi++) {
+                        for (int between_yi = 1; between_yi < step_factor; between_yi++) {
+                            const int yy    = yi * step_factor + between_yi;
+                            const int left  = ssquad4_lidx(from_level, xi * step_factor, yy);
+                            const int right = ssquad4_lidx(from_level, (xi + 1) * step_factor, yy);
+
+                            for (int between_xi = 1; between_xi < step_factor; between_xi++) {
+                                const scalar_t fl = (1 - between_xi * h);
+                                const scalar_t fr = (between_xi * h);
+
+                                const int xx     = xi * step_factor + between_xi;
+                                const int center = ssquad4_lidx(from_level, xx, yy);
+
+                                c[left] += fl * c[center];
+                                c[right] += fr * c[center];
+                            }
+                        }
+                    }
+                }
+
+                // Restrict the coefficients along the y-axis (edges)
+                for (int yi = 0; yi < to_level; yi++) {
+                    for (int xi = 0; xi <= to_level; xi++) {
+                        for (int between_yi = 1; between_yi < step_factor; between_yi++) {
+                            const scalar_t fb      = (1 - between_yi * h);
+                            const scalar_t ft      = (between_yi * h);
+                            const int      to_lidx = ssquad4_lidx(from_level, xi * step_factor, yi * step_factor + between_yi);
+                            c[ssquad4_lidx(from_level, xi * step_factor, yi * step_factor)] += fb * c[to_lidx];
+                            c[ssquad4_lidx(from_level, xi * step_factor, (yi + 1) * step_factor)] += ft * c[to_lidx];
+                        }
+                    }
+                }
+            }
+
+            // Scatter elemental data
+            // Fill matching nodes with from data while gathering
+            for (int d = 0; d < vec_size; d++) {
+                for (int yi = 0; yi <= to_level; yi++) {
+                    for (int xi = 0; xi <= to_level; xi++) {
+                        // Use top level stride
+                        const int to_lidx = ssquad4_lidx(to_level * to_level_stride, xi * to_level_stride, yi * to_level_stride);
+
+                        // Use stride to convert from "from" to "to" local indexing
+                        const int from_lidx = ssquad4_lidx(from_level, xi * step_factor, yi * step_factor);
+
+                        const idx_t idx = to_elements[to_lidx][e];
+#pragma omp atomic update
+                        to[idx * vec_size + d] += from_coeffs[d][from_lidx];
+                    }
+                }
+            }
         }
 
         for (int d = 0; d < vec_size; d++) {
@@ -200,7 +284,7 @@ int ssquad4_prolongate(const ptrdiff_t                   nelements,
                 }
 #endif
 
-                // Interpolate the coefficients along the y-axis (edges)
+                // Interpolate the coefficients along the x-axis (center)
                 for (int yi = 0; yi < from_level; yi++) {
                     for (int xi = 0; xi < from_level; xi++) {
                         for (int between_yi = 1; between_yi < from_to_step; between_yi++) {
@@ -232,6 +316,7 @@ int ssquad4_prolongate(const ptrdiff_t                   nelements,
 #endif
             }
 
+            // Scatter elemental data
             for (int yi = 0; yi <= to_level; yi++) {
                 for (int xi = 0; xi <= to_level; xi++) {
                     const int v         = ssquad4_lidx(to_level, xi, yi);
