b153a387e14d8215c0a40dddb0becc9b44b46008
Patrick Zulian <patrick.zulian@gmail.com>
2025-01-14 15:35:25 +0100

    started test for prolong

--- frontend/tests/sfem_SemiStructuredMeshTest.cpp
+++ frontend/tests/sfem_SemiStructuredMeshTest.cpp
@@ -48,10 +48,32 @@ int test_derefine() {
     return SFEM_TEST_SUCCESS;
 }
 
+int test_prolong_restrict()
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    
+    int L = 8;
+    int SFEM_BASE_RESOLUTION = 1;
+    SFEM_READ_ENV(SFEM_BASE_RESOLUTION, atoi);
+
+    auto m = sfem::Mesh::create_hex8_cube(
+            comm, SFEM_BASE_RESOLUTION * 2, SFEM_BASE_RESOLUTION * 1, SFEM_BASE_RESOLUTION * 1, 0, 0, 0, 2, 1, 1);
+
+    auto fs = sfem::FunctionSpace::create(m, 1);
+    fs->promote_to_semi_structured(L);
+
+    // Do this before any other operation
+    fs->semi_structured_mesh().apply_hierarchical_renumbering();
+
+    auto coarse_mesh = fs->semi_structured_mesh().derefine(levels[2]);
+
+}
+
 
 int main(int argc, char *argv[]) {
     SFEM_UNIT_TEST_INIT(argc, argv);
     SFEM_RUN_TEST(test_derefine);
+    SFEM_RUN_TEST(test_prolong_restrict);
     SFEM_UNIT_TEST_FINALIZE();
     return SFEM_UNIT_TEST_ERR();
 }
