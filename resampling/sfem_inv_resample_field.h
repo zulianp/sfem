37a2e68ba3e5eb9946c9c3c991346be8e72a928b
Simone <sriva@susePC>
2025-01-16 16:50:50 +0100

    mesh to grid build test

--- resampling/sfem_inv_resample_field.h
+++ resampling/sfem_inv_resample_field.h
@@ -55,6 +55,46 @@ tet4_inv_resample_field_local(const ptrdiff_t                      start_element
                               const geom_t* const SFEM_RESTRICT    delta,           // SDF
                               real_t* const SFEM_RESTRICT          data);                    // SDF: Output
 
+/**
+ * @brief Check if a point is inside a tetrahedron
+ *
+ * This function determines whether a point (px, py, pz) is inside a tetrahedron
+ * defined by its four vertices (x0, y0, z0), (x1, y1, z1), (x2, y2, z2), and (x3, y3, z3).
+ *
+ * @param px x coordinate of the point
+ * @param py y coordinate of the point
+ * @param pz z coordinate of the point
+ * @param x0 x coordinate of the 1st vertex
+ * @param y0 y coordinate of the 1st vertex
+ * @param z0 z coordinate of the 1st vertex
+ * @param x1 x coordinate of the 2nd vertex
+ * @param y1 y coordinate of the 2nd vertex
+ * @param z1 z coordinate of the 2nd vertex
+ * @param x2 x coordinate of the 3rd vertex
+ * @param y2 y coordinate of the 3rd vertex
+ * @param z2 z coordinate of the 3rd vertex
+ * @param x3 x coordinate of the 4th vertex
+ * @param y3 y coordinate of the 4th vertex
+ * @param z3 z coordinate of the 4th vertex
+ * @return int Returns 1 if the point is inside the tetrahedron, 0 otherwise.
+ */
+int                                           //
+check_p_inside_testreadnum(const real_t px,   //
+                           const real_t py,   //
+                           const real_t pz,   //
+                           const real_t x0,   //
+                           const real_t y0,   //
+                           const real_t z0,   //
+                           const real_t x1,   //
+                           const real_t y1,   //
+                           const real_t z1,   //
+                           const real_t x2,   //
+                           const real_t y2,   //
+                           const real_t z2,   //
+                           const real_t x3,   //
+                           const real_t y3,   //
+                           const real_t z3);  //
+
 #ifdef __cplusplus
 }  // end extern "C"
 #endif
