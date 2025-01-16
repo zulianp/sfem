62f1723cd7cce40205b74676981ce8c6112b27c1
Patrick Zulian <patrick.zulian@gmail.com>
2025-01-16 14:46:36 +0100

    GMG test passing for poisson problem, Removed bad parallel for

--- ssmg/sfem_ssgmg.cpp
+++ ssmg/sfem_ssgmg.cpp
@@ -9,6 +9,8 @@ namespace sfem {
     std::shared_ptr<Multigrid<real_t>> create_ssgmg(const std::shared_ptr<Function> &f,
                                                     const enum ExecutionSpace        es,
                                                     const std::shared_ptr<Input>    &in) {
+        SFEM_TRACE_SCOPE("create_ssgmg");
+
         if (!f->space()->has_semi_structured_mesh()) {
             SFEM_ERROR("[Error] create_ssgmg cannot build MG without a semistructured mesh");
             return nullptr;
@@ -18,17 +20,20 @@ namespace sfem {
         auto     &ssmesh = f->space()->semi_structured_mesh();
         const int L      = ssmesh.level();
 
-        std::vector<int> levels =  ssmesh.derefinement_levels();
+        std::vector<int> levels = ssmesh.derefinement_levels();
+
+        // Order from finest to coarsest!
+        std::reverse(levels.begin(), levels.end());
         const int nlevels = levels.size();
-        
+
         std::vector<std::shared_ptr<Function>> functions;
         functions.push_back(f);
 
-        for (int l = L; l >= 1; l--) {
-            printf("Derefine %d -> %d\n", levels[l], levels[l - 1]);
+        for (int l = 1; l < nlevels; l++) {
+            printf("Derefine %d -> %d\n", levels[l - 1], levels[l]);
             auto f_prev  = functions.back();
             auto fs_prev = f_prev->space();
-            auto fs_next = fs_prev->derefine(levels[l - 1]);
+            auto fs_next = fs_prev->derefine(levels[l]);
             functions.push_back(f_prev->derefine(fs_next, true));
         }
 
@@ -52,15 +57,22 @@ namespace sfem {
             smoothers_or_solver.push_back(smoother);
         }
 
+        // Coarse level
+        auto fi        = functions.back();
+        auto linear_op = sfem::create_linear_operator("MF", fi, nullptr, es);
+        operators.push_back(linear_op);
+
         // Coarse-grid solver
-        auto coarse_solver = sfem::create_cg<real_t>(operators[nlevels - 1], es);
+        auto coarse_solver = sfem::create_cg<real_t>(operators.back(), es);
+        coarse_solver->set_max_it(10000);
+        coarse_solver->verbose = false;
 
         // Fine level
         bool enable_coarse_space_preconditioner = false;
-        in->get("enable_coarse_space_preconditioner", enable_coarse_space_preconditioner);
+        if (in) in->get("enable_coarse_space_preconditioner", enable_coarse_space_preconditioner);
 
         if (enable_coarse_space_preconditioner) {
-            auto f_coarse = functions[nlevels - 1];
+            auto f_coarse = functions.back();
             auto diag     = sfem::create_buffer<real_t>(f_coarse->space()->n_dofs(), es);
             f_coarse->hessian_diag(nullptr, diag->data());
             auto sj_coarse = sfem::create_shiftable_jacobi(diag, es);
@@ -69,24 +81,35 @@ namespace sfem {
 
         smoothers_or_solver.push_back(coarse_solver);
 
+        for (int i = 0; i < nlevels; i++) {
+            auto s = functions[i]->space();
+            printf("%d) \tL=%d\n", i, s->has_semi_structured_mesh() ? s->semi_structured_mesh().level() : 1);
+
+            functions[i]->describe(std::cout);
+        }
+
         auto mg = std::make_shared<Multigrid<real_t>>();
-        {  // Construct actual multigrid
+
+
+        {  
+            SFEM_TRACE_SCOPE("create_ssgmg::construct");
+        // Construct actual multigrid
             mg->add_level(operators[0],
                           smoothers_or_solver[0],
                           nullptr,
-                          sfem::create_hierarchical_restriction(functions[1]->space(), functions[0]->space(), es));
+                          sfem::create_hierarchical_restriction(functions[0]->space(), functions[1]->space(), es));
 
             // Intermediate levels
             for (int i = 1; i < nlevels - 1; i++) {
                 auto restriction = sfem::create_hierarchical_restriction(functions[i]->space(), functions[i + 1]->space(), es);
-                
+
                 auto prolong_unconstr =
                         sfem::create_hierarchical_prolongation(functions[i]->space(), functions[i - 1]->space(), es);
-                
+
                 auto prolongation = sfem::make_op<real_t>(
                         prolong_unconstr->rows(),
                         prolong_unconstr->cols(),
-                        [=](const real_t *const from, real_t *const to) {
+                        [prolong_unconstr, f = functions[i - 1]](const real_t *const from, real_t *const to) {
                             prolong_unconstr->apply(from, to);
                             f->apply_zero_constraints(to);
                         },
@@ -96,11 +119,10 @@ namespace sfem {
             }
 
             // Coarse level
-            mg->add_level(
-                    operators[nlevels - 1],
-                    smoothers_or_solver[nlevels - 1],
-                    sfem::create_hierarchical_prolongation(functions[nlevels - 1]->space(), functions[nlevels - 2]->space(), es),
-                    nullptr);
+            mg->add_level(operators.back(),
+                          smoothers_or_solver.back(),
+                          sfem::create_hierarchical_prolongation(functions.back()->space(), functions[nlevels - 2]->space(), es),
+                          nullptr);
         }
 
 #ifdef SFEM_ENABLE_CUDA
@@ -114,6 +136,7 @@ namespace sfem {
             mg->default_init();
         }
 
+        mg->verbose = true;
         return mg;
     }
 
