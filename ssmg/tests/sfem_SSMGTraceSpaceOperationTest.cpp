53254b805f4eb235bc6de7e6ad7c178bd79ca23f
Patrick Zulian <patrick.zulian@gmail.com>
2025-01-17 15:34:45 +0100

    ...

--- ssmg/tests/sfem_SSMGTraceSpaceOperationTest.cpp
+++ ssmg/tests/sfem_SSMGTraceSpaceOperationTest.cpp
@@ -40,19 +40,32 @@ int test_trace_space_operations(const std::shared_ptr<sfem::FunctionSpace> &coar
     auto &&fine_ssmesh   = fine_fs->semi_structured_mesh();
     auto &&coarse_ssmesh = coarse_fs->semi_structured_mesh();
 
+    ptrdiff_t n_nodes{0};
+    idx_t * nodes{nullptr};
+    SFEM_TEST_ASSERT(sshex8_extract_nodeset_from_sideset(coarse_ssmesh.level(),
+                                                         coarse_ssmesh.element_data(),
+                                                         sideset->parent()->size(),
+                                                         sideset->parent()->data(),
+                                                         sideset->lfi()->data(),
+                                                         &n_nodes,
+                                                         &nodes) == SFEM_SUCCESS);
+
+    auto coarse_nodeset = sfem::manage_host_buffer(n_nodes, nodes);
+
     {
         auto            points     = coarse_ssmesh.points()->data();
-        const ptrdiff_t n          = coarse_fs->n_dofs();
+        const ptrdiff_t n          = coarse_nodeset->size();
         const int       block_size = coarse_fs->block_size();
 
+        auto idx  = coarse_nodeset->data();
         auto data = coarse_x->data();
         for (ptrdiff_t i = 0; i < n; i++) {
-            data[i * block_size] = (points[0][i] + 1) * (points[1][i] + 1) * (points[2][i] + 1);
+            data[idx[i] * block_size] = 1;
         }
     }
 
 #ifdef SFEM_ENABLE_CUDA
-    coarse_x = sfem::to_device(coarse_x);
+    if (es == sfem::EXECUTION_SPACE_DEVICE) coarse_x = sfem::to_device(coarse_x);
 #endif
 
     const int nexs       = (fine_ssmesh.level() + 1) * (fine_ssmesh.level() + 1);
@@ -65,8 +78,6 @@ int test_trace_space_operations(const std::shared_ptr<sfem::FunctionSpace> &coar
                                                          sideset->lfi()->data(),
                                                          fine_sides->data()) == SFEM_SUCCESS);
 
-    // std::cout << "fine_sides: " << fine_sides->extent(0) << " x " << fine_sides->extent(1) << "\n";
-
     SFEM_TEST_ASSERT(ssquad4_prolongate(fine_sides->extent(1),                        // nelements,
                                         coarse_ssmesh.level(),                        // rom_level
                                         fine_ssmesh.level() / coarse_ssmesh.level(),  // from_level_stride
@@ -78,23 +89,52 @@ int test_trace_space_operations(const std::shared_ptr<sfem::FunctionSpace> &coar
                                         coarse_x->data(),
                                         fine_x->data()) == SFEM_SUCCESS);
 
-#if 0
-    sfem::create_directory(name.c_str());
-    sfem::create_directory((name + "/fields").c_str());
+    auto restricted_x = sfem::create_buffer<real_t>(coarse_fs->n_dofs(), es);
+    auto count = sfem::create_host_buffer<uint16_t>(fine_ssmesh.n_nodes());
 
-    SFEM_TEST_ASSERT(fine_fs->semi_structured_mesh().export_as_standard((name + "/mesh").c_str()) == SFEM_SUCCESS);
-
-    sfem::Output out(fine_fs);
-    out.enable_AoS_to_SoA(true);
-
-    out.set_output_dir((name + "/fields").c_str());
-    SFEM_TEST_ASSERT(out.write("u", fine_x->data()) == SFEM_SUCCESS);
+#ifdef SFEM_ENABLE_CUDA
+    if (es == sfem::EXECUTION_SPACE_DEVICE) count = sfem::to_device(count);
 #endif
+
+    SFEM_TEST_ASSERT(ssquad4_element_node_incidence_count(
+                             fine_ssmesh.level(), 1, fine_sides->extent(1), fine_sides->data(), count->data()) == SFEM_SUCCESS);
+
+    SFEM_TEST_ASSERT(ssquad4_restrict(fine_sides->extent(1),
+                                      fine_ssmesh.level(),
+                                      1,
+                                      fine_sides->data(),
+                                      count->data(),
+                                      coarse_ssmesh.level(),
+                                      fine_ssmesh.level() / coarse_ssmesh.level(),
+                                      fine_sides->data(),
+                                      fine_fs->block_size(),
+                                      fine_x->data(),
+                                      restricted_x->data()) == SFEM_SUCCESS);
+
+#ifdef SFEM_ENABLE_CUDA
+    if (es == sfem::EXECUTION_SPACE_DEVICE) {
+        restricted_x = sfem::to_host(restricted_x);
+    }
+#endif
+
+    {
+        auto            points     = coarse_ssmesh.points()->data();
+        const ptrdiff_t n          = coarse_nodeset->size();
+        const int       block_size = coarse_fs->block_size();
+
+        auto rx = restricted_x->data();
+        auto cx = coarse_x->data();
+
+
+        for (ptrdiff_t i = 0; i < n; i++) {
+            SFEM_TEST_ASSERT( (cx[i] != 0) == (rx[i] != 0));
+        }
+    }
 
     return SFEM_TEST_SUCCESS;
 }
 
-int test_trace_prolongation() {
+int test_trace_space_prolongation_restriction() {
     MPI_Comm comm = MPI_COMM_WORLD;
     auto     es   = sfem::EXECUTION_SPACE_HOST;
 
@@ -106,9 +146,9 @@ int test_trace_prolongation() {
     }
 
     int SFEM_ELEMENT_REFINE_LEVEL = 4;
-    // SFEM_READ_ENV(SFEM_ELEMENT_REFINE_LEVEL, atoi);
+    SFEM_READ_ENV(SFEM_ELEMENT_REFINE_LEVEL, atoi);
 
-    int SFEM_BASE_RESOLUTION = 1;
+    int SFEM_BASE_RESOLUTION = 8;
     SFEM_READ_ENV(SFEM_BASE_RESOLUTION, atoi);
 
     geom_t Lx = 1;
@@ -123,7 +163,7 @@ int test_trace_prolongation() {
     auto sideset = sfem::Sideset::create_from_selector(
             m, [=](const geom_t /*x*/, const geom_t y, const geom_t z) -> bool { return y > -1e-5 && y < 1e-5; });
 
-    return test_trace_space_operations(fs->derefine(2), fs, sideset, "test_trace_prolongation", es);
+    return test_trace_space_operations(fs->derefine(2), fs, sideset, "test_trace_space_prolongation_restriction", es);
 }
 
 int main(int argc, char *argv[]) {
@@ -133,7 +173,7 @@ int main(int argc, char *argv[]) {
     sfem::register_device_ops();
 #endif
 
-    SFEM_RUN_TEST(test_trace_prolongation);
+    SFEM_RUN_TEST(test_trace_space_prolongation_restriction);
 
     SFEM_UNIT_TEST_FINALIZE();
     return SFEM_UNIT_TEST_ERR();
