2b47414912299f2e0252daac6ca4a62b0a44cd7c
Simone Riva <sriva@daint-ln002.cscs.ch>
2025-01-29 11:13:55 +0100

    update

--- workflows/resample/example.sh
+++ workflows/resample/example.sh
@@ -69,7 +69,7 @@ echo $sizes
 echo $origins
 echo $scaling
 
-n_procs=1
+n_procs=16
 # n_procs=2
 # n_procs=8
 
@@ -78,7 +78,7 @@ Nsight_OUTPUT="/capstor/scratch/cscs/sriva/app/NVidia_prof_out/ncu_grid_to_mesh"
 
 LAUNCH="mpiexec -np $n_procs "
 # LAUNCH="srun -p debug -n $n_procs  ${Nsight_PATH}/ncu  --set roofline --print-details body  -f --section ComputeWorkloadAnalysis -o ${Nsight_OUTPUT} "
-# LAUNCH="srun -p debug -n $n_procs -N 1 ./mps-wrapper.sh "
+LAUNCH="srun -p normal -n $n_procs --gpus-per-task=1  ./mps-wrapper.sh "
 # LAUNCH=""
 
 GRID_TO_MESH="grid_to_mesh"
