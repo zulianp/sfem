4f78a8d814b1e192743169af5508535a5e4b45d4
Patrick Zulian <patrick.zulian@gmail.com>
2025-02-27 17:34:16 +0100

    2D SDF approx looks ok, but unsatisfactory

--- python/sfem/mesh/star_mesh.py
+++ python/sfem/mesh/star_mesh.py
@@ -29,13 +29,14 @@ for i in range(num_points*2):
     x_coords[i] = center[0] + current_radius * np.cos(angles[i])
     y_coords[i] = center[1] + current_radius * np.sin(angles[i])
 
-# Add the first point at the end to close the shape
-x_coords = np.append(x_coords, x_coords[0])
-y_coords = np.append(y_coords, y_coords[0])
-
-n = len(x_coords) - 1  # Subtract 1 since last point equals first
+n = len(x_coords)  # Subtract 1 since last point equals first
 i0 = np.arange(n, dtype=np.int32)
-i1 = np.arange(1, n+1, dtype=np.int32)
+i1 = np.arange(1, n, dtype=np.int32)
+i1 = np.append(i1, 0)
+
+# print(i0)
+# print(i1)
+# print(x_coords)
 
 os.path.exists('star') or os.makedirs('star')
 
