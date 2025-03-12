6ced22f05349690ce76957ab241f2445f0360f04
Patrick Zulian <patrick.zulian@gmail.com>
2025-03-12 18:59:35 +0100

    ...

--- operators/spectral_hex/spectral_hex_laplacian.cpp
+++ operators/spectral_hex/spectral_hex_laplacian.cpp
@@ -177,74 +177,60 @@ void spectral_hex_laplacian_apply(  // Shape functions per quad point Q x S
         gz[q]       = gzq;
     }
 
-#ifndef NDEBUG
-    printf("gx) %g %g %g %g, %g %g %g %g\n", gx[0], gx[1], gx[2], gx[3], gx[4], gx[5], gx[6], gx[7]);
-    printf("gy) %g %g %g %g, %g %g %g %g\n", gy[0], gy[1], gy[2], gy[3], gy[4], gy[5], gy[6], gy[7]);
-    printf("gz) %g %g %g %g, %g %g %g %g\n", gz[0], gz[1], gz[2], gz[3], gz[4], gz[5], gz[6], gz[7]);
+// #ifndef NDEBUG
+//     printf("gx) %g %g %g %g, %g %g %g %g\n", gx[0], gx[1], gx[2], gx[3], gx[4], gx[5], gx[6], gx[7]);
+//     printf("gy) %g %g %g %g, %g %g %g %g\n", gy[0], gy[1], gy[2], gy[3], gy[4], gy[5], gy[6], gy[7]);
+//     printf("gz) %g %g %g %g, %g %g %g %g\n", gz[0], gz[1], gz[2], gz[3], gz[4], gz[5], gz[6], gz[7]);
 
-    T expected[8] = {0};
-    for (int k = 0; k < N; k++) {
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                for (int qk = 0; qk < Q; qk++) {
-                    for (int qj = 0; qj < Q; qj++) {
-                        for (int qi = 0; qi < Q; qi++) {
-                            const int ii = qi * N + i;
-                            const int jj = qj * N + j;
-                            const int kk = qk * N + k;
+//     T expected[8] = {0};
+//     for (int k = 0; k < N; k++) {
+//         for (int j = 0; j < N; j++) {
+//             for (int i = 0; i < N; i++) {
+//                 for (int qk = 0; qk < Q; qk++) {
+//                     for (int qj = 0; qj < Q; qj++) {
+//                         for (int qi = 0; qi < Q; qi++) {
+//                             const int ii = qi * N + i;
+//                             const int jj = qj * N + j;
+//                             const int kk = qk * N + k;
 
-                            const int q_kji = qk * Q * Q + qj * Q + qi;
+//                             const int q_kji = qk * Q * Q + qj * Q + qi;
 
-                            T acc = 0;
-                            // D * S * S
-                            acc += D[ii] * S[jj] * S[kk] * gx[q_kji];
-                            acc += D[ii] * S[jj] * S[kk] * gy[q_kji];
-                            acc += D[ii] * S[jj] * S[kk] * gz[q_kji];
+//                             T acc = 0;
+//                             // D * S * S
+//                             acc += D[ii] * S[jj] * S[kk] * gx[q_kji];
 
-                            // S * D * S
-                            acc += S[ii] * D[jj] * S[kk] * gx[q_kji];
-                            acc += S[ii] * D[jj] * S[kk] * gy[q_kji];
-                            acc += S[ii] * D[jj] * S[kk] * gz[q_kji];
+//                             // S * D * S
+//                             acc += S[ii] * D[jj] * S[kk] * gy[q_kji];
 
-                            // S * S * D
-                            acc += S[ii] * S[jj] * D[kk] * gx[q_kji];
-                            acc += S[ii] * S[jj] * D[kk] * gy[q_kji];
-                            acc += S[ii] * S[jj] * D[kk] * gz[q_kji];
+//                             // S * S * D
+//                             acc += S[ii] * S[jj] * D[kk] * gz[q_kji];
 
-                            expected[k * N * N + j * N + i] += acc * qw[qk] * qw[qj] * qw[qi];
-                        }
-                    }
-                }
-            }
-        }
-    }
-#endif
+//                             expected[k * N * N + j * N + i] += acc * qw[qk] * qw[qj] * qw[qi];
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// #endif
 
     spectral_hex_triad_integrate_add<N, Q, T>(D, S, S, qw, gx, out);
-    spectral_hex_triad_integrate_add<N, Q, T>(S, D, S, qw, gx, out);
-    spectral_hex_triad_integrate_add<N, Q, T>(S, S, D, qw, gx, out);
-
-    spectral_hex_triad_integrate_add<N, Q, T>(D, S, S, qw, gy, out);
     spectral_hex_triad_integrate_add<N, Q, T>(S, D, S, qw, gy, out);
-    spectral_hex_triad_integrate_add<N, Q, T>(S, S, D, qw, gy, out);
-
-    spectral_hex_triad_integrate_add<N, Q, T>(D, S, S, qw, gz, out);
-    spectral_hex_triad_integrate_add<N, Q, T>(S, D, S, qw, gz, out);
     spectral_hex_triad_integrate_add<N, Q, T>(S, S, D, qw, gz, out);
 
-#ifndef NDEBUG
-    printf("expected) %g %g %g %g, %g %g %g %g\n",
-           expected[0],
-           expected[1],
-           expected[2],
-           expected[3],
-           expected[4],
-           expected[5],
-           expected[6],
-           expected[7]);
+// #ifndef NDEBUG
+//     printf("expected) %g %g %g %g, %g %g %g %g\n",
+//            expected[0],
+//            expected[1],
+//            expected[2],
+//            expected[3],
+//            expected[4],
+//            expected[5],
+//            expected[6],
+//            expected[7]);
 
-    printf("actual) %g %g %g %g, %g %g %g %g\n", out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7]);
-#endif
+//     printf("actual) %g %g %g %g, %g %g %g %g\n", out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7]);
+// #endif
 }
 
 template void spectral_hex_laplacian_apply<2, 2, scalar_t>(const scalar_t* const SFEM_RESTRICT,
@@ -291,12 +277,6 @@ extern "C" int spectral_hex_laplacian_apply(const int                         or
     scalar_t S[2 * 2];
     scalar_t D[2 * 2];
 
-    // scalar_t qx[2] = {0, 1};
-    // scalar_t qw[2]         = {0.5, 0.5};
-
-    // scalar_t qx[2] = {0, 1};
-    // scalar_t qw[2]         = {0.5, 0.5};
-
     static const scalar_t qx[line_q2_n] = {0.2113248654, 0.7886751346};
     static const scalar_t qw[line_q2_n] = {1. / 2, 1. / 2};
 
@@ -324,6 +304,7 @@ extern "C" int spectral_hex_laplacian_apply(const int                         or
             ev[v] = elements[v][i];
         }
 
+        // Permuting from std HEX8 to lexicographic
         element_u[0] = u[ev[0]];
         element_u[1] = u[ev[1]];
         element_u[3] = u[ev[2]];
@@ -339,10 +320,9 @@ extern "C" int spectral_hex_laplacian_apply(const int                         or
 
         // Assume affine here!
         hex8_fff(lx, ly, lz, 0.5, 0.5, 0.5, fff);
-
-#if 1
         spectral_hex_laplacian_apply<2, 2, scalar_t>(S, D, fff, qw, element_u, element_vector);
 
+        // Permuting from lexicographic to std HEX8
 #pragma omp atomic update
         values[ev[0]] += element_vector[0];
 #pragma omp atomic update
@@ -360,19 +340,8 @@ extern "C" int spectral_hex_laplacian_apply(const int                         or
 #pragma omp atomic update
         values[ev[7]] += element_vector[6];
 
-#else
-        hex8_laplacian_apply_fff_integral(fff, element_u, element_vector);
-
-        for (int edof_i = 0; edof_i < 8; ++edof_i) {
-            const idx_t dof_i = ev[edof_i];
-
-#pragma omp atomic update
-            values[dof_i] += element_vector[edof_i];
-        }
-#endif
-
-#ifndef NDEBUG
-        // #if 0
+// #ifndef NDEBUG
+#if 0
         int perm[8] = {0, 1, 3, 2, 4, 5, 7, 6};
 
         for (int v = 0; v < 8; v++) {
