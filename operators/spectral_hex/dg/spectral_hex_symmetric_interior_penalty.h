14a68b8209333089319d5802d02537dcc46bc178
Patrick Zulian <patrick.zulian@gmail.com>
2025-03-27 19:00:33 +0100

    Comments for SIP implementation

--- operators/spectral_hex/dg/spectral_hex_symmetric_interior_penalty.h
+++ operators/spectral_hex/dg/spectral_hex_symmetric_interior_penalty.h
@@ -8,15 +8,17 @@
 extern "C" {
 #endif
 
-// < v n, {{ grad u }}> - < grad v, [[u]]/2> + <v n, tau [[ u ]]>>
-int spectral_hex_symmetric_interior_penalty(const int                         order,
-                                            const ptrdiff_t                   nelements,
-                                            const ptrdiff_t                   nnodes,
-                                            idx_t** const SFEM_RESTRICT       elements,
-                                            geom_t** const SFEM_RESTRICT      points,
-                                            const real_t                      tau,
-                                            const real_t* const SFEM_RESTRICT u,
-                                            real_t* const SFEM_RESTRICT       values);
+// - < v n, {{ grad u }}> - < grad v, [[u]]/2> + <v n, tau [[u]]>>
+int spectral_hex_symmetric_interior_penalty_apply(const int                                order,
+                                                  const ptrdiff_t                          nelements,
+                                                  const ptrdiff_t                          nnodes,
+                                                  idx_t** const SFEM_RESTRICT              elements,
+                                                  const element_idx_t* const SFEM_RESTRICT adj_table,
+                                                  const sshex_side_code_t* const           side_code,
+                                                  geom_t** const SFEM_RESTRICT             points,
+                                                  const real_t                             tau,
+                                                  const real_t* const SFEM_RESTRICT        u,
+                                                  real_t* const SFEM_RESTRICT              values);
 
 #ifdef __cplusplus
 }
