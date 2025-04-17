f1abcbf8f0700ecdc9e5b5e4bacfe3fce5f6239f
Patrick Zulian <patrick.zulian@gmail.com>
2025-04-17 16:06:32 +0200

    Drafted cu_sshex8_restriction

--- python/codegen/cu_ssquad4_restrict.py
+++ python/codegen/cu_ssquad4_restrict.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-
 # Pseudo code for CUDA kernel. Tiled linear interpolation on semi-structured meshes
 # (TILE_SIZE={4,8} for {2,3}D)
 
 import numpy as np
 
-from_level = 1
-to_level = 2
-padding = (to_level % 2) == 0
+from_level = 8
+to_level = 4
+padding = (from_level % 2) == 0
 
-step_factor = int(to_level / from_level)
-nsteps = int(to_level / step_factor)
+step_factor = int(from_level / to_level)
+nsteps = int(from_level / step_factor)
 
 n_points = step_factor + 1
 phi0 = np.linspace(1.0, 0.0, num=n_points)
@@ -28,20 +27,20 @@ phi[0, :] = phi0[:]
 phi[1, :] = phi1[:]
 
 # Padding, bounds check in CUDA
-rH = np.zeros((from_level + 1 + padding, from_level + 1 + padding))
+rH = np.zeros((to_level + 1 + padding, to_level + 1 + padding))
 
 # Padding, bounds check in CUDA
-rh = np.zeros((to_level + 1 + padding, to_level + 1 + padding))
+rh = np.zeros((from_level + 1 + padding, from_level + 1 + padding))
 
-for i in range(0, to_level + 1):
-    for j in range(0, to_level + 1):
+for i in range(0, from_level + 1):
+    for j in range(0, from_level + 1):
         # rh[i, j] = i * (to_level + 1) + j
-        rh[i, j] = i
-        # rh[i, j] = 1
+        # rh[i, j] = i
+        rh[i, j] = 1
 
 # Pseudo code for cuda tile level workload
-for i in range(0, from_level + padding):
-    for j in range(0, from_level + padding):
+for i in range(0, to_level + padding):
+    for j in range(0, to_level + padding):
 
         # Parallel execution on different threads ?
         for k1 in range(0, 2):
@@ -56,10 +55,10 @@ for i in range(0, from_level + padding):
                 l_end = step_factor + l_odd
                 m_end = step_factor + m_odd
 
-                if i == from_level:
+                if i == to_level:
                     l_end = l_odd + 1
 
-                if j == from_level:
+                if j == to_level:
                     m_end = m_odd + 1
 
                 for l in range(l_odd, l_end, 2):
@@ -70,15 +69,15 @@ for i in range(0, from_level + padding):
                         # Round robin execution
                         for d1 in range(0, 2):
                             for d2 in range(0, 2):
-                                c = rh[ll + d1, mm + d2] # Divide by count factor
-                                # Safe shared mem addition without atomics or intrinsics ?
+                                c = rh[ll + d1, mm + d2]  # Divide by count factor
+                                # Register add? 
                                 rH[i + k1, j + k2] += (
                                     c * phi[k1, l + d1] * phi[k2, m + d2]
-                        )
+                                )
 
                 # Atomic add to global memory ...
 
 
 # Remove padding for output (bounds check in CUDA)
-rH = rH[0 : (from_level + 1), 0 : (from_level + 1)]
+rH = rH[0 : (to_level + 1), 0 : (to_level + 1)]
 print(np.round(rH, 2))
