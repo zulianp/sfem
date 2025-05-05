cc4c889d868281a50d61d261b616f273a0b71d99
Patrick Zulian <patrick.zulian@gmail.com>
2025-05-05 12:17:56 +0200

    Removing crashes for SPMG cuda porting

--- algebra/cuda/sfem_cuda_ShiftableJacobi.cu
+++ algebra/cuda/sfem_cuda_ShiftableJacobi.cu
@@ -5,37 +5,8 @@
 
 #include "sfem_mask.h"
 
-// FIXME move to CUDA header
+#include "cu_mask.cuh"
 
-static inline __device__ int cu_mask_get(ptrdiff_t i, const mask_t* const mask) {
-    const static size_t nbits = (sizeof(mask_t) * 8);
-    const ptrdiff_t     idx   = i / nbits;
-    mask_t              shift = i - idx * nbits;
-
-    assert(shift < nbits);
-    assert(shift >= 0);
-
-    mask_t m;
-    m        = mask[idx];
-    mask_t q = 1 << shift;
-    return !!(m & q);
-}
-
-// static inline __device__ void cu_mask_set(ptrdiff_t i, mask_t* const mask) {
-//     const static size_t nbits = (sizeof(mask_t) * 8);
-//     const ptrdiff_t     idx   = i / nbits;
-//     mask_t              shift = i - idx * nbits;
-//     mask_t              q     = 1 << shift;
-//     mask[idx] |= q;
-// }
-
-// static inline __device__ void cu_mask_unset(ptrdiff_t i, mask_t* const mask) {
-//     const static size_t nbits = (sizeof(mask_t) * 8);
-//     const ptrdiff_t     idx   = i / nbits;
-//     mask_t              shift = i - idx * nbits;
-//     mask_t              q     = 1 << shift;
-//     mask[idx]                 = q ^ mask[idx];
-// }
 
 namespace sfem {
     namespace private_ {
