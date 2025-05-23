47b9e93bd75cb847e75b3b88a69e72a336adc04f
Simone <sriva@susePC>
2025-05-23 10:07:46 +0200

    Refactor Jacobian Determinant Functions and Add New Resampling Function
    - Refactored the Jacobian determinant functions for tetrahedron categories (0 to 5) in `hyteg_jacobian_double.c` to improve readability by formatting the mathematical expressions.
    - Added a new function prototype `tet4_resample_field_local_refine_adjoint_hyteg` in `sfem_resample_field.h` for local refinement of tetrahedral elements using the HyTEG method.

---
+++ resampling/adjoint/sfem_resample_field_adjoint_hyteg.c
@@ -0,0 +1,119 @@
+#include "sfem_resample_field.h"
+#include "sfem_resample_field_tet4_math.h"
+#include "sfem_stack.h"
+
+#include "mass.h"
+// #include "read_mesh.h"
+#include "matrixio_array.h"
+
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+// #define real_t real_type
+
+#include "hyteg.h"
+#include "quadratures_rule.h"
+
+#define real_type real_t
+#define SFEM_RESTRICT __restrict__
+
+#define SFEM_RESAMPLE_GAP_DUAL
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+// tet4_resample_field_local_refine_adjoint_hyteg ////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+int                                                                                                  //
+tet4_resample_field_local_refine_adjoint_hyteg(const ptrdiff_t                      start_element,   // Mesh
+                                               const ptrdiff_t                      end_element,     //
+                                               const ptrdiff_t                      nnodes,          //
+                                               const idx_t** const SFEM_RESTRICT    elems,           //
+                                               const geom_t** const SFEM_RESTRICT   xyz,             //
+                                               const ptrdiff_t* const SFEM_RESTRICT n,               // SDF
+                                               const ptrdiff_t* const SFEM_RESTRICT stride,          //
+                                               const geom_t* const SFEM_RESTRICT    origin,          //
+                                               const geom_t* const SFEM_RESTRICT    delta,           //
+                                               const real_t* const SFEM_RESTRICT    weighted_field,  // Input weighted field
+                                               const real_t                         alpha_th,        // Threshold for alpha
+                                               real_t* const SFEM_RESTRICT          data) {                   //
+
+    PRINT_CURRENT_FUNCTION;
+    int ret = 0;
+
+    const real_type ox = (real_type)origin[0];
+    const real_type oy = (real_type)origin[1];
+    const real_type oz = (real_type)origin[2];
+
+    const real_type dx = (real_type)delta[0];
+    const real_type dy = (real_type)delta[1];
+    const real_type dz = (real_type)delta[2];
+
+    const real_type d_min = dx < dy ? (dx < dz ? dx : dz) : (dy < dz ? dy : dz);
+
+    const real_type hexahedron_volume = dx * dy * dz;
+
+#if SFEM_LOG_LEVEL >= 5
+    printf("============================================================\n");
+    printf("Start: %s: %s:%d \n", __FUNCTION__, __FILE__, __LINE__);
+    printf("Heaxahedron volume = %g\n", hexahedron_volume);
+    printf("============================================================\n");
+#endif
+
+    int degenerated_tetrahedra_cnt = 0;
+    int uniform_refine_cnt         = 0;
+
+    for (ptrdiff_t element_i = start_element; element_i < end_element; element_i++) {
+        // loop over the 4 vertices of the tetrahedron
+        idx_t ev[4];
+        for (int v = 0; v < 4; ++v) {
+            ev[v] = elems[v][element_i];
+        }
+
+        // Read the coordinates of the vertices of the tetrahedron
+        const real_type x0 = xyz[0][ev[0]];
+        const real_type x1 = xyz[0][ev[1]];
+        const real_type x2 = xyz[0][ev[2]];
+        const real_type x3 = xyz[0][ev[3]];
+
+        const real_type y0 = xyz[1][ev[0]];
+        const real_type y1 = xyz[1][ev[1]];
+        const real_type y2 = xyz[1][ev[2]];
+        const real_type y3 = xyz[1][ev[3]];
+
+        const real_type z0 = xyz[2][ev[0]];
+        const real_type z1 = xyz[2][ev[1]];
+        const real_type z2 = xyz[2][ev[2]];
+        const real_type z3 = xyz[2][ev[3]];
+
+        // Compute the alpha_tet to decide if the tetrahedron is refined
+        // Sides of the tetrahedron
+        real_type edges_length[6];
+
+        int vertex_a = -1;
+        int vertex_b = -1;
+
+        const real_type max_edges_length =          //
+                tet_edge_max_length(x0,             //
+                                    y0,             //
+                                    z0,             //
+                                    x1,             //
+                                    y1,             //
+                                    z1,             //
+                                    x2,             //
+                                    y2,             //
+                                    z2,             //
+                                    x3,             //
+                                    y3,             //
+                                    z3,             //
+                                    &vertex_a,      // Output
+                                    &vertex_b,      // Output
+                                    edges_length);  // Output
+
+        const real_t alpha_tet = max_edges_length / d_min;
+    }
+
+    RETURN_FROM_FUNCTION(ret);
+}
--- resampling/hyteg/hyteg_jacobian_double.c
+++ resampling/hyteg/hyteg_jacobian_double.c
@@ -7,75 +7,92 @@
 #include "hyteg_jacobian_double.h"
 #include <math.h>
 
-
-double det_jacobian_cat0_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat0_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 0
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
 
-double det_jacobian_cat1_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat1_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 1
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
 
-double det_jacobian_cat2_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat2_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 2
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
 
-double det_jacobian_cat3_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat3_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 3
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
 
-double det_jacobian_cat4_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat4_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 4
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
 
-double det_jacobian_cat5_dbl(
-    const double x0, const double y0, const double z0, //
-    const double x1, const double y1, const double z1, // 
-    const double x2, const double y2, const double z2, //
-    const double x3, const double y3, const double z3, //
-    const double L //
+double det_jacobian_cat5_dbl(const double x0, const double y0, const double z0,  //
+                             const double x1, const double y1, const double z1,  //
+                             const double x2, const double y2, const double z2,  //
+                             const double x3, const double y3, const double z3,  //
+                             const double L                                      //
 ) {
     // Calculate determinant of Jacobian for tetrahedron category 5
-    double det = (-x0*y1*z2 + x0*y1*z3 + x0*y2*z1 - x0*y2*z3 - x0*y3*z1 + x0*y3*z2 + x1*y0*z2 - x1*y0*z3 - x1*y2*z0 + x1*y2*z3 + x1*y3*z0 - x1*y3*z2 - x2*y0*z1 + x2*y0*z3 + x2*y1*z0 - x2*y1*z3 - x2*y3*z0 + x2*y3*z1 + x3*y0*z1 - x3*y0*z2 - x3*y1*z0 + x3*y1*z2 + x3*y2*z0 - x3*y2*z1)/pow(L, 3);
+    double det = (-x0 * y1 * z2 + x0 * y1 * z3 + x0 * y2 * z1 - x0 * y2 * z3 - x0 * y3 * z1 + x0 * y3 * z2 + x1 * y0 * z2 -
+                  x1 * y0 * z3 - x1 * y2 * z0 + x1 * y2 * z3 + x1 * y3 * z0 - x1 * y3 * z2 - x2 * y0 * z1 + x2 * y0 * z3 +
+                  x2 * y1 * z0 - x2 * y1 * z3 - x2 * y3 * z0 + x2 * y3 * z1 + x3 * y0 * z1 - x3 * y0 * z2 - x3 * y1 * z0 +
+                  x3 * y1 * z2 + x3 * y2 * z0 - x3 * y2 * z1) /
+                 pow(L, 3);
     return det;
 }
