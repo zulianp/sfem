3cf26aefc59aa46860c6d68525c135608eb5f4dd
Patrick Zulian <patrick.zulian@gmail.com>
2025-06-20 20:40:58 +0200

    ...

--- python/utils/quantize.py
+++ python/utils/quantize.py
@@ -2,32 +2,12 @@
 
 import numpy as np
 
-real_t = np.float64
-
-prec = 64
-prec = 32
-
-if prec == 64:
-	quant_t = np.int64
-	max_quant_t = real_t(2)**(56-1e-9)
-	M = 1
-elif prec == 32:
-	quant_t = np.int32
-	max_quant_t = real_t(2)**(31-1e-9)
-	M = 2
-else:
-	quant_t = np.int8
-	max_quant_t = real_t(2)**(7-1e-9)
-	M = 8
-
-n = 1000
-seg_size = n
-nfactors = n // seg_size
-
-a = np.linspace(0.1, (n-1)*10000, n, dtype=real_t)
-
-def quantize(real_t, a, quant_t, max_quant_t, M, nfactors = len(a)):
+def quantize(real_t, a, quant_t, max_quant_t, M, nfactors = None):
 	n = len(a)
+	
+	if nfactors == None:
+		nfactors = n
+
 	factors = np.ones(nfactors, dtype=real_t)
 	q = []
 
@@ -75,11 +55,45 @@ def dequantize(quant_t, max_quant_t, q, factors, real_t):
 		a_reconstructed[s:e] *= factors[fact]  
 	return a_reconstructed
 
-q, factors = quantize(real_t, a, quant_t, max_quant_t, M, nfactors)
-a_reconstructed = dequantize(quant_t, max_quant_t, q, factors, real_t)
 
-diff = a_reconstructed - a
-print('1 norm ', np.sum(np.abs(diff)))
-print('2 norm ', np.sum(np.sqrt(np.sum(diff * diff))))
+if __name__ == '__main__':
 
-print('1 rel norm ', np.sum(np.abs(diff/a)))
+	real_t = np.float64
+	# real_t = np.float32
+
+	# prec = 64
+	# prec = 32
+	# prec = 16
+	prec = 8
+
+	if prec == 64:
+		quant_t = np.int64
+		max_quant_t = real_t(2)**(56-1e-9)
+		M = 1
+	elif prec == 32:
+		quant_t = np.int32
+		max_quant_t = real_t(2)**(31-1e-9)
+		M = 2
+	elif prec == 16:
+		quant_t = np.int32
+		max_quant_t = real_t(2)**(15-1e-9)
+		M = 4
+	else:
+		quant_t = np.int8
+		max_quant_t = real_t(2)**(7-1e-9)
+		M = 8
+
+	n = 1000090
+	seg_size = n
+	nfactors = n // seg_size
+
+	a = np.linspace(0.1, (n-1)*10000, n, dtype=real_t)
+
+	q, factors = quantize(real_t, a, quant_t, max_quant_t, M, nfactors)
+	a_reconstructed = dequantize(quant_t, max_quant_t, q, factors, real_t)
+
+	diff = a_reconstructed - a
+	print('1 norm     ', np.sum(np.abs(diff)))
+	print('2 norm     ', np.sum(np.sqrt(np.sum(diff * diff))))
+	print('RMSE       ', np.sum(np.sqrt(np.sum(diff * diff)/n)))
+	print('1 rel norm ', np.sum(np.abs(diff/a)))
