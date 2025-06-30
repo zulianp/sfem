f59a41144142ae24912f890ca84d5e34f37759c2
Patrick Zulian <patrick.zulian@gmail.com>
2025-06-30 17:59:43 +0200

    ...

--- frontend/ops/sfem_SemiStructuredEMLaplacian.hpp
+++ frontend/ops/sfem_SemiStructuredEMLaplacian.hpp
@@ -22,7 +22,7 @@ public:
     int gradient(const real_t *const x, real_t *const out) override;
     int apply(const real_t *const x, const real_t *const h, real_t *const out) override;
     int value(const real_t *x, real_t *const out) override;
-    // int report(const real_t *const) override;
-    // std::shared_ptr<Op> clone() const override;
+    int report(const real_t *const) override;
+    std::shared_ptr<Op> clone() const override;
 };
 } // namespace sfem 
