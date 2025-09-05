ef1695e0f611a97b44b3816592cb445ea8b60be0
Patrick Zulian <patrick.zulian@gmail.com>
2025-09-05 13:24:56 +0200

    Plotting connectivity

--- drivers/mesh/clustering.exe.cpp
+++ drivers/mesh/clustering.exe.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
     auto elem2cluster      = sfem::create_host_buffer<idx_t>(n_elements);
     auto elem2cluster_data = elem2cluster->data();
 
-    // int nsxe = elem_num_sides(side_type(mesh->element_type()));
+    int sxe = elem_num_sides(mesh->element_type());
 
     // Initialize all elements as unassigned
     for (ptrdiff_t i = 0; i < n_elements; i++) {
@@ -78,7 +78,10 @@ int main(int argc, char *argv[]) {
             current_cluster++;
             if (current_cluster >= n_clusters) break;
 
-            printf("Cluster %ld is full, starting new cluster, %ld / %ld elements assigned\n", current_cluster-1, elements_assigned, n_elements);
+            printf("Cluster %ld is full, starting new cluster, %ld / %ld elements assigned\n",
+                   current_cluster - 1,
+                   elements_assigned,
+                   n_elements);
         }
 
         // Find the best unassigned element for current cluster
@@ -86,22 +89,39 @@ int main(int argc, char *argv[]) {
         idx_t     best_connectivity = -1;
         ptrdiff_t best_elem_idx     = -1;
 
-        for (ptrdiff_t i = 0; i < unassigned_elements.size(); i++) {
+        ptrdiff_t start = unassigned_elements.size() - 1;
+        ptrdiff_t end = -1;
+        ptrdiff_t inc = -1;
+
+        if(!cluster_sizes[current_cluster]) {
+            // Do not take it from the last neighbors when creating a new cluster
+            start = 0;
+            end = unassigned_elements.size();
+            inc = 1;
+        }
+
+        for (ptrdiff_t i = start; i != end; i += inc) {
             ptrdiff_t elem = unassigned_elements[i];
             if (elem2cluster_data[elem] == SFEM_IDX_INVALID) {
                 idx_t connectivity = calculate_connectivity(elem, current_cluster);
 
                 // Prefer elements with higher connectivity to current cluster
-                if (connectivity > best_connectivity || (connectivity == best_connectivity && elem < best_elem)) {
+                if (connectivity > best_connectivity) {
                     best_elem         = elem;
                     best_connectivity = connectivity;
                     best_elem_idx     = i;
+
+                    if (best_connectivity == sxe) {
+                        break;
+                    }
                 }
             }
         }
 
         // If no element found, take the first unassigned one
         if (best_elem == -1) {
+            fprintf(stderr, "Should never come here!\n");
+            // for (ptrdiff_t i = unassigned_elements.size() - 1; i >= 0 ; i--) {
             for (ptrdiff_t i = 0; i < unassigned_elements.size(); i++) {
                 ptrdiff_t elem = unassigned_elements[i];
                 if (elem2cluster_data[elem] == SFEM_IDX_INVALID) {
@@ -124,21 +144,24 @@ int main(int argc, char *argv[]) {
         std::swap(unassigned_elements[best_elem_idx], unassigned_elements.back());
         unassigned_elements.pop_back();
 
-        for(ptrdiff_t k = adj_ptr[best_elem]; k < adj_ptr[best_elem+1]; k++) {
+        for (ptrdiff_t k = adj_ptr[best_elem]; k < adj_ptr[best_elem + 1]; k++) {
             ptrdiff_t neigh = adj_idx[k];
-            if(elem2cluster_data[neigh] == SFEM_IDX_INVALID) {
+            if (elem2cluster_data[neigh] == SFEM_IDX_INVALID) {
                 unassigned_elements.push_back(neigh);
             }
         }
     }
 
+    assert(elements_assigned == n_elements);
+
     // Output
     sfem::create_directory(output_folder.c_str());
 
     // Write the clustering mapping as binary file
-    elem2cluster->to_file((output_folder + "/elem2cluster.raw").c_str());
+    // elem2cluster->to_file((output_folder + "/elem2cluster.raw").c_str());
 
     // Write reordered mesh
+    mesh->reorder_elements_from_tags(elem2cluster);
     mesh->write(output_folder.c_str());
 
     if (!rank) {
@@ -147,12 +170,19 @@ int main(int argc, char *argv[]) {
         idx_t max_connectivity   = 0;
         idx_t min_connectivity   = n_elements;
 
+        auto econ = sfem::create_host_buffer<int>(n_elements);
+        auto d_econ = econ->data();
+
         for (ptrdiff_t i = 0; i < n_elements; i++) {
             idx_t connectivity = calculate_connectivity(i, elem2cluster_data[i]);
+            d_econ[i] = connectivity;
             total_connectivity += connectivity;
             if (connectivity > max_connectivity) max_connectivity = connectivity;
             if (connectivity < min_connectivity) min_connectivity = connectivity;
         }
+
+
+        econ->to_file((output_folder + "/connectivity.raw").c_str());
 
         double avg_connectivity = (double)total_connectivity / n_elements;
 
@@ -164,6 +194,9 @@ int main(int argc, char *argv[]) {
         printf("  Min connectivity: %d\n", min_connectivity);
         printf("Output files written to: %s\n", output_folder.c_str());
     }
+
+    
+
 
     return MPI_Finalize();
 }
