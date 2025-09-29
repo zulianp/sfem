e708107ab160751723b140bd7fa070a9df36ffb9
Simone <sriva@susePC>
2025-09-29 15:11:47 +0200

    Fix variable name typo and improve CSV output handling in benchmark script

--- workflows/resample/bench_adjoint.sh
+++ workflows/resample/bench_adjoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 export SFEM_CLUSTER_SIZE_ADJOINT=256
-export SFEM_TETS_PER_BLOCK_ADJOINT=16
+export SFEM_TET_PER_BLOCK_ADJOINT=16
 export SFEM_REPETITIONS_ADJOINT=10
+
+export OUT_FILENAME_RAW=/capstor/scratch/cscs/sriva/prj/sfem_d/sfem/workflows/resample/test_field.raw
 
 
 output_file="resuts_adjoint.csv"
+rm -f "$output_file"
+
 
 # Scrive l'intestazione nel file CSV
 echo "cluster_size,tet_per_block,nelements,closk,Throughtput" > "$output_file"
@@ -18,21 +22,17 @@ tets_per_block_list=(4 6 8 12 16 24 32)
 
 for cluster_size in "${cluster_size_list[@]}"; do
     for tets_per_block in "${tets_per_block_list[@]}"; do
+
         export SFEM_CLUSTER_SIZE_ADJOINT=$cluster_size
-        export SFEM_TETS_PER_BLOCK_ADJOINT=$tets_per_block
-        # echo "Running with cluster size: $cluster_size, tets per block: $tets_per_block \n"
+        export SFEM_TET_PER_BLOCK_ADJOINT=$tets_per_block
+        echo "Running with cluster size: $cluster_size, tets per block: $tets_per_block"
 
         # Esegue l'esempio, filtra la riga di benchmark e la aggiunge al file CSV
-        ./example.sh | grep "<cluster_bench>" | while read -r line; do
-            # Estrae i dati dopo <cluster_bench>
-            data=$(echo "$line" | sed 's/<cluster_bench> *//')
-            # Sostituisce gli spazi con virgole
-            csv_data=$(echo "$data" | tr -s ' ' ',')
-            # Scrive la riga completa nel file CSV
-            echo "$data" >> "$output_file"
-            echo "$data"
-        done
-
+        line=$(./example.sh | grep "<cluster_bench>")
+        data=$(echo "$line" | sed 's/<cluster_bench> *//')
+        
+        echo "$data" >> "$output_file"
+        echo "$data"
 
     done
 done
