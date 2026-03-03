dff5a18bb5fdb124cbe6517f0c845360151a1ccf
Simone <simone.riva@usi.ch>
2026-03-03 11:50:28 +0100

    Cell List for Adjoint Resampling - Main version ready and working. - debug variant of the cell list transfer.

--- resampling/adjoint/sfem_resample_field_adjoint_cell.c
+++ resampling/adjoint/sfem_resample_field_adjoint_cell.c
@@ -139,9 +139,9 @@ update_hex_quad_node(const real_t                         x,               // Ph
 
     const real_t *inv_J_tet = &(mesh_geom->inv_Jacobian[index_tet * 9]);  // Inverse Jacobian for the current tet
 
-    const real_t x0_n = mesh_geom->vetices_zero[ev[0] * 3 + 0];  // x coordinate of vertex 0
-    const real_t y0_n = mesh_geom->vetices_zero[ev[0] * 3 + 1];  // y coordinate of vertex 0
-    const real_t z0_n = mesh_geom->vetices_zero[ev[0] * 3 + 2];  // z coordinate of vertex 0
+    const real_t x0_n = mesh_geom->vetices_zero[index_tet * 3 + 0];  // x coordinate of vertex 0
+    const real_t y0_n = mesh_geom->vetices_zero[index_tet * 3 + 1];  // y coordinate of vertex 0
+    const real_t z0_n = mesh_geom->vetices_zero[index_tet * 3 + 2];  // z coordinate of vertex 0
 
     // Compute the coordinates of the quadrature point in the reference tetrahedron using the inverse Jacobian transformation.
     const real_t x_o = x - x0_n;
@@ -159,35 +159,37 @@ update_hex_quad_node(const real_t                         x,               // Ph
 
     const real_t wf_quad = f0 * wf0 + f1 * wf1 + f2 * wf2 + f3 * wf3;
 
-    const real_t hex8_f0 = (1.0 - l_x) * (1.0 - l_y) * (1.0 - l_z);
-    const real_t hex8_f1 = l_x * (1.0 - l_y) * (1.0 - l_z);
-    const real_t hex8_f2 = l_x * l_y * (1.0 - l_z);
-    const real_t hex8_f3 = (1.0 - l_x) * l_y * (1.0 - l_z);
-    const real_t hex8_f4 = (1.0 - l_x) * (1.0 - l_y) * l_z;
-    const real_t hex8_f5 = l_x * (1.0 - l_y) * l_z;
-    const real_t hex8_f6 = l_x * l_y * l_z;
-    const real_t hex8_f7 = (1.0 - l_x) * l_y * l_z;
+    const real_t one_minus_lx = (1.0 - l_x);
+    const real_t one_minus_ly = (1.0 - l_y);
+    const real_t one_minus_lz = (1.0 - l_z);
+
+    const real_t c0 = one_minus_lx * one_minus_ly;
+    const real_t c1 = l_x * one_minus_ly;
+    const real_t c2 = l_x * l_y;
+    const real_t c3 = one_minus_lx * l_y;
+
+    const real_t hex8_f0 = c0 * one_minus_lz;
+    const real_t hex8_f1 = c1 * one_minus_lz;
+    const real_t hex8_f2 = c2 * one_minus_lz;
+    const real_t hex8_f3 = c3 * one_minus_lz;
+    const real_t hex8_f4 = c0 * l_z;
+    const real_t hex8_f5 = c1 * l_z;
+    const real_t hex8_f6 = c2 * l_z;
+    const real_t hex8_f7 = c3 * l_z;
 
     const real_t wf_quad_QW = wf_quad * phys_w;
 
-    // hex_element_field[base_index + off0] += wf_quad_QW * hex8_f0;
-    // hex_element_field[base_index + off1] += wf_quad_QW * hex8_f1;
-    // hex_element_field[base_index + off2] += wf_quad_QW * hex8_f2;
-    // hex_element_field[base_index + off3] += wf_quad_QW * hex8_f3;
-    // hex_element_field[base_index + off4] += wf_quad_QW * hex8_f4;
-    // hex_element_field[base_index + off5] += wf_quad_QW * hex8_f5;
-    // hex_element_field[base_index + off6] += wf_quad_QW * hex8_f6;
-    // hex_element_field[base_index + off7] += wf_quad_QW * hex8_f7;
+    hex_element_field[base_index + off0] += wf_quad_QW * hex8_f0;
+    hex_element_field[base_index + off1] += wf_quad_QW * hex8_f1;
+    hex_element_field[base_index + off2] += wf_quad_QW * hex8_f2;
+    hex_element_field[base_index + off3] += wf_quad_QW * hex8_f3;
+    hex_element_field[base_index + off4] += wf_quad_QW * hex8_f4;
+    hex_element_field[base_index + off5] += wf_quad_QW * hex8_f5;
+    hex_element_field[base_index + off6] += wf_quad_QW * hex8_f6;
+    hex_element_field[base_index + off7] += wf_quad_QW * hex8_f7;
 
-    hex_element_field[base_index + off0] = 1.0;
-    hex_element_field[base_index + off1] = 1.0;
-    hex_element_field[base_index + off2] = 1.0;
-    hex_element_field[base_index + off3] = 1.0;
-    hex_element_field[base_index + off4] = 1.0;
-    hex_element_field[base_index + off5] = 1.0;
-    hex_element_field[base_index + off6] = 1.0;
-    hex_element_field[base_index + off7] = 1.0;
-}
+    return 0;
+}  // END Function: update_hex_quad_node
 
 //////////////////////////////////////////////
 // update_hex_field
@@ -195,7 +197,7 @@ update_hex_quad_node(const real_t                         x,               // Ph
 int                                                                      //
 update_hex_quad_node_vz(const real_t                         x,          // Physical x coordinate of the quadrature point
                         const real_t                         y,          // Physical y coordinate of the quadrature point
-                        const real_t                        *z,          // Physical z coordinate of the quadrature point
+                        const real_t                        *z,          // Physical z coordinate of the quadrature points
                         const ptrdiff_t                      z_size,     // Size of the z array (number of z values to process)
                         const real_t                         phys_w,     // Quadrature weight for the quadrature point
                         const ptrdiff_t                      index_tet,  // The index of the tet containing the quadrature point
@@ -313,7 +315,7 @@ update_hex_quad_node_vz(const real_t                         x,          // Phys
 
     // Process all buffered values with efficient vectorized loop
 
-    real_t hex_element_field_local[8] = {0};  // Local array to accumulate contributions for the hex element field
+    real_t hex_element_field_local[MAX_Z_SIZE][8] = {0};  // Local array to accumulate contributions for the hex element field
 
     // Precalculate factors that are independent from idx
     const real_t one_minus_lx = (1.0 - l_x);
@@ -352,25 +354,38 @@ update_hex_quad_node_vz(const real_t                         x,          // Phys
 
         const real_t wf_quad_QW = wf_quad * phys_w;
 
-        hex_element_field_local[0] += wf_quad_QW * hex8_f0;
-        hex_element_field_local[1] += wf_quad_QW * hex8_f1;
-        hex_element_field_local[2] += wf_quad_QW * hex8_f2;
-        hex_element_field_local[3] += wf_quad_QW * hex8_f3;
-        hex_element_field_local[4] += wf_quad_QW * hex8_f4;
-        hex_element_field_local[5] += wf_quad_QW * hex8_f5;
-        hex_element_field_local[6] += wf_quad_QW * hex8_f6;
-        hex_element_field_local[7] += wf_quad_QW * hex8_f7;
+        hex_element_field_local[idx][0] += wf_quad_QW * hex8_f0;
+        hex_element_field_local[idx][1] += wf_quad_QW * hex8_f1;
+        hex_element_field_local[idx][2] += wf_quad_QW * hex8_f2;
+        hex_element_field_local[idx][3] += wf_quad_QW * hex8_f3;
+        hex_element_field_local[idx][4] += wf_quad_QW * hex8_f4;
+        hex_element_field_local[idx][5] += wf_quad_QW * hex8_f5;
+        hex_element_field_local[idx][6] += wf_quad_QW * hex8_f6;
+        hex_element_field_local[idx][7] += wf_quad_QW * hex8_f7;
     }  // END for (idx)
 
     // After processing all z values, accumulate the local contributions into the global hex element field
-    hex_element_field[off0] += hex_element_field_local[0];
-    hex_element_field[off1] += hex_element_field_local[1];
-    hex_element_field[off2] += hex_element_field_local[2];
-    hex_element_field[off3] += hex_element_field_local[3];
-    hex_element_field[off4] += hex_element_field_local[4];
-    hex_element_field[off5] += hex_element_field_local[5];
-    hex_element_field[off6] += hex_element_field_local[6];
-    hex_element_field[off7] += hex_element_field_local[7];
+    for (ptrdiff_t idx = 0; idx < z_size; ++idx) {
+        const ptrdiff_t base_index = base_index_buffer[idx];
+
+        hex_element_field[base_index + off0] += hex_element_field_local[idx][0];
+        hex_element_field[base_index + off1] += hex_element_field_local[idx][1];
+        hex_element_field[base_index + off2] += hex_element_field_local[idx][2];
+        hex_element_field[base_index + off3] += hex_element_field_local[idx][3];
+        hex_element_field[base_index + off4] += hex_element_field_local[idx][4];
+        hex_element_field[base_index + off5] += hex_element_field_local[idx][5];
+        hex_element_field[base_index + off6] += hex_element_field_local[idx][6];
+        hex_element_field[base_index + off7] += hex_element_field_local[idx][7];
+    }
+
+    // hex_element_field[off0] += hex_element_field_local[0];
+    // hex_element_field[off1] += hex_element_field_local[1];
+    // hex_element_field[off2] += hex_element_field_local[2];
+    // hex_element_field[off3] += hex_element_field_local[3];
+    // hex_element_field[off4] += hex_element_field_local[4];
+    // hex_element_field[off5] += hex_element_field_local[5];
+    // hex_element_field[off6] += hex_element_field_local[6];
+    // hex_element_field[off7] += hex_element_field_local[7];
 
     return 0;
 }  // END Function: update_hex_quad_node_vz
@@ -402,7 +417,7 @@ compress_and_reorder(int    *keyArray,  //
 
     // Return the count of valid items
     return newCount;
-}
+}  // END Function: compress_and_reorder
 
 //////////////////////////////////////////////
 // update_hex_field
@@ -464,10 +479,11 @@ update_hex_field(cell_list_split_3d_2d_map_t         *split_map,       // Cell l
                                                              z_size,        //
                                                              tet_indices);  //
 
+        compress_and_reorder(tet_indices, z_array, z_size);
+
         for (ptrdiff_t k = 0; k < z_size; k++) {
-            if (tet_indices[k] < 0) {
-                continue;  // No tet found for this z level, skip to the next
-            }
+            // If compress and reorder is called it is ensured that after the first -1 there is no others tets.
+            if (tet_indices[k] < 0) break;
 
             update_hex_quad_node(phys_x,          //
                                  phys_y,          //
@@ -495,6 +511,102 @@ update_hex_field(cell_list_split_3d_2d_map_t         *split_map,       // Cell l
     tet_indices = NULL;
 
     return 0;
+}  // END Function: update_hex_field
+
+//////////////////////////////////////////////
+// update_hex_field_vz
+//////////////////////////////////////////////
+int                                                                       //
+update_hex_field_vz(cell_list_split_3d_2d_map_t         *split_map,       // Cell list split map data structure
+                    boxes_t                             *boxes,           // Boxes data structure
+                    mesh_tet_geom_t                     *mesh_geom,       // Mesh geometry data structure
+                    const ptrdiff_t                      i_grid,          // The i index of the grid point in the hex mesh
+                    const ptrdiff_t                      j_grid,          // The j index of the grid point in the hex mesh
+                    const mesh_t *const SFEM_RESTRICT    mesh,            // Mesh: mesh_t struct
+                    const ptrdiff_t *const SFEM_RESTRICT n,               // SDF: n[3]
+                    const ptrdiff_t *const SFEM_RESTRICT stride,          // SDF: stride[3]
+                    const geom_t *const SFEM_RESTRICT    origin,          // SDF: origin[3]
+                    const geom_t *const SFEM_RESTRICT    delta,           // SDF: delta[3]
+                    const real_t *const SFEM_RESTRICT    weighted_field,  // Weighted field
+                    real_t *const SFEM_RESTRICT          hex_field) {              // Output field for the hex cell containing (x,y,z)
+
+    // get the physical coordinates of the grid point (i_grid, j_grid) in the hex mesh.
+    const real_t grid_x = origin[0] + i_grid * delta[0];
+    const real_t grid_y = origin[1] + j_grid * delta[1];
+    const int    z_size = n[2];
+
+    const real_t *quad_x = get_Q_nodes_x_p();
+    const real_t *quad_y = get_Q_nodes_y_p();
+    const real_t *quad_z = get_Q_nodes_z_p();
+    const real_t *quad_w = get_Q_weights_p();
+    const int     dim_q  = get_dim_qad();
+
+    const real_t hex_volume = delta[0] * delta[1] * delta[2];
+
+    real_t *z_array     = malloc(z_size * sizeof(real_t));
+    int    *tet_indices = malloc(z_size * sizeof(int));
+
+    for (int q_ijk = 0; q_ijk < dim_q; q_ijk++) {
+        const real_t q_x = quad_x[q_ijk];
+        const real_t q_y = quad_y[q_ijk];
+        const real_t q_z = quad_z[q_ijk];
+        const real_t q_w = quad_w[q_ijk];
+
+        const real_t phys_x      = grid_x + q_x * delta[0];
+        const real_t phys_y      = grid_y + q_y * delta[1];
+        const real_t phys_z_base = origin[2] + q_z * delta[2];
+
+        // Populate z_array with the physical z coordinates for the current quadrature point
+        const real_t delta_z = delta[2];
+        for (ptrdiff_t k = 0; k < z_size; k++) {
+            z_array[k] = phys_z_base + (real_t)k * delta_z;
+        }
+
+        query_cell_list_3d_2d_split_map_mesh_given_xy_tets_v(split_map,     //
+                                                             boxes,         //
+                                                             mesh_geom,     //
+                                                             phys_x,        //
+                                                             phys_y,        //
+                                                             z_array,       //
+                                                             z_size,        //
+                                                             tet_indices);  //
+
+        compress_and_reorder(tet_indices, z_array, z_size);
+
+        real_t vz[MAX_Z_SIZE];  // Buffer to hold vz values for the current quadrature point
+                                // real_t phys_w[MAX_Z_SIZE];  // Buffer to hold physical weights for the current quadrature point
+
+        real_t phys_w = q_w * hex_volume;
+        // Assuming the weight is the same for all z values for this quadrature point, adjust if needed
+
+        ptrdiff_t k = 0;
+        while (true) {
+            const ptrdiff_t tet_index    = tet_indices[k];
+            ptrdiff_t       vz_index_loc = 0;
+
+            while (k < z_size && tet_index == tet_indices[k] && vz_index_loc < MAX_Z_SIZE) {
+                vz[vz_index_loc] = z_array[k];
+                vz_index_loc += 1;
+                k += 1;
+            }
+
+            update_hex_quad_node_vz(phys_x,          //
+                                    phys_y,          //
+                                    vz,              //
+                                    vz_index_loc,    //
+                                    phys_w,          //
+                                    tet_index,       //
+                                    mesh,            //
+                                    mesh_geom,       //
+                                    stride,          //
+                                    origin,          //
+                                    delta,           //
+                                    weighted_field,  //
+                                    hex_field);      //
+
+            if (k >= z_size) break;
+        }
+    }
 }
 
 //////////////////////////////////////////////
@@ -519,23 +631,23 @@ transfer_to_hex_field_cell_tet4(cell_list_split_3d_2d_map_t         *split_map, 
 
     for (ptrdiff_t i_grid = 0; i_grid < x_size; i_grid++) {
         for (ptrdiff_t j_grid = 0; j_grid < y_size; j_grid++) {
-            update_hex_field(split_map,       //
-                             boxes,           //
-                             mesh_geom,       //
-                             i_grid,          //
-                             j_grid,          //
-                             mesh,            //
-                             n,               //
-                             stride,          //
-                             origin,          //
-                             delta,           //
-                             weighted_field,  //
-                             hex_field);      //
+            update_hex_field_vz(split_map,       //
+                                boxes,           //
+                                mesh_geom,       //
+                                i_grid,          //
+                                j_grid,          //
+                                mesh,            //
+                                n,               //
+                                stride,          //
+                                origin,          //
+                                delta,           //
+                                weighted_field,  //
+                                hex_field);      //
         }
     }
 
     RETURN_FROM_FUNCTION(0);
-}
+}  // END Function: transfer_to_hex_field_cell_tet4
 
 /////////////////////////////////////////////////
 // tet4_resample_field_adjoint_hex_quad_norm
@@ -630,4 +742,4 @@ tet4_resample_field_adjoint_cell_quad(const ptrdiff_t                      start
     free_side_length_histograms(&histograms);
 
     RETURN_FROM_FUNCTION(0);
-}
+}  // END Function: tet4_resample_field_adjoint_cell_quad
