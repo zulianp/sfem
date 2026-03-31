754907b72258496c1802dafd93c55f1473114000
Patrick Zulian <patrick.zulian@gmail.com>
2026-03-31 11:40:29 +0200

    Fixed hyperelasticity workflow, still some bugs in surface sideset creation

--- workflows/hyperelasticity/hyperelasticity.sh
+++ workflows/hyperelasticity/hyperelasticity.sh
@@ -3,7 +3,6 @@
 set -e
 
 source $CODE_DIR/merge_git_repos/sfem/venv/bin/activate
-# export SFEM_PATH=$INSTALL_DIR/sfem
 
 if [[ -z "$SFEM_PATH" ]]
 then
@@ -20,43 +19,39 @@ export PATH=$CODE_DIR/merge_git_repos/sfem/data/benchmarks/meshes:$PATH
 
 HERE=$PWD
 
-rm -rf aorta_geometry
-if [[ ! -d aorta_geometry ]]
+rm -rf geometry
+if [[ ! -d geometry ]]
 then
-	mkdir -p aorta_geometry
-	cd aorta_geometry
+	mkdir -p geometry
+	cd geometry
 
-
-	cylinder.py aorta.vtk 1
-	db_to_raw.py aorta.vtk aorta --select_elem_type=tetra
-	surf_type=tri3
-	mesh_p1_to_p2 aorta aorta
+	cylinder 0.1 1 mesh.vtk --order=2
+	db_to_raw mesh.vtk mesh --select_elem_type=tetra10
 	surf_type=tri6
 
-	sfc aorta aorta
+	sfc mesh mesh
 	
-	skin aorta skin_aorta
-	# raw_to_db.py skin_aorta skin_aorta.vtk
+	skin mesh mesh_surface
+	raw_to_db mesh_surface mesh_surface.vtk
 
 	set -x
 
-	SFEM_DEBUG=1 create_sideset aorta -0.51 0 0  0.8 	inlet
-	SFEM_DEBUG=1 create_sideset aorta  0.51 0 0  0.8 	outlet
+	create_sideset mesh -0.51 0 0  0.5 	inlet
+	create_sideset mesh  0.51 0 0  0.5 	outlet
 
-	# raw_to_db.py inlet/surf 			inlet/surf.vtk 				--coords=aorta --cell_type=$surf_type
-	# raw_to_db.py outlet/surf 			outlet/surf.vtk 			--coords=aorta --cell_type=$surf_type
-	# raw_to_db.py aorta 					aorta.vtk 
+	surface_from_sideset mesh inlet  inlet/surf
+	surface_from_sideset mesh outlet outlet/surf
+
+	raw_to_db inlet/surf   inlet/surf.vtk 	--coords=mesh
+	raw_to_db outlet/surf  outlet/surf.vtk  --coords=mesh
+	raw_to_db mesh 		   mesh.vtk 
 
 	cd $HERE
 fi
 
-export SFEM_ROTATE_SIDESET=aorta_geometry/outlet
-# export SFEM_ROTATE_ANGLE=6
-# export SFEM_ROTATE_STEPS=31
-export SFEM_ROTATE_ANGLE=3.14
-export SFEM_ROTATE_STEPS=6
-# export SFEM_ROTATE_ANGLE=1.2
-# export SFEM_ROTATE_STEPS=10
+export SFEM_ROTATE_SIDESET=geometry/outlet
+export SFEM_ROTATE_ANGLE=12
+export SFEM_ROTATE_STEPS=24
 export SFEM_NEOHOOKEAN_OGDEN_USE_AOS=1
 export SFEM_USE_PARTIAL_ASSEMBLY=1
 export SFEM_ELEMENTS_PER_PACK=2048 
@@ -65,7 +60,5 @@ export SFEM_USE_PRECONDITIONER=0
 export SFEM_ENABLE_LINE_SEARCH=0
 rm -rf output
 
-$LAUNCH hyperelasticy aorta_geometry/aorta dirichlet.yaml output
-# raw_to_db.py aorta_geometry/aorta output.vtk -p 'output/out/*.*' $EXTRA_OPTIONS
-
-raw_to_db.py output/mesh output.xdmf -p "output/out/disp.0.*.*,output/out/disp.1.*.*,output/out/disp.2.*.*" --transient --n_time_steps=$(( SFEM_ROTATE_STEPS + 1 ))
+$LAUNCH hyperelasticy geometry/mesh dirichlet.yaml output
+raw_to_db output/mesh output.xdmf -p "output/out/disp.0.*.*,output/out/disp.1.*.*,output/out/disp.2.*.*" --transient --n_time_steps=$(( SFEM_ROTATE_STEPS + 1 ))
