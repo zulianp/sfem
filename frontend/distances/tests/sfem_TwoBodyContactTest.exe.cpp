3da43b9e42e40a1c6302c1fc3f842b547450839a
Patrick Zulian <patrick.zulian@gmail.com>
2026-05-29 15:41:33 +0200

    TET4 self contact

--- frontend/distances/tests/sfem_TwoBodyContactTest.exe.cpp
+++ frontend/distances/tests/sfem_TwoBodyContactTest.exe.cpp
@@ -21,8 +21,17 @@
 using namespace sfem;
 
 std::shared_ptr<Function> create_function(const ptrdiff_t nx, const ExecutionSpace es) {
-    auto mesh1 = smesh::Mesh::create_tet4_cube(Communicator::self(), nx, nx, nx, 0, 0, 0, 1, 1, 1);
-    auto mesh2 = smesh::Mesh::create_tet4_cube(Communicator::self(), nx, nx, nx, 0.1, 1.1, 0.1, 0.9, 1.9, 0.9);
+    auto mesh1 = smesh::Mesh::create_tet4_cube(Communicator::self(), nx, std::max<ptrdiff_t>(1, nx / 5), nx, 0, 0.8, 0, 1, 1, 1);
+    auto mesh2 = smesh::Mesh::create_tet4_cube(Communicator::self(),
+                                               std::max<ptrdiff_t>(1, nx / 2),
+                                               nx,
+                                               std::max<ptrdiff_t>(1, nx / 2),
+                                               0.25,
+                                               1.1,
+                                               0.25,
+                                               0.75,
+                                               1.9,
+                                               0.75);
 
     auto mesh = smesh::concatenate(mesh1, mesh2);
 
@@ -34,8 +43,17 @@ std::shared_ptr<Function> create_function(const ptrdiff_t nx, const ExecutionSpa
         return y > (1.9 - 1e-4) && y < (1.9 + 1e-4);
     });
 
-    auto bottom_ss = sfem::Sideset::create_from_selector(
-            mesh, [=](const geom_t /*x*/, const geom_t y, const geom_t /*z*/) -> bool { return y > (-1e-4) && y < (1e-4); });
+    // auto bottom_ss = sfem::Sideset::create_from_selector(
+    //         mesh,
+    //         [=](const geom_t /*x*/, const geom_t y, const geom_t /*z*/) -> bool { return y > (0.8 - 1e-4) && y < (0.8 + 1e-4);
+    //         });
+
+    auto left_ss = sfem::Sideset::create_from_selector(
+            mesh, [=](const geom_t x, const geom_t /*y*/, const geom_t /*z*/) -> bool { return x > (-1e-4) && x < (1e-4); });
+
+    auto right_ss = sfem::Sideset::create_from_selector(
+            mesh,
+            [=](const geom_t x, const geom_t /*y*/, const geom_t /*z*/) -> bool { return x > (1 - 1e-4) && x < (1 + 1e-4); });
 
     const int dim   = mesh->spatial_dimension();
     auto      space = FunctionSpace::create(mesh, dim);
@@ -46,8 +64,10 @@ std::shared_ptr<Function> create_function(const ptrdiff_t nx, const ExecutionSpa
     auto f = Function::create(space);
     f->add_operator(op);
 
-    auto top_ns    = smesh::create_nodeset_from_sidesets(mesh, top_ss);
-    auto bottom_ns = smesh::create_nodeset_from_sidesets(mesh, bottom_ss);
+    auto top_ns = smesh::create_nodeset_from_sidesets(mesh, top_ss);
+    // auto bottom_ns = smesh::create_nodeset_from_sidesets(mesh, bottom_ss);
+    auto left_ns  = smesh::create_nodeset_from_sidesets(mesh, left_ss);
+    auto right_ns = smesh::create_nodeset_from_sidesets(mesh, right_ss);
 
     assert(top_ns != nullptr);
     assert(bottom_ns != nullptr);
@@ -55,14 +75,23 @@ std::shared_ptr<Function> create_function(const ptrdiff_t nx, const ExecutionSpa
     assert(bottom_ns->size() > 0);
 
     DirichletConditions::Condition xtop{.sidesets = top_ss, .nodeset = top_ns, .value = 0, .component = 0};
-    DirichletConditions::Condition ytop{.sidesets = top_ss, .nodeset = top_ns, .value = -0.8, .component = 1};
+    DirichletConditions::Condition ytop{.sidesets = top_ss, .nodeset = top_ns, .value = -0.4, .component = 1};
     DirichletConditions::Condition ztop{.sidesets = top_ss, .nodeset = top_ns, .value = 0, .component = 2};
 
-    DirichletConditions::Condition xbottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 0};
-    DirichletConditions::Condition ybottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 1};
-    DirichletConditions::Condition zbottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 2};
+    DirichletConditions::Condition xleft{.sidesets = left_ss, .nodeset = left_ns, .value = 0, .component = 0};
+    DirichletConditions::Condition yleft{.sidesets = left_ss, .nodeset = left_ns, .value = 0, .component = 1};
+    DirichletConditions::Condition zleft{.sidesets = left_ss, .nodeset = left_ns, .value = 0, .component = 2};
 
-    auto conds = sfem::create_dirichlet_conditions(space, {xtop, ytop, ztop, xbottom, ybottom, zbottom}, es);
+    DirichletConditions::Condition xright{.sidesets = right_ss, .nodeset = right_ns, .value = 0, .component = 0};
+    DirichletConditions::Condition yright{.sidesets = right_ss, .nodeset = right_ns, .value = 0, .component = 1};
+    DirichletConditions::Condition zright{.sidesets = right_ss, .nodeset = right_ns, .value = 0, .component = 2};
+
+    // DirichletConditions::Condition xbottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 0};
+    // DirichletConditions::Condition ybottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 1};
+    // DirichletConditions::Condition zbottom{.sidesets = bottom_ss, .nodeset = bottom_ns, .value = 0, .component = 2};
+
+    auto conds = sfem::create_dirichlet_conditions(space, {xtop, ytop, ztop, xleft, yleft, zleft, xright, yright, zright}, es);
+    // auto conds = sfem::create_dirichlet_conditions(space, {xtop, ytop, ztop, xbottom, ybottom, zbottom}, es);
     f->add_constraint(conds);
 
     return f;
@@ -510,12 +539,14 @@ void nljacobi(ContactData&                                 cd,
     const mask_t* const contact_mask = contact_node_mask->data();
     real_t* const       xd           = x->data();
 
+    // If the material is nonlinear should be inside the loop
+    blas->values(elast_diag_values->size(), 0, elast_diag_values->data());
+    f->hessian_block_diag_sym(x->data(), elast_diag_values->data());
+
     for (int loop = 0; loop < n_loops; ++loop) {
         blas->values(ndofs, 0, material_grad->data());
-        blas->values(elast_diag_values->size(), 0, elast_diag_values->data());
 
         f->gradient(x->data(), material_grad->data());
-        f->hessian_block_diag_sym(x->data(), elast_diag_values->data());
 
         const real_t* const eg = material_grad->data();
         const real_t* const ed = elast_diag_values->data();
@@ -692,12 +723,12 @@ int test_two_body_contact() {
     ccd->find_earliest_impact_time(p0, p1, toi, 69, 1e-12);
 
     printf("TOI: %g\n", toi);
-    SFEM_TEST_APPROXEQ(toi, 0.125, 1e-2);
+    SFEM_TEST_APPROXEQ(toi, 0.25, 1e-2);
 
     // toi *= 1.1;
     blas->scal(space->n_dofs(), toi, displacement->data());
 
-    const real_t search_radius     = 0.06;
+    const real_t search_radius     = 0.02;
     const real_t search_radius_sqr = search_radius * search_radius;
 
     auto surface_elements = surface->block(0)->elements();
@@ -832,16 +863,8 @@ int test_two_body_contact() {
 
     auto agumentation = sfem::create_buffer<real_t>(trace_space->n_dofs(), es);
 
-    real_t penalty     = 10;
-    auto   grad        = sfem::create_buffer<real_t>(space->n_dofs(), es);
-    auto   macaulay    = sfem::create_buffer<real_t>(trace_space->n_dofs(), es);
-    auto   diag_values = sfem::create_buffer<real_t>(dim * dim, trace_space->n_dofs(), es);
-
-    auto elast_diag_values = sfem::create_buffer<real_t>(dim * dim * space->n_dofs(), es);
-    f->hessian_block_diag_sym(displacement->data(), elast_diag_values->data());
-
-    auto constraint_mask = sfem::create_buffer<mask_t>(mask_count(space->n_dofs()), es);
-    f->constraints_mask(constraint_mask->data());
+    real_t penalty          = 10;
+    auto   lagr_mult_normal = sfem::create_buffer<real_t>(space->n_dofs(), es);
 
     f->apply_constraints(displacement->data());
     f->apply_constraints(rhs->data());
@@ -850,8 +873,8 @@ int test_two_body_contact() {
     out->enable_AoS_to_SoA(true);
     out->set_output_dir(smesh::Path("contact_output"));
 
-    const int outer_loops = 20;
-    const int inner_loops = 1000;
+    const int outer_loops = 800;
+    const int inner_loops = 100;
     for (int outer = 0; outer < outer_loops; ++outer) {
         recompute_contact_conditions();
 
@@ -866,11 +889,34 @@ int test_two_body_contact() {
 
         nljacobi(cd, f, displacement, penalty, inner_loops);
 
-        recompute_contact_conditions();
+        blas->values(space->n_dofs(), 0, lagr_mult_normal->data());
+
+        {
+            const idx_t* const  node_mapping          = surface->node_mapping()->data();
+            const real_t* const lagr_mult             = agumentation->data();
+            const real_t* const normal_x              = normals->data()[0];
+            const real_t* const normal_y              = normals->data()[1];
+            const real_t* const normal_z              = normals->data()[2];
+            real_t* const       lagr_mult_normal_data = lagr_mult_normal->data();
+            const ptrdiff_t     n                     = surface->node_mapping()->size();
+
+#pragma omp parallel for
+            for (ptrdiff_t i = 0; i < n; ++i) {
+                const ptrdiff_t dof            = node_mapping[i] * 3;
+                const real_t    lm             = lagr_mult[i];
+                lagr_mult_normal_data[dof + 0] = lm * normal_x[i];
+                lagr_mult_normal_data[dof + 1] = lm * normal_y[i];
+                lagr_mult_normal_data[dof + 2] = lm * normal_z[i];
+            }
+        }
+
         out->write_time_step("disp", outer, displacement->data());
         out->write_time_step("distance", outer, distances_whole->data());
         out->write_time_step("directors", outer, directors->data());
+        out->write_time_step("lagr_mult_normal", outer, lagr_mult_normal->data());
         out->log_time(outer);
+
+        recompute_contact_conditions();
     }
 
     return SFEM_TEST_SUCCESS;
