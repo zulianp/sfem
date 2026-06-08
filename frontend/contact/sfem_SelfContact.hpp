924867b8977442113ab632ad11cbf01b4ff1cf6a
Patrick Zulian <patrick.zulian@gmail.com>
2026-06-08 19:01:03 +0200

    Small refactor

---
+++ frontend/contact/sfem_SelfContact.hpp
@@ -0,0 +1,36 @@
+#ifndef SFEM_SELF_CONTACT_HPP
+#define SFEM_SELF_CONTACT_HPP
+
+#include "sfem_FunctionSpace.hpp"
+#include "smesh_crs_graph.hpp"
+
+#include <memory>
+
+namespace sfem {
+
+    class Contact {
+    public:
+        virtual ~Contact() = default;
+
+        virtual void recompute() = 0;
+
+        virtual const std::shared_ptr<smesh::CRSGraph<count_t, idx_t>>& graph() const           = 0;
+        virtual smesh::SharedBuffer<real_t>&                            values()                = 0;
+        virtual smesh::SharedBuffer<real_t>&                            mass_vector()           = 0;
+        virtual smesh::SharedBuffer<real_t*>&                           normals()               = 0;
+        virtual smesh::SharedBuffer<real_t>&                            distances()             = 0;
+        virtual smesh::SharedBuffer<real_t>&                            frozen_displacement()   = 0;
+        virtual const smesh::SharedBuffer<real_t>&                      distances_whole() const = 0;
+        virtual const smesh::SharedBuffer<real_t>&                      directors() const       = 0;
+    };
+
+    std::shared_ptr<Contact> create_contact(const std::shared_ptr<FunctionSpace>&  space,
+                                            const std::shared_ptr<smesh::Mesh>&    surface,
+                                            const std::shared_ptr<Buffer<real_t>>& displacement,
+                                            real_t                                 margin,
+                                            real_t                                 search_radius_sqr,
+                                            ExecutionSpace                         es);
+
+}  // namespace sfem
+
+#endif  // SFEM_SELF_CONTACT_HPP
