10f9ddee995cce8f61e27f8c62f7d9e6c3e3297b
Patrick Zulian <patrick.zulian@gmail.com>
2026-06-12 10:33:49 +0200

    ...

--- algebra/bsr/sfem_BSR.hpp
+++ algebra/bsr/sfem_BSR.hpp
@@ -3,10 +3,16 @@
 
 #include <cstddef>
 #include <iostream>
+#include <limits>
 #include <memory>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 #include "sfem_MatrixFreeLinearSolver.hpp"
 #include "sfem_aliases.hpp"
+#include "smesh_types.hpp"
 
 namespace sfem {
     template <typename R, typename C, typename TStorage>
@@ -221,7 +227,289 @@ namespace sfem {
         return SFEM_SUCCESS;
     }
 
-    // TODO: implement bsr_mm_sym and bsr_mm_apply (see CRS as a reference)
+    template <typename R, typename C>
+    void bsr_mm_sym(const ptrdiff_t              block_rows,
+                    const ptrdiff_t              c_block_cols,
+                    const R* const SFEM_RESTRICT a_rowptr,
+                    const C* const SFEM_RESTRICT a_colidx,
+                    const R* const SFEM_RESTRICT b_rowptr,
+                    const C* const SFEM_RESTRICT b_colidx,
+                    R* const SFEM_RESTRICT       c_rowptr,
+                    R* const SFEM_RESTRICT       mask_workspace,
+                    const int                    n_workspaces) {
+        const R unseen = smesh::invalid_idx<R>();
+
+        c_rowptr[0] = 0;
+
+#pragma omp parallel num_threads(n_workspaces)
+        {
+#ifdef _OPENMP
+            const int tid = omp_get_thread_num();
+#else
+            const int tid = 0;
+#endif
+            R* const SFEM_RESTRICT mask = &mask_workspace[tid * c_block_cols];
+
+            for (ptrdiff_t i = 0; i < c_block_cols; i++) {
+                mask[i] = unseen;
+            }
+
+#pragma omp for schedule(static)
+            for (ptrdiff_t i = 0; i < block_rows; i++) {
+                R nnz = 0;
+
+                const R                      a_begin = a_rowptr[i];
+                const R                      a_end   = a_rowptr[i + 1];
+                const C* const SFEM_RESTRICT a_cols  = &a_colidx[a_begin];
+
+                for (R a_k = 0, a_len = a_end - a_begin; a_k < a_len; a_k++) {
+                    const C a_j = a_cols[a_k];
+
+                    const R                      b_begin = b_rowptr[a_j];
+                    const R                      b_end   = b_rowptr[a_j + 1];
+                    const C* const SFEM_RESTRICT b_cols  = &b_colidx[b_begin];
+
+                    for (R b_k = 0, b_len = b_end - b_begin; b_k < b_len; b_k++) {
+                        const C b_j = b_cols[b_k];
+
+                        if (mask[b_j] != i) {
+                            mask[b_j] = i;
+                            nnz++;
+                        }
+                    }
+                }
+
+                c_rowptr[i + 1] = nnz;
+            }
+        }
+
+        for (ptrdiff_t c_i = 0; c_i < block_rows; c_i++) {
+            c_rowptr[c_i + 1] += c_rowptr[c_i];
+        }
+    }
+
+    template <typename TStorage>
+    void bsr_block_mm_acc(const TStorage* const SFEM_RESTRICT a_block,
+                          const TStorage* const SFEM_RESTRICT b_block,
+                          TStorage* const SFEM_RESTRICT       c_block,
+                          const int                           row_block_size,
+                          const int                           inner_block_size,
+                          const int                           col_block_size,
+                          const bool                          init) {
+        if (init) {
+            for (int d1 = 0; d1 < row_block_size; d1++) {
+                TStorage* const SFEM_RESTRICT c_row = &c_block[d1 * col_block_size];
+                for (int d2 = 0; d2 < col_block_size; d2++) {
+                    TStorage sum = 0;
+                    for (int d_inner = 0; d_inner < inner_block_size; d_inner++) {
+                        sum += a_block[d1 * inner_block_size + d_inner] * b_block[d_inner * col_block_size + d2];
+                    }
+                    c_row[d2] = sum;
+                }
+            }
+        } else {
+            for (int d1 = 0; d1 < row_block_size; d1++) {
+                TStorage* const SFEM_RESTRICT c_row = &c_block[d1 * col_block_size];
+                for (int d2 = 0; d2 < col_block_size; d2++) {
+                    TStorage sum = 0;
+                    for (int d_inner = 0; d_inner < inner_block_size; d_inner++) {
+                        sum += a_block[d1 * inner_block_size + d_inner] * b_block[d_inner * col_block_size + d2];
+                    }
+                    c_row[d2] += sum;
+                }
+            }
+        }
+    }
+
+    template <typename R, typename C, typename TStorage>
+    void bsr_mm_apply(const ptrdiff_t                     block_rows,
+                      const ptrdiff_t                     c_block_cols,
+                      const int                           a_row_block_size,
+                      const int                           a_col_block_size,
+                      const int                           b_col_block_size,
+                      const R* const SFEM_RESTRICT        a_rowptr,
+                      const C* const SFEM_RESTRICT        a_colidx,
+                      const TStorage* const SFEM_RESTRICT a_values,
+                      const R* const SFEM_RESTRICT        b_rowptr,
+                      const C* const SFEM_RESTRICT        b_colidx,
+                      const TStorage* const SFEM_RESTRICT b_values,
+                      const R* const SFEM_RESTRICT        c_rowptr,
+                      C* const SFEM_RESTRICT              c_colidx,
+                      TStorage* const SFEM_RESTRICT       c_values,
+                      R* const SFEM_RESTRICT              next_workspace,
+                      TStorage* const SFEM_RESTRICT       acc_workspace,
+                      const int                           n_workspaces) {
+        const R init   = std::numeric_limits<R>::max();
+        const R unseen = smesh::invalid_idx<R>();
+
+        const int a_block_matrix_size = a_row_block_size * a_col_block_size;
+        const int b_block_matrix_size = a_col_block_size * b_col_block_size;
+        const int c_block_matrix_size = a_row_block_size * b_col_block_size;
+
+#pragma omp parallel num_threads(n_workspaces)
+        {
+#ifdef _OPENMP
+            const int tid = omp_get_thread_num();
+#else
+            const int tid = 0;
+#endif
+            R* const SFEM_RESTRICT        next = &next_workspace[tid * c_block_cols];
+            TStorage* const SFEM_RESTRICT acc  = &acc_workspace[tid * c_block_cols * c_block_matrix_size];
+
+            for (ptrdiff_t i = 0; i < c_block_cols; i++) {
+                next[i] = unseen;
+            }
+
+#pragma omp for schedule(static)
+            for (ptrdiff_t i = 0; i < block_rows; i++) {
+                R head = init;
+                R len  = 0;
+
+                const R                      a_begin = a_rowptr[i];
+                const R                      a_end   = a_rowptr[i + 1];
+                const C* const SFEM_RESTRICT a_cols  = &a_colidx[a_begin];
+
+                for (R a_k = 0, a_len = a_end - a_begin; a_k < a_len; a_k++) {
+                    const C a_j = a_cols[a_k];
+
+                    const R                      b_begin = b_rowptr[a_j];
+                    const R                      b_end   = b_rowptr[a_j + 1];
+                    const C* const SFEM_RESTRICT b_cols  = &b_colidx[b_begin];
+
+                    const TStorage* const SFEM_RESTRICT a_block = &a_values[(a_begin + a_k) * a_block_matrix_size];
+
+                    for (R b_k = 0, b_len = b_end - b_begin; b_k < b_len; b_k++) {
+                        const C b_j = b_cols[b_k];
+
+                        const TStorage* const SFEM_RESTRICT b_block = &b_values[(b_begin + b_k) * b_block_matrix_size];
+
+                        if (next[b_j] == unseen) {
+                            next[b_j] = head;
+                            head      = b_j;
+                            bsr_block_mm_acc(a_block,
+                                             b_block,
+                                             &acc[b_j * c_block_matrix_size],
+                                             a_row_block_size,
+                                             a_col_block_size,
+                                             b_col_block_size,
+                                             true);
+                            len++;
+                        } else {
+                            bsr_block_mm_acc(a_block,
+                                             b_block,
+                                             &acc[b_j * c_block_matrix_size],
+                                             a_row_block_size,
+                                             a_col_block_size,
+                                             b_col_block_size,
+                                             false);
+                        }
+                    }
+                }
+
+                R offset = c_rowptr[i];
+                for (R k = 0; k < len; k++) {
+                    c_colidx[offset] = head;
+
+                    TStorage* const SFEM_RESTRICT       c_block   = &c_values[offset * c_block_matrix_size];
+                    const TStorage* const SFEM_RESTRICT acc_block = &acc[head * c_block_matrix_size];
+                    for (int d = 0; d < c_block_matrix_size; d++) {
+                        c_block[d] = acc_block[d];
+                    }
+
+                    offset++;
+
+                    const R temp = head;
+                    head         = next[head];
+                    next[temp]   = unseen;
+                }
+            }
+        }
+    }
+
+    template <typename R, typename C, typename TStorage>
+    int bsr_mm(const ptrdiff_t               c_block_cols,
+               const int                     a_row_block_size,
+               const int                     a_col_block_size,
+               const int                     b_col_block_size,
+               const SharedBuffer<R>&        a_rowptr,
+               const SharedBuffer<C>&        a_colidx,
+               const SharedBuffer<TStorage>& a_values,
+               const SharedBuffer<R>&        b_rowptr,
+               const SharedBuffer<C>&        b_colidx,
+               const SharedBuffer<TStorage>& b_values,
+               SharedBuffer<R>&              c_rowptr,
+               SharedBuffer<C>&              c_colidx,
+               SharedBuffer<TStorage>&       c_values) {
+        const ptrdiff_t block_rows = a_rowptr->size() - 1;
+
+        if (c_rowptr->size() != block_rows + 1) {
+            c_rowptr = create_host_buffer<R>(block_rows + 1);
+        }
+
+        const R* const SFEM_RESTRICT        d_a_rowptr = a_rowptr->data();
+        const C* const SFEM_RESTRICT        d_a_colidx = a_colidx->data();
+        const TStorage* const SFEM_RESTRICT d_a_values = a_values->data();
+        const R* const SFEM_RESTRICT        d_b_rowptr = b_rowptr->data();
+        const C* const SFEM_RESTRICT        d_b_colidx = b_colidx->data();
+        const TStorage* const SFEM_RESTRICT d_b_values = b_values->data();
+
+        R* const SFEM_RESTRICT d_c_rowptr = c_rowptr->data();
+
+#ifdef _OPENMP
+        const int n_workspaces = omp_get_max_threads();
+#else
+        const int n_workspaces = 1;
+#endif
+
+        auto mask_workspace = create_host_buffer<R>(n_workspaces * c_block_cols);
+
+        bsr_mm_sym(block_rows,
+                   c_block_cols,
+                   d_a_rowptr,
+                   d_a_colidx,
+                   d_b_rowptr,
+                   d_b_colidx,
+                   d_c_rowptr,
+                   mask_workspace->data(),
+                   n_workspaces);
+
+        const ptrdiff_t nblocks             = d_c_rowptr[block_rows];
+        const ptrdiff_t c_block_matrix_size = (ptrdiff_t)a_row_block_size * b_col_block_size;
+
+        if (c_colidx->size() != nblocks) {
+            c_colidx = create_host_buffer<C>(nblocks);
+        }
+
+        if (c_values->size() != nblocks * c_block_matrix_size) {
+            c_values = create_host_buffer<TStorage>(nblocks * c_block_matrix_size);
+        }
+
+        C* const SFEM_RESTRICT        d_c_colidx = c_colidx->data();
+        TStorage* const SFEM_RESTRICT d_c_values = c_values->data();
+
+        auto next_workspace = create_host_buffer<R>(n_workspaces * c_block_cols);
+        auto acc_workspace  = create_host_buffer<TStorage>(n_workspaces * c_block_cols * c_block_matrix_size);
+
+        bsr_mm_apply(block_rows,
+                     c_block_cols,
+                     a_row_block_size,
+                     a_col_block_size,
+                     b_col_block_size,
+                     d_a_rowptr,
+                     d_a_colidx,
+                     d_a_values,
+                     d_b_rowptr,
+                     d_b_colidx,
+                     d_b_values,
+                     d_c_rowptr,
+                     d_c_colidx,
+                     d_c_values,
+                     next_workspace->data(),
+                     acc_workspace->data(),
+                     n_workspaces);
+
+        return SFEM_SUCCESS;
+    }
 
     template <typename T>
     void bsr_scale_output(const ptrdiff_t rows, const T scale_output, T* const SFEM_RESTRICT y) {
@@ -401,6 +689,79 @@ namespace sfem {
                          t_colidx->data(),
                          t_values->data(),
                          scaling,
+                         x,
+                         y);
+            };
+
+            return ret;
+        }
+
+        std::shared_ptr<BSR<R, C, TStorage, T>> mm(const std::shared_ptr<BSR<R, C, TStorage, T>>& other) const {
+            SFEM_TRACE_SCOPE("BSR::mm");
+            if (execution_space() != EXECUTION_SPACE_HOST || other->execution_space() != EXECUTION_SPACE_HOST) {
+                // TODO: Implement device version
+                SFEM_ERROR("Matrix multiplication is not supported for non-host execution space");
+                return nullptr;
+            }
+
+            const ptrdiff_t a_block_rows = row_ptr->size() - 1;
+            const ptrdiff_t b_block_rows = other->row_ptr->size() - 1;
+            if (block_cols_ != b_block_rows) {
+                SFEM_ERROR("BSR::mm incompatible block dimensions: A has %td block cols, B has %td block rows",
+                           block_cols_,
+                           b_block_rows);
+                return nullptr;
+            }
+            if (col_block_size_ != other->row_block_size_) {
+                SFEM_ERROR("BSR::mm incompatible block sizes: A col block size %d, B row block size %d",
+                           col_block_size_,
+                           other->row_block_size_);
+                return nullptr;
+            }
+
+            auto ret     = std::make_shared<BSR<R, C, TStorage, T>>();
+            ret->row_ptr = create_host_buffer<R>(0);
+            ret->col_idx = create_host_buffer<C>(0);
+            ret->values  = create_host_buffer<TStorage>(0);
+
+            bsr_mm(other->block_cols_,
+                   row_block_size_,
+                   col_block_size_,
+                   other->col_block_size_,
+                   row_ptr,
+                   col_idx,
+                   values,
+                   other->row_ptr,
+                   other->col_idx,
+                   other->values,
+                   ret->row_ptr,
+                   ret->col_idx,
+                   ret->values);
+
+            const ptrdiff_t ret_block_rows = a_block_rows;
+            const ptrdiff_t ret_block_cols = other->block_cols_;
+            const int       ret_row_bs     = row_block_size_;
+            const int       ret_col_bs     = other->col_block_size_;
+
+            ret->block_cols_                = ret_block_cols;
+            ret->row_block_size_            = ret_row_bs;
+            ret->col_block_size_            = ret_col_bs;
+            ret->uniform_pre_output_scaling = 0;
+            ret->execution_space_           = EXECUTION_SPACE_HOST;
+
+            auto c_rowptr = ret->row_ptr;
+            auto c_colidx = ret->col_idx;
+            auto c_values = ret->values;
+
+            ret->apply_ = [=](const T* const x, T* const y) {
+                bsr_spmv(ret_block_rows,
+                         ret_block_cols,
+                         ret_row_bs,
+                         ret_col_bs,
+                         c_rowptr->data(),
+                         c_colidx->data(),
+                         c_values->data(),
+                         static_cast<T>(0),
                          x,
                          y);
             };
--- algebra/bsr/tests/sfem_BSRTest.cpp
+++ algebra/bsr/tests/sfem_BSRTest.cpp
@@ -102,11 +102,120 @@ int test_bsr_transpose_apply() {
     return SFEM_TEST_SUCCESS;
 }
 
+int test_bsr_mm_rectangular() {
+    using R = sfem::count_t;
+    using C = sfem::idx_t;
+    using T = sfem::real_t;
+
+    // 1x1 blocks: identical sparsity/numerics to the CRS mm test.
+    auto a_rowptr = make_buffer<R>({0, 2, 3, 5});
+    auto a_colidx = make_buffer<C>({0, 2, 1, 0, 3});
+    auto a_values = make_buffer<T>({1, 2, 3, 4, 5});
+
+    auto b_rowptr = make_buffer<R>({0, 1, 3, 4, 6});
+    auto b_colidx = make_buffer<C>({1, 0, 1, 0, 0, 1});
+    auto b_values = make_buffer<T>({7, 8, 9, 10, 11, 12});
+
+    auto c_rowptr = sfem::create_host_buffer<R>(0);
+    auto c_colidx = sfem::create_host_buffer<C>(0);
+    auto c_values = sfem::create_host_buffer<T>(0);
+
+    SFEM_TEST_ASSERT(sfem::bsr_mm(2, 1, 1, 1, a_rowptr, a_colidx, a_values, b_rowptr, b_colidx, b_values, c_rowptr, c_colidx, c_values) ==
+                     SFEM_SUCCESS);
+
+    const R expected_rowptr[] = {0, 2, 4, 6};
+    SFEM_TEST_EQ(c_rowptr->size(), static_cast<size_t>(4));
+    SFEM_ASSERT_ARRAY_EQ(4, c_rowptr->data(), expected_rowptr);
+
+    std::vector<T> dense(6, 0);
+    for (ptrdiff_t r = 0; r < 3; ++r) {
+        for (R k = c_rowptr->data()[r]; k < c_rowptr->data()[r + 1]; ++k) {
+            const C col = c_colidx->data()[k];
+            SFEM_TEST_ASSERT(col >= 0 && col < 2);
+            dense[static_cast<size_t>(r) * 2 + col] = c_values->data()[k];
+        }
+    }
+
+    const T expected_dense[] = {20, 7, 24, 27, 55, 88};
+    SFEM_ASSERT_ARRAY_APPROX_EQ(6, dense.data(), expected_dense, 1e-6);
+
+    return SFEM_TEST_SUCCESS;
+}
+
+int test_bsr_mm_apply() {
+    using R = sfem::count_t;
+    using C = sfem::idx_t;
+    using T = sfem::real_t;
+
+    auto a = sfem::h_bsr_spmv<R, C, T>(2, 2, 2, a_rowptr(), a_colidx(), a_values(), static_cast<T>(0));
+
+    // B has two 2x2 identity blocks in column 0.
+    auto b_rowptr = make_buffer<R>({0, 1, 2});
+    auto b_colidx = make_buffer<C>({0, 0});
+    auto b_values = make_buffer<T>({1, 0, 0, 1, 1, 0, 0, 1});
+
+    auto b = sfem::h_bsr_spmv<R, C, T>(2, 1, 2, b_rowptr, b_colidx, b_values, static_cast<T>(0));
+    auto c = a->mm(b);
+
+    SFEM_TEST_EQ(c->rows(), static_cast<ptrdiff_t>(4));
+    SFEM_TEST_EQ(c->cols(), static_cast<ptrdiff_t>(2));
+
+    // C(:,0) = A(:,0) + A(:,1), C(1,0) = A(1,0)
+    const T expected_blocks[] = {6, 8, 10, 12, 9, 10, 11, 12};
+    SFEM_TEST_EQ(c->values->size(), static_cast<size_t>(8));
+    SFEM_ASSERT_ARRAY_APPROX_EQ(8, c->values->data(), expected_blocks, 1e-6);
+
+    return SFEM_TEST_SUCCESS;
+}
+
+int test_bsr_rap() {
+    using R = sfem::count_t;
+    using C = sfem::idx_t;
+    using T = sfem::real_t;
+
+    auto r_rowptr = make_buffer<R>({0, 2, 4});
+    auto r_colidx = make_buffer<C>({0, 1, 1, 2});
+    auto r_values = make_buffer<T>({1, 0.5, 0.5, 1});
+
+    auto a_rowptr = make_buffer<R>({0, 2, 5, 7});
+    auto a_colidx = make_buffer<C>({0, 1, 0, 1, 2, 1, 2});
+    auto a_values = make_buffer<T>({4, 1, 1, 3, 2, 2, 5});
+
+    auto p_rowptr = make_buffer<R>({0, 1, 3, 4});
+    auto p_colidx = make_buffer<C>({0, 0, 1, 1});
+    auto p_values = make_buffer<T>({1, 0.5, 0.5, 1});
+
+    auto r = sfem::h_bsr_spmv<R, C, T>(2, 3, 1, r_rowptr, r_colidx, r_values, 0);
+    auto a = sfem::h_bsr_spmv<R, C, T>(3, 3, 1, a_rowptr, a_colidx, a_values, 0);
+    auto p = sfem::h_bsr_spmv<R, C, T>(3, 2, 1, p_rowptr, p_colidx, p_values, 0);
+    auto c = sfem::rap(r, a, p);
+
+    SFEM_TEST_EQ(c->rows(), static_cast<ptrdiff_t>(2));
+    SFEM_TEST_EQ(c->cols(), static_cast<ptrdiff_t>(2));
+
+    std::vector<T> dense(4, 0);
+    for (ptrdiff_t i = 0; i < c->rows(); ++i) {
+        for (R k = c->row_ptr->data()[i]; k < c->row_ptr->data()[i + 1]; ++k) {
+            const C col = c->col_idx->data()[k];
+            SFEM_TEST_ASSERT(col >= 0 && col < c->cols());
+            dense[static_cast<size_t>(i) * c->cols() + col] = c->values->data()[k];
+        }
+    }
+
+    const T expected_dense[] = {5.75, 2.25, 2.25, 7.75};
+    SFEM_ASSERT_ARRAY_APPROX_EQ(4, dense.data(), expected_dense, 1e-6);
+
+    return SFEM_TEST_SUCCESS;
+}
+
 int main(int argc, char* argv[]) {
     SFEM_UNIT_TEST_INIT(argc, argv);
 
     SFEM_RUN_TEST(test_bsr_transpose);
     SFEM_RUN_TEST(test_bsr_transpose_apply);
+    SFEM_RUN_TEST(test_bsr_mm_rectangular);
+    SFEM_RUN_TEST(test_bsr_mm_apply);
+    SFEM_RUN_TEST(test_bsr_rap);
 
     SFEM_UNIT_TEST_FINALIZE();
     return SFEM_UNIT_TEST_ERR();
